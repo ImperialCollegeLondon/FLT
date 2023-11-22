1f59f4ab03bedab264087a1b675c6f340940f3d0
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-11-22 19:34:18 +0000

    remove spaces

--- FLT.lean
+++ FLT.lean
@@ -1,7 +1,7 @@
 import Mathlib
 
-theorem PNat.pow_add_pow_ne_pow 
+theorem PNat.pow_add_pow_ne_pow
     (x y z : ℕ+)
-    (n : ℕ) (hn : n > 2) : 
+    (n : ℕ) (hn : n > 2) :
     x^n + y^n ≠ z^n := by
   sorry
