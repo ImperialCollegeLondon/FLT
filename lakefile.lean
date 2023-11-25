48860d85efe0296dfff76ec821c534ceff98b983
utensil <utensilcandel@gmail.com>
2023-11-25 20:18:23 +0800

    Add doc-gen4

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,11 @@ package «FLT» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+-- This is run only if we're in `dev` mode. This is so not everyone has to build doc-gen
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
+
 @[default_target]
 lean_lib «FLT» {
   -- add any library configuration options here
