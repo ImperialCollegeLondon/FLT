dbe9d794da5c2127fbd96368ff93061b66dfe934
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-12-06 03:07:35 +0000

    preliminary thing

---
+++ FLT/TateCurve.lean
@@ -0,0 +1,9 @@
+/-
+
+Need Tate uniformisation from Silverman 2 Ch 5. Don't need surjectivity.
+May as well work over a general field complete wrt a nontrivial nonarch norm.
+
+Key result needed: Galois action on Qpbar-points of p-torsion of elliptic curve E
+is isomorphic to some explicit quotient of Qpbar^* by q(E)^ℤ.
+
+-/
