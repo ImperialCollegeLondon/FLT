5c8a683168a01922e61443373c5ee991909a91f6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-12-14 19:10:43 +0000

    add stub HardlyRamified file

---
+++ FLT/Basic/HardlyRamified.lean
@@ -0,0 +1,13 @@
+import Mathlib.Tactic
+
+/-
+Taylor: You could say that
+a representation
+
+r: G_Q -> GL_2(F_p)
+
+is *hardly ramified* if 1) det = cyclo, 2) unramified outside 2p, 3)
+r|_{G_p} comes from ffgs and r|_{G_2}^ss is unramified.
+
+Theorem: p-torsion in Frey curve is hardly ramified.
+-/
