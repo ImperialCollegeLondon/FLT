a0cc286368cf801c1b018f29caa29f68900aea13
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-12-14 19:11:23 +0000

    update stub Tate curve file

--- FLT/TateCurve.lean
+++ FLT/TateCurve.lean
@@ -4,6 +4,14 @@ Need Tate uniformisation from Silverman 2 Ch 5. Don't need surjectivity.
 May as well work over a general field complete wrt a nontrivial nonarch norm.
 
 Key result needed: Galois action on Qpbar-points of p-torsion of elliptic curve E
-is isomorphic to some explicit quotient of Qpbar^* by q(E)^ℤ.
+over Qp is isomorphic to some explicit quotient of Qpbar^* by q(E)^ℤ.
 
+One of the hardest parts of the argument is surjectivity; we don't need it so I propose
+we skip it. This enables the entire argument to be run over a field complete with respect
+to a nontrivial nonarchimedean real-valued norm.
+
+Key thing we want: injection from K^* / <q> into E(K) where K is a field complete wrt
+a nontriv nonarch norm, E is the Frey curve and q is chosen appropriately; proof that the injection is functorial in the sense that if K -> L
+is an injection of fields and the norm on L restricts to the norm on K then the obvious
+diagram commutes.
 -/
