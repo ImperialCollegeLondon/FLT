0cbcdc40440809b5175430d40d29998bc3e1b1ff
Jujian Zhang <jujian.zhang1998@outlook.com>
2024-03-13 20:42:37 +0000

    fix

--- FLT/Proj3/LessCrazyIdea.lean
+++ FLT/Proj3/LessCrazyIdea.lean
@@ -1,19 +1,21 @@
-import Mathlib.Algebra.Category.CommAlgebraCat.Monoidal
+import FLT.Proj3.CommAlgebraCat.Monoidal
+import FLT.for_mathlib.HopfAlgebra.Basic
 import Mathlib.CategoryTheory.Yoneda
-import Mathlib.Proj2
+
 
 open CategoryTheory Opposite
 open scoped MonoidalCategory
 
-variable (k : Type) [CommRing k]
+variable (k A : Type) [CommRing k] [CommRing A] [Bialgebra k A]
 
-variable (A : CommAlgebraCat.{0} k)
-
-variable (Δ :  A ⟶ A ⊗ A) (ε : A ⟶ CommAlgebraCat.of k k)
+variable (Δ :  CommAlgebraCat.of k A ⟶ CommAlgebraCat.of k A ⊗ CommAlgebraCat.of k A)
+variable (ε : CommAlgebraCat.of k A ⟶ CommAlgebraCat.of k k)
 
 
 variable {k} in
-def square (F : CommAlgebraCat k ⥤ Type) [F.Corepresentable] : CommAlgebraCat k ⥤ Type where
+@[simps]
+def square (F : CommAlgebraCat k ⥤ Type) [F.Corepresentable] :
+    CommAlgebraCat k ⥤ Type where
   obj A := (F.obj A) × (F.obj A)
   map f x := ⟨F.map f x.1, F.map f x.2⟩
 
@@ -34,49 +36,27 @@ instance (A : CommAlgebraCat k) [Bialgebra k A] : Monoid (F.obj A) where
   mul_one := sorry
 -/
 
-lemma ex1 [AffineMonoid k F] : Bialgebra k A := sorry
+-- lemma ex1 [AffineMonoid k F] : Bialgebra k A := sorry
 -- #check Points.mul1
 
-@[simp]
-def coyonedaToAlgHom {X : CommAlgebraCat k} (f : (coyoneda.obj <| op A).obj X) :
-  A →ₐ[k] X := f
+-- instance [Bialgebra k A] {X : CommAlgebraCat k}
+--   (f : A →ₐ[k] X) : (Points (R := k) (A := A) (L := X)) f := sorry
 
-def algHomToCoyoneda {X : CommAlgebraCat k} (f : A →ₐ[k] X) :
-  (coyoneda.obj <| op A).obj X := f
+instance : Bialgebra k ↑(op (CommAlgebraCat.of k A)).unop :=
+  inferInstanceAs <| Bialgebra k A
 
-open Points
-variable {k A} in
-set_option tactic.skipAssignedInstances false
--- def asPoint [Bialgebra k A] {X : CommAlgebraCat k} (f : A →ₐ[k] X) :
---     Points.Points (R := k) (A := A) (L := X) where
---       toFun := _
---       map_one' := _
---       map_mul' := _
---       map_zero' := _
---       map_add' := _
---       commutes' := _
+def ex2 [Bialgebra k A]  :
+    AffineMonoid k (coyoneda.obj <| op <| CommAlgebraCat.of k A) where
+  m :=
+  { app := fun X xx => AlgHomPoint.mul xx.1 xx.2
+    naturality := by
+      intros X Y f
+      ext ⟨(x1 : A →ₐ[k] X), (x2 : A →ₐ[k] X)⟩
+      simp only [coyoneda_obj_obj, unop_op, square_obj, CommAlgebraCat.coe_of, types_comp_apply,
+        square_map, coyoneda_obj_map]
+      sorry }
+  e := _
 
-instance [Bialgebra k A] {X : CommAlgebraCat k}
-  (f : A →ₐ[k] X) : (Points (R := k) (A := A) (L := X)) f := sorry
+-- variable (g : A →ₐ[k] A) [Bialgebra k A]
 
-def ex2 [Bialgebra k A]  : AffineMonoid k (coyoneda.obj <| op A) where
-    m :=
-    { app := fun X xx => algHomToCoyoneda k A <| _
-
-        -- Points.mul1 (R := k) (A := A) (L := X)
-        -- -- _ _
-        --   (asPoint <| coyonedaToAlgHom k A xx.1) (asPoint <| coyonedaToAlgHom k A xx.2)
-    -- by
-    --     have x1 : A ⟶ X := xx.1
-    --     have x2 : A ⟶ X := xx.2
-
-        -- let y1 : F.coreprX ⟶ X := F.coreprW.symm.hom.app X xx.1
-
-        -- let y2 : F.coreprX ⟶ X := F.coreprW.symm.hom.app X xx.2
-        -- let h := y1 ⊗ y2
-      naturality := _ }
-    e := _
-
-variable (g : A →ₐ[k] A) [Bialgebra k A]
-
-lemma sidjao :  (A →ₐ[k] A) = (Points (R := k) (A := A) (L := A)) := rfl
+-- lemma sidjao :  (A →ₐ[k] A) = (Points (R := k) (A := A) (L := A)) := rfl
