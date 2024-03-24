6190505d4d87eb63d4712694cb4dd2941e652b47
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-03-24 16:15:23 +0000

    yael script for FLT

--- FLT.lean
+++ FLT.lean
@@ -1,8 +1,9 @@
-import FLT.Basic.Reductions
-import FLT.for_mathlib.Coalgebra.Monoid
-import FLT.TateCurve.TateCurve
-import FLT.for_mathlib.Coalgebra.Sweedler
 import FLT.Basic.HardlyRamified
+import FLT.Basic.Reductions
+import FLT.Basic.ZulipQ
+import FLT.EllipticCurve.Torsion
+import FLT.for_mathlib.Coalgebra.Monoid
+import FLT.for_mathlib.Coalgebra.Sweedler
 import FLT.for_mathlib.Coalgebra.TensorProduct
 import FLT.for_mathlib.HopfAlgebra.Basic
-import FLT.EllipticCurve.GaloisRepresentation
+import FLT.TateCurve.TateCurve
