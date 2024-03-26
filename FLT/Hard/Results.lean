990ac45fb1f4b01cd60701e529cd93451e1853e3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-03-26 16:17:26 +0000

    stub for hard results

---
+++ FLT/Hard/Results.lean
@@ -0,0 +1,13 @@
+/-
+
+Mazur's theorem (easy to state)
+
+cyclic base change (need definition of auto rep)
+
+Moret-Bailly (need to be able to say curve)
+
+pi -> rho (need def of auto rep)
+
+JL (need def of auto rep)
+
+-/
