1eff78d24db4d1e97cc44857f51c4b1b48bcec7e
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-03-26 16:44:14 +0000

    update file list

--- FLT.lean
+++ FLT.lean
@@ -1,9 +1,10 @@
-import FLT.Basic.HardlyRamified
 import FLT.Basic.Reductions
-import FLT.Basic.ZulipQ
 import FLT.EllipticCurve.Torsion
 import FLT.for_mathlib.Coalgebra.Monoid
 import FLT.for_mathlib.Coalgebra.Sweedler
 import FLT.for_mathlib.Coalgebra.TensorProduct
 import FLT.for_mathlib.HopfAlgebra.Basic
+import FLT.GaloisRepresentation.HardlyRamified
+import FLT.GroupScheme.FiniteFlat
+import FLT.Hard.Results
 import FLT.TateCurve.TateCurve
