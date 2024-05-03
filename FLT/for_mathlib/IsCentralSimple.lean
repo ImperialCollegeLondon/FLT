a1b396a5548bc9bd0a5353ef16e825c02bf26b21
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-05-03 02:41:31 +0100

    matrices are central simple algebras

---
+++ FLT/for_mathlib/IsCentralSimple.lean
@@ -0,0 +1,60 @@
+/-
+Copyright (c) 2024 Kevin Buzzard. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kevin Buzzard
+-/
+
+import Mathlib.Algebra.Quaternion -- probably get away with less
+
+
+/-!
+# Characteristic predicate for central simple algebras
+
+In this file we define the predicate `IsCentralSimple K D` where `K` is a field
+and `D` is a (noncommutative) `K`-algebra.
+
+Note that the predicate makes sense just for `K` a `CommRing` but it doesn't give the
+right definition; for a commutative ring base one should use the theory of Azumaya algebras.
+This adds an extra layer of complication which we don't need. In fact ideals of `K`
+immediately give rise to nontrivial quotients of `D` so there are no central simple
+algebras in this case according to our definition.
+
+-/
+
+universe u v w
+
+structure IsCentralSimple
+    (K : Type u) [Field K] (D : Type v) [Ring D] [Algebra K D] : Prop where
+  is_central : ∀ d : D, d ∈ Subring.center D → ∃ k : K, d = algebraMap K D k
+  is_simple : IsSimpleOrder (RingCon D)
+
+variable (K : Type u) [Field K]
+
+theorem MatrixRing.isCentralSimple (ι : Type v) (hι : Fintype ι) (hnonempty : Nonempty ι) [DecidableEq ι] :
+    IsCentralSimple K (Matrix ι ι K) := sorry
+
+namespace IsCentralSimple
+
+variable (D : Type v) [Ring D] [Algebra K D] (h : IsCentralSimple K D)
+
+/-
+\begin{lemma}
+    \label{IsCentralSimple.baseChange}
+    If $D$ is a central simple algebra over~$K$ and $L/K$ is a field extension, then $L\otimes_KD$
+    is a central simple algebra over~$L$.
+\end{lemma}
+\begin{proof}
+    This is not too hard: it's lemma b of section 12.4 in Peirce's "Associative algebras".
+    Will maybe write more on Saturday.
+\end{proof}
+-/
+
+open scoped TensorProduct
+
+-- lemma baseChange (L : Type w) [Field L] [Algebra K L] : IsCentralSimple L (L ⊗[K] D) := sorry
+
+end IsCentralSimple
+
+-- restrict to 4d case
+-- theorem exists_quaternionAlgebra_iso (hK : (2 : K) ≠ 0) :
+--     ∃ a b : K, Nonempty (D ≃ₐ[K] ℍ[K, a, b]) := sorry
