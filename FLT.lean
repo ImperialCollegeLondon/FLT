b17d8d31e27a3124dee6dfc30560a92fd06730f6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-05-05 15:51:58 +0100

    Mario's suggestion about FLT

--- FLT.lean
+++ FLT.lean
@@ -1,11 +1,23 @@
+import Mathlib.Tactic
 import FLT.Basic.Reductions
-import FLT.EllipticCurve.Torsion
-import FLT.for_mathlib.Coalgebra.Monoid
-import FLT.for_mathlib.Coalgebra.Sweedler
-import FLT.for_mathlib.Coalgebra.TensorProduct
-import FLT.for_mathlib.HopfAlgebra.Basic
-import FLT.for_mathlib.IsCentralSimple
-import FLT.GaloisRepresentation.HardlyRamified
-import FLT.GroupScheme.FiniteFlat
-import FLT.Hard.Results
-import FLT.TateCurve.TateCurve
+
+/-!
+
+# Fermat's Last Theorem
+
+There are many ways of stating Fermat's Last Theorem.
+In this file, we give the traditional statement using
+the positive integers `ℕ+`, and deduce it from
+a proof of Mathlib's version `FermatLastTheorem`
+of the statement (which is a statement about the
+nonnegative integers `ℕ`.)
+
+-/
+
+/-- Fermat's Last Theorem for positive naturals. -/
+theorem PNat.pow_add_pow_ne_pow
+    (x y z : ℕ+)
+    (n : ℕ) (hn : n > 2) :
+    x^n + y^n ≠ z^n :=
+  PNat.pow_add_pow_ne_pow_of_FermatLastTheorem FLT.Wiles_Taylor_Wiles x y z n hn
+
