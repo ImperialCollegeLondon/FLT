83e6e505d0e88df8caebd809508e6a6326e38a7e
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2024-06-01 09:03:16 +0800

    Update FieldPart.lean

--- FLT/for_mathlib/FieldPart.lean
+++ FLT/for_mathlib/FieldPart.lean
@@ -2,17 +2,19 @@ import Mathlib.Tactic
 import Mathlib.FieldTheory.IsSepClosed
 import Mathlib.RingTheory.Algebraic
 
-def div_contain_root_is_field (D : Type*) [Ring D] [DivisionRing D]
+def div_contain_root_is_field (D : Type*)[Ring D][DivisionRing D]
     (p : ℕ) [CharP D p](h : ∀ (a : D), ∃ (m : ℕ), (a ^ p ^ m) ∈ Subring.center D) :
     IsField D where
-  exists_pair_ne := sorry
+  exists_pair_ne := exists_pair_ne D
   mul_comm := sorry
-  mul_inv_cancel := sorry
+  mul_inv_cancel := by 
+    intro x hx ; use x⁻¹ 
+    exact GroupWithZero.mul_inv_cancel x hx
 
 abbrev p_radical_extension (K E: Type*) [Field K] [DivisionRing E] [Algebra K E] (p : ℕ) [CharP K p]
     (_ : p.Prime) := ∀(x : E), ∃(m : ℕ), x ^ p ^ m ∈ (Algebra.ofId K E).range
 
-
+open Polynomial
 lemma findim_divring_over_sep_closed (K : Type*) (D : Type*) [Field K]
     [IsSepClosed K] [DivisionRing D] [Algebra K D] [FiniteDimensional K D]
     (p : ℕ) (hp : p.Prime) [CharP K p]:
@@ -20,6 +22,13 @@ lemma findim_divring_over_sep_closed (K : Type*) (D : Type*) [Field K]
   have alg_ext: ∀(d : D), IsAlgebraic K d := sorry
   have p_rad : p_radical_extension K D p hp := by
     intro d ; let f := minpoly K d
-
+    have hf: ∃(m : ℕ),
+        f ∈ (Algebra.adjoin K {X^p^m} : Subalgebra K K[X]) ∧
+        f ∉ (Algebra.adjoin K {X^p^(m+1)} : Subalgebra K K[X]):= by
+      sorry
+    obtain ⟨m, h1, h2⟩ := hf
+    let g : (Algebra.adjoin K {X^p^m} : Subalgebra K K[X]) := f X
     sorry
+
   sorry
+
