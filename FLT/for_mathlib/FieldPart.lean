b842badc4e24fee99dd38dd26c34ee4825635bc0
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2024-06-02 16:13:44 +0800

    one possible approach

--- FLT/for_mathlib/FieldPart.lean
+++ FLT/for_mathlib/FieldPart.lean
@@ -24,137 +24,156 @@ abbrev fun1 {D : Type*} [DivisionRing D](x : D)(hx : x ≠ 0): D →+* D where
   map_zero' := by simp only [mul_zero, zero_mul]
   map_add' := by simp only ; intro x1 y ; rw [mul_add, add_mul]
 
+set_option maxHeartbeats 800000 in
 theorem division_char_is_commutative {D : Type*} [DivisionRing D] {p : ℕ} [Fact p.Prime] [CharP D p]
     (h : ∀ x : D, ∃ (m : ℕ),  x ^ (p ^ (m + 1)) ∈ Subring.center D) : IsField D where
     exists_pair_ne := by exact exists_pair_ne D
     mul_comm := by
       intro x
-      have eq1 : ∀ (y : D), y * x = x * y := by
-        rw [← Subring.mem_center_iff]
-        by_contra! hx
-        let hx1 := h x
-        cases' hx1 with m hxm
-        let fun1 := fun (a : D) ↦ x * a * x⁻¹
-        let id := fun (a : D) ↦ a
-        have x_neq_0 : x ≠ 0 := by
+      suffices x ∈ Subring.center D by
+        rw [Subring.mem_center_iff] at this
+        intro y ; exact Eq.symm (SemiconjBy.eq (this y))
+      by_contra! hx
+      let hx1 := h x
+      cases' hx1 with m hxm
+      let fun1 : Module.End D D :={
+          toFun := fun a ↦ x * a * x⁻¹
+          map_add' := fun y1 y2 ↦ by simp only ; rw [mul_add, add_mul]
+          map_smul' := fun d y ↦ by simp only [smul_eq_mul, RingHom.id_apply]; sorry
+        }
+      let id : Module.End D D :={
+          toFun := id
+          map_add' := fun x y ↦ by simp only [id_eq]
+          map_smul' := fun d x ↦ by simp only [smul_eq_mul, id_eq, RingHom.id_apply]
+        }
+      have x_neq_0 : x ≠ 0 := by
           intro hx0
           rw [hx0] at hx
           exact hx (Set.zero_mem_center D)
-        have x1 : x⁻¹ * x = 1 := by simp_all
-        have fun_eq1 : fun1^[p ^ (m + 1)] - id = 0 := by
+      have x1 : x⁻¹ * x = 1 := by simp_all
+      have fun_eq1 : fun1^[p ^ (m + 1)] - id = 0 := by
           ext d
           simp only [Pi.sub_apply, Pi.zero_apply]
+          change (fun1.toFun)^[p ^ (m + 1)] d - id.toFun d = 0
           rw [mul_left_right_iterate]
-          simp only [inv_pow]
-          rw [Subring.mem_center_iff] at hxm
+          simp only [inv_pow]; rw [Subring.mem_center_iff] at hxm
           specialize hxm d
-          rw [← hxm, mul_assoc,DivisionRing.mul_inv_cancel, mul_one, sub_self]
+          rw [← hxm, mul_assoc,DivisionRing.mul_inv_cancel, mul_one, id_eq, sub_self]
           apply pow_ne_zero ; rwa [ne_eq]
-        have fun_eq2 : ((fun1 - id)^[p ^ (m + 1)]) = fun1 ^[p ^ (m + 1)] - id := by
+      have fun_eq2 : ((fun1 - id)^[p ^ (m + 1)]) = fun1 ^[p ^ (m + 1)] - id := by
           induction' (m + 1) with n hn
           · ext d
-            simp only [pow_zero, Function.iterate_one, Pi.sub_apply]
+            simp only [pow_zero, Function.iterate_one, LinearMap.sub_apply, Pi.sub_apply]
           · ext d
             rw[pow_add, Function.iterate_mul, Function.iterate_mul, hn]
             simp only [pow_one, Pi.sub_apply]
-            
             sorry
-        rw[fun_eq1] at fun_eq2
-        have fun_eq3 : fun1 - id ≠ 0 := by
-          intro heq
-          have H := congr_fun heq
-          simp only [Pi.sub_apply, Pi.zero_apply, fun1, id] at H
-          apply hx
-          rw [Subring.mem_center_iff]
-          intro a
-          specialize H a
-          rw [← mul_left_inj' x_neq_0, zero_mul, sub_mul, mul_assoc, x1, mul_one, sub_eq_zero] at H
-          exact H.symm
-        have h1 : ∃ l : ℕ, (fun1 - id)^[l] ≠ 0 ∧ ∀ n : ℕ, (fun1 - id)^[n.succ + l] = 0 := by
-          by_contra! h11
-          have h12 := h11 1 fun_eq3
-          sorry
-          ---
-        cases' h1 with l hl
-        rcases hl with ⟨hl1, hl2⟩
-        have h2 : ∃ b : D, (fun1 - id)^[l] b ≠ 0 := by
-          by_contra! hb
-          apply hl1
-          ext b
-          specialize hb b
-          exact hb
-        cases' h2 with b hb
-        set y := (fun1 - id)^[l - 1] b
-        set z := (fun1 - id) y
-        have l1 : l ≥ 1 := by
-          by_contra! hl
-          have l0 : l = 0 := by omega
-          have l1 := hl2 0
-          rw [l0] at hl1 l1
-          exact fun_eq3 l1
-        have hz : z ≠ 0 := by
-          intro hzq
-          simp [z, y] at hzq
-          apply hb
-          have hz1 : (fun1 - id) ((fun1 - id)^[l - 1] b) =
-            fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b) := by simp only [Pi.sub_apply]
-          rw [← hz1, ← Function.iterate_succ_apply' (f := fun1 - id) (n := l - 1)] at hzq
-          rwa [show l = (l-1).succ by omega]
-        have hz1 : fun1 z = z := by
-          rw[← sub_eq_zero]
-          simp [z, y] at *
-          rw[show fun1 (fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b))
-            - (fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b))
-            = (fun1 - id) (fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b)) by simp,
-            show fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b)
-            = (fun1 - id) ((fun1 - id)^[l - 1] b) by simp only [Pi.sub_apply],
-            ← Function.iterate_succ_apply' (f := fun1 - id) (n := l - 1),
-            ← Function.iterate_succ_apply' (f := fun1 - id) (n := (l - 1).succ),
-            show (l - 1).succ.succ = l + 1 by omega]
-          obtain funl1 := hl2 0
-          rw [show 0 + 1 = 1 by omega] at funl1
-          rw [add_comm]
-          calc
-          _ = (0 : D → D) b := by rw [funl1]
-          _ = 0 := by simp
-        set w := z⁻¹ * y
-        have hw1 : fun1 w = w + 1 := by
-          simp [fun1, w] at *
-          rw [← mul_assoc, mul_assoc, show y * x⁻¹ = 1 * (y * x⁻¹)  by rw[one_mul], ← x1, ← mul_assoc,
-            ← mul_assoc, ← mul_assoc, mul_assoc, mul_assoc, x1]
-          nth_rewrite 2 [← mul_assoc]
-          calc
-          _ = x * z⁻¹ * x⁻¹ * fun1 y := by aesop
-          _ = (x * z * x⁻¹)⁻¹ * fun1 y := by group
-          _ = z⁻¹ * fun1 y := by rw [hz1]
-          _ = z⁻¹ * (y + z) := by aesop
-          _ = z⁻¹ * y + z⁻¹ * z := by exact LeftDistribClass.left_distrib z⁻¹ y z
-          _ = z⁻¹ * y + 1 := by rw [show z⁻¹ * z = 1 by simp_all]
-        cases' h w with wm hwm
-        have final : w ^ p ^ (wm + 1) = w ^ p ^ (wm + 1) + 1 := by
-          calc
-          _ = w ^ p ^ (wm + 1) * 1 := by aesop
-          _ = w ^ p ^ (wm + 1) * x * x⁻¹ := by aesop
-          _ = x * w ^ p ^ (wm + 1) * x⁻¹ := by
-            rw [Subring.mem_center_iff] at hwm
-            specialize hwm x
-            rw [← hwm]
-          _ = (x * w * x⁻¹) ^ p ^ (wm + 1) := by
-            set q := p ^ (wm + 1)
-            induction' q with q1 hq1
-            · simp only [pow_zero, mul_one]
-              exact DivisionRing.mul_inv_cancel x x_neq_0
-            · nth_rewrite 2 [pow_add]
-              rw [pow_one, ← mul_assoc, ← mul_assoc, ← hq1]
-              nth_rewrite 4 [mul_assoc]
-              rw [x1]
-              nth_rewrite 3 [mul_assoc, mul_assoc]
-              rw [one_mul, pow_add, pow_one]
-          _ = (w + 1) ^ p ^ (wm + 1) := by aesop
-          _ = w ^ p ^ (wm + 1) + 1 := by rw [add_pow_char_pow_of_commute (h := Commute.one_right w), one_pow]
-        aesop
-      intro y
-      exact (eq1 y).symm
+      sorry
+      -- have eq1 : ∀ (y : D), y * x = x * y := by
+
+
+
+
+      --    --fun (a : D) ↦ a
+
+
+
+
+      --   rw[fun_eq1] at fun_eq2
+      --   -- have fun_eq3 : fun1 - id ≠ 0 := by
+      --   --   intro heq
+      --   --   have H := congr_fun heq
+      --   --   simp only [Pi.sub_apply, Pi.zero_apply, fun1, id] at H
+      --   --   apply hx
+      --   --   rw [Subring.mem_center_iff]
+      --   --   intro a
+      --   --   specialize H a
+      --   --   rw [← mul_left_inj' x_neq_0, zero_mul, sub_mul, mul_assoc, x1, mul_one, sub_eq_zero] at H
+      --   --   exact H.symm
+      --   have h1 : ∃ l : ℕ, (fun1 - id)^[l] ≠ 0 ∧ ∀ n : ℕ, (fun1 - id)^[n.succ + l] = 0 := by
+      --     by_contra! h11
+      --     sorry
+      --     ---
+      --   cases' h1 with l hl
+      --   rcases hl with ⟨hl1, hl2⟩
+      --   have h2 : ∃ b : D, (fun1 - id)^[l] b ≠ 0 := by
+      --     by_contra! hb
+      --     apply hl1
+      --     ext b
+      --     specialize hb b
+      --     exact hb
+      --   cases' h2 with b hb
+      --   set y := (fun1 - id)^[l - 1] b
+      --   set z := (fun1 - id) y
+      --   have l1 : l ≥ 1 := by
+      --     by_contra! hl
+      --     have l0 : l = 0 := by omega
+      --     have l1 := hl2 0
+      --     rw [l0] at hl1 l1
+      --     aesop?
+      --   have hz : z ≠ 0 := by
+      --     intro hzq
+      --     simp [z, y] at hzq
+      --     apply hb
+      --     have hz1 : (fun1 - id) ((fun1 - id)^[l - 1] b) =
+      --       fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b) := by simp only [Pi.sub_apply]
+      --     rw [← hz1, ← Function.iterate_succ_apply' (f := fun1 - id) (n := l - 1)] at hzq
+      --     rwa [show l = (l-1).succ by omega]
+      --   have hz1 : fun1 z = z := by
+      --     rw[← sub_eq_zero]
+      --     simp [z, y] at *
+      --     rw[show fun1 (fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b))
+      --       - (fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b))
+      --       = (fun1 - id) (fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b)) by simp,
+      --       show fun1 ((fun1 - id)^[l - 1] b) - id ((fun1 - id)^[l - 1] b)
+      --       = (fun1 - id) ((fun1 - id)^[l - 1] b) by simp only [Pi.sub_apply],
+      --       ← Function.iterate_succ_apply' (f := fun1 - id) (n := l - 1),
+      --       ← Function.iterate_succ_apply' (f := fun1 - id) (n := (l - 1).succ),
+      --       show (l - 1).succ.succ = l + 1 by omega]
+      --     obtain funl1 := hl2 0
+      --     rw [show 0 + 1 = 1 by omega] at funl1
+      --     rw [add_comm]
+      --     calc
+      --     _ = (0 : D → D) b := by rw [funl1]
+      --     _ = 0 := by simp
+      --   set w := z⁻¹ * y
+      --   have hw1 : fun1 w = w + 1 := by
+      --     simp [fun1, w] at *
+      --     rw [← mul_assoc, mul_assoc, show y * x⁻¹ = 1 * (y * x⁻¹)  by rw[one_mul], ← x1, ← mul_assoc,
+      --       ← mul_assoc, ← mul_assoc, mul_assoc, mul_assoc, x1]
+      --     nth_rewrite 2 [← mul_assoc]
+      --     calc
+      --     _ = x * z⁻¹ * x⁻¹ * fun1 y := by aesop
+      --     _ = (x * z * x⁻¹)⁻¹ * fun1 y := by group
+      --     _ = z⁻¹ * fun1 y := by rw [hz1]
+      --     _ = z⁻¹ * (y + z) := by aesop
+      --     _ = z⁻¹ * y + z⁻¹ * z := by exact LeftDistribClass.left_distrib z⁻¹ y z
+      --     _ = z⁻¹ * y + 1 := by rw [show z⁻¹ * z = 1 by simp_all]
+      --   cases' h w with wm hwm
+      --   have final : w ^ p ^ (wm + 1) = w ^ p ^ (wm + 1) + 1 := by
+      --     calc
+      --     _ = w ^ p ^ (wm + 1) * 1 := by aesop
+      --     _ = w ^ p ^ (wm + 1) * x * x⁻¹ := by aesop
+      --     _ = x * w ^ p ^ (wm + 1) * x⁻¹ := by
+      --       rw [Subring.mem_center_iff] at hwm
+      --       specialize hwm x
+      --       rw [← hwm]
+      --     _ = (x * w * x⁻¹) ^ p ^ (wm + 1) := by
+      --       set q := p ^ (wm + 1)
+      --       induction' q with q1 hq1
+      --       · simp only [pow_zero, mul_one]
+      --         exact DivisionRing.mul_inv_cancel x x_neq_0
+      --       · nth_rewrite 2 [pow_add]
+      --         rw [pow_one, ← mul_assoc, ← mul_assoc, ← hq1]
+      --         nth_rewrite 4 [mul_assoc]
+      --         rw [x1]
+      --         nth_rewrite 3 [mul_assoc, mul_assoc]
+      --         rw [one_mul, pow_add, pow_one]
+      --     _ = (w + 1) ^ p ^ (wm + 1) := by aesop
+      --     _ = w ^ p ^ (wm + 1) + 1 := by rw [add_pow_char_pow_of_commute (h := Commute.one_right w), one_pow]
+      --   aesop
+      -- intro y
+      -- exact (eq1 y).symm
     mul_inv_cancel := by
       intro a ha ; use a⁻¹
       exact DivisionRing.mul_inv_cancel a ha
