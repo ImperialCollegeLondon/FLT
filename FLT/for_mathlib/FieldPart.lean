0d45ddd8819fc71e4ac9abf8bbcf1fc0c985f03d
Whysoserioushah <109107491+Whysoserioushah@users.noreply.github.com>
2024-06-09 20:30:33 +0800

    some cleanup and more efficient lemma 3

--- FLT/for_mathlib/FieldPart.lean
+++ FLT/for_mathlib/FieldPart.lean
@@ -5,9 +5,6 @@ import Mathlib.RingTheory.LittleWedderburn
 import Mathlib.Algebra.Field.Basic
 open scoped Classical
 
--- variable (D : Type*) [DivisionRing D]
-
-
 theorem mul_left_right_iterate {G : Type*} [Monoid G] (a b : G) (n : ℕ) : (a * · * b)^[n] =
     (a ^ n * · * b ^ n) := by
   induction' n with n hn
@@ -93,40 +90,31 @@ lemma conj_char_pow_eq_one (x : D) (hx' : x ≠ 0)
     (conj x) ^ p ^ (m + 1) - 1 = 0 := by
   ext d ; simp only [LinearMap.sub_apply, LinearMap.one_apply, LinearMap.zero_apply]
   rw [conj_pow_eq]; simp only [LinearMap.coe_mk, AddHom.coe_mk]
-  have := (Subring.mem_center_iff (R := D)).1 hx d
-  rw [← this, mul_assoc, mul_inv_cancel]
-  · simp only [mul_one, sub_self]
-  · exact pow_ne_zero (p ^ (m + 1)) hx'
+  rw [← ((Subring.mem_center_iff (R := D)).1 hx d), mul_assoc, mul_inv_cancel (by aesop)]
+  simp only [mul_one, sub_self]
 
-lemma conj_compose (n : ℕ) (x y : D): (conj x - 1) (((conj x - 1) ^ n) y) =
+lemma conj_one_compose (n : ℕ) (x y : D): (conj x - 1) (((conj x - 1) ^ n) y) =
     ((conj x - 1) ^ (n + 1)) y := by
-  symm; rw [add_comm, pow_add, pow_one]; simp
+  rw [add_comm, pow_add, pow_one]; simp
 
 theorem division_char_is_commutative {D : Type*} [DivisionRing D] {p : ℕ} [Fact p.Prime] [CharP D p]
     (h : ∀ x : D, ∃ (m : ℕ),  x ^ (p ^ (m + 1)) ∈ Subring.center D) : IsField D where
     exists_pair_ne := by exact exists_pair_ne D
     mul_comm := by
       intro x
-      suffices ∀ (y : D), y * x = x * y by
-        intro y ; exact (this y).symm
+      suffices ∀ (y : D), y * x = x * y by intro y ; exact (this y).symm
       obtain ⟨m, hm⟩ := h x
       by_contra! hy
       cases' hy with y hy
-      have hx : x ≠ 0 := by
-        intro hx
-        simp_all only [mul_zero, zero_mul, ne_eq, not_true_eq_false, exists_const]
-      have x1 : x⁻¹ * x = 1 := by simp_all
-      have x2 : x * x⁻¹ = 1 := by simp_all
+      have hx : x ≠ 0 := by intro hx; simp_all
       have ineq1 : (conj x) - 1 ≠ 0 := by
-        intro h1
-        obtain h1 := (DFunLike.ext_iff.1 h1) y
+        intro h1; obtain h1 := (DFunLike.ext_iff.1 h1) y
         simp only [LinearMap.sub_apply, LinearMap.coe_mk, AddHom.coe_mk, LinearMap.one_apply,
           LinearMap.zero_apply, sub_eq_zero] at h1
-        apply hy; rw [← h1, mul_assoc, x1, h1, mul_one]
+        apply hy; rw [← h1, mul_assoc, (inv_mul_cancel hx), h1, mul_one]
       obtain ⟨l, hl, hn⟩ := upper_bound p x hx ⟨m, hm⟩
       have h1 : ∃ b : D, (((conj x) - 1) ^ l) b ≠ 0 := by
-        by_contra! hb;
-        exact hl (DFunLike.ext_iff.2 hb)
+        by_contra! hb; exact hl (DFunLike.ext_iff.2 hb)
       cases' h1 with b hb
       set z := (((conj x) - 1)^ (l - 1)) b
       set w := ((conj x) - 1) z
@@ -136,13 +124,12 @@ theorem division_char_is_commutative {D : Type*} [DivisionRing D] {p : ℕ} [Fac
         have l1 := hn 0
         rw [l0, add_zero, zero_add] at l1; exact ineq1 l1
       have hw0 : w ≠ 0 := by
-        intro hw
-        simp only [w, z] at hw; apply hb
-        rw [show l = 1 + (l - 1) by omega, pow_add] ; simp only [pow_one, LinearMap.mul_apply, hw]
+        intro hw; simp only [w, z] at hw; apply hb
+        rw [show l = 1 + (l - 1) by omega, pow_add] ; simp [hw]
       have hw : (conj x) w = w := by
         rw [← sub_eq_zero]; nth_rw 2 [show (w = (1 : Module.End ℤ D) w) by rw [LinearMap.one_apply]]
         rw [← LinearMap.sub_apply]; simp only [w, z]
-        rw [conj_compose (l - 1) x b, show (l - 1) + 1 = l by omega, conj_compose,
+        rw [conj_one_compose (l - 1) x b, show (l - 1) + 1 = l by omega, conj_one_compose,
           ← zero_add (l + 1), ← add_assoc]
         exact DFunLike.ext_iff.1 (hn 0) b
       set q := w⁻¹ * z
@@ -151,33 +138,26 @@ theorem division_char_is_commutative {D : Type*} [DivisionRing D] {p : ℕ} [Fac
         simp only [LinearMap.coe_mk, AddHom.coe_mk, q]
         simp only [LinearMap.coe_mk, AddHom.coe_mk] at hw h1
         have : x * w⁻¹ * x⁻¹ = w⁻¹ := by nth_rewrite 2 [← hw] ; group
-        nth_rewrite 1 [← one_mul z, ← x1]
+        nth_rewrite 1 [← one_mul z, ← (inv_mul_cancel hx)]
         rw [← mul_assoc, ← mul_assoc, ← mul_assoc, this, mul_assoc, mul_assoc]
-        nth_rewrite 2 [← mul_assoc]
-        rw [h1, mul_add, inv_mul_cancel hw0]
+        nth_rewrite 2 [← mul_assoc]; simp only [h1, mul_add, (inv_mul_cancel hw0)]
       cases' h q with qm hq
       have final : q ^ p ^ (qm + 1) = q ^ p ^ (qm + 1) + 1 := by
-        nth_rw 1 [← mul_one (q ^ p ^ (qm + 1)),← x2,← mul_assoc,← (Subring.mem_center_iff.1 hq) x]
+        nth_rw 1 [← mul_one (q ^ p ^ (qm + 1)), ← (mul_inv_cancel hx), ← mul_assoc,
+          ← (Subring.mem_center_iff.1 hq) x]
         calc
         _ = (x * q * x⁻¹) ^ p ^ (qm + 1) := by
           set e := p ^ (qm + 1)
           induction' e with e he
-          · simp only [pow_zero, mul_one]
-            exact DivisionRing.mul_inv_cancel x hx
-          · nth_rewrite 2 [pow_add]
-            rw [ pow_add, pow_one, pow_one, ← he]
-            nth_rewrite 2 [← one_mul (a := q)]
-            rw [← x1, ← mul_assoc, ← mul_assoc]
-            nth_rewrite 2 [← mul_assoc, ← mul_assoc, ← mul_assoc] ; rfl
+          · simp only [pow_zero, mul_one, (DivisionRing.mul_inv_cancel x hx)]
+          · rw [pow_add, pow_add, pow_one, pow_one, ← he]
+            nth_rewrite 2 [← one_mul (a := q)]; rw [← (inv_mul_cancel hx)]; group
         _ = (q + 1) ^ p ^ (qm + 1) := by
           simp only [LinearMap.coe_mk, AddHom.coe_mk] at hq_add ; rw[hq_add]
         _ = q ^ p ^ (qm + 1) + 1 := by
           rw [add_pow_char_pow_of_commute (h := Commute.one_right q), one_pow]
-
       simp only [self_eq_add_right, one_ne_zero] at final
-    mul_inv_cancel := by
-      intro a ha ; use a⁻¹
-      exact DivisionRing.mul_inv_cancel a ha
+    mul_inv_cancel := by intro a ha ; exact ⟨a⁻¹, mul_inv_cancel ha⟩
 
 
 abbrev p_radical_extension (K E: Type*) [Field K] [DivisionRing E] [Algebra K E] (p : ℕ) [CharP K p]
@@ -203,16 +183,6 @@ theorem fin_version [Finite K] [Algebra K D] [FiniteDimensional K D] :
   exact Finite.isDomain_to_isField D |>.mul_comm x y
 
 variable (R : Type*) [Semiring R]
-
-abbrev unit_group : Group Rˣ where
-  mul_assoc _ _ _ := mul_assoc _ _ _
-  one_mul := _
-  mul_one := _
-  npow_zero _ := pow_zero _
-  npow_succ _ _ := pow_succ _ _
-  zpow_zero' _ := pow_zero _
-  zpow_succ' _ _ := pow_succ _ _
-  mul_left_inv _ := mul_left_inv _
 
 variable [Algebra K D] [FiniteDimensional K D] [CharP D p]
 
@@ -360,7 +330,7 @@ lemma edison_lemma3 (d : D) {hff : f = minpoly K d}{m : ℕ}(g : K[X])
         simp only [hg, mul_comp]
       rw [hq, hff] at this
       have irr_f := minpoly.irreducible (A := K) (x := d) (Algebra.IsIntegral.isIntegral d)
-      obtain ⟨hf1, hf2⟩ := irr_f ; obtain ⟨ha, hb⟩ := hab
+      obtain ⟨-, hf2⟩ := irr_f ; obtain ⟨ha, hb⟩ := hab
       specialize hf2 (a.comp (X ^ p ^ (m - 1))) (b.comp (X ^ p ^ (m - 1))) this
       cases' hf2 with hf2 hf2
       · have ha' : IsUnit a := by
@@ -442,9 +412,8 @@ lemma findim_divring_over_sep_closed [Infinite K] (D : Type*)
       rw [hg, hf'] at this
       simp only [monic_X_pow, Monic.leadingCoeff, one_pow, mul_one] at this
       exact this.symm
-    have g_irr : Irreducible g := by
-      have : f = minpoly K d := rfl
-      exact edison_lemma3 p g hg (hff := this)
+
+    have g_irr : Irreducible g := edison_lemma3 p g hg (hff := rfl) --lemma 3
 
     have g_nin : g ∉ Algebra.adjoin K {X^p} := by  -- lemma 4
       intro h
@@ -483,7 +452,7 @@ lemma findim_divring_over_sep_closed [Infinite K] (D : Type*)
           exact_mod_cast hf'
 
     have g_sep : Separable g := separable_or p g_irr |>.elim id $ by
-      rintro ⟨h1, ⟨g, hg, rfl⟩⟩
+      rintro ⟨h1, ⟨g, -, rfl⟩⟩
       exfalso
       refine g_nin ?_
       rw [expand_eq_comp_X_pow]
