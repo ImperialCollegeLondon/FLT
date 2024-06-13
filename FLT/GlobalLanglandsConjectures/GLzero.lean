c8e1a287ef99a14b1fe9c46ef1a0c3613dfb02f0
Jonas Bayer <jonas.bayer999@gmail.com>
2024-06-13 15:36:21 +0200

    Classification for GL0, parts of surjectivity

--- FLT/GlobalLanglandsConjectures/GLzero.lean
+++ FLT/GlobalLanglandsConjectures/GLzero.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Kevin Buzzaed. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard
+Authors: Kevin Buzzard, Jonas Bayer
 -/
 import Mathlib.RingTheory.DedekindDomain.Ideal
 import Mathlib.RingTheory.IntegralRestrict
@@ -56,7 +56,53 @@ namespace GL0
 
 -- the weakest form of the classification theorem
 theorem classification_aux : ∀ (ρ : Weight 0),
-    Function.Bijective (fun f ↦ f 1 : AutomorphicFormForGLnOverQ 0 ρ → ℂ) := sorry
+    Function.Bijective (fun (f : AutomorphicFormForGLnOverQ 0 ρ) ↦ (f 1 : ℂ)) := by
+    intro ρ
+    rw [Function.Bijective]
+    apply And.intro
+    · sorry
+    · intro c
+      let automorphicForm: AutomorphicFormForGLnOverQ 0 ρ := {
+        toFun := fun _ => c,
+        is_smooth := {
+          continuous := by continuity
+          loc_cst := by
+            rw [IsLocallyConstant]
+            aesop
+          smooth := by simp [smooth_const]
+        }
+        is_periodic := by simp
+        is_slowly_increasing := by
+          intros x
+          exact {
+          bounded_by := by
+            simp
+            apply Exists.intro (Complex.abs c)
+            apply Exists.intro 0
+            simp
+        }
+        is_finite_cod := by
+          intros x
+          rw [FiniteDimensional]
+          exact {
+            out := by sorry
+          }
+        has_finite_level := by
+          let U : Subgroup (GL (Fin 0) (DedekindDomain.FiniteAdeleRing ℤ ℚ)) := {
+            carrier := {1},
+            one_mem' := by simp,
+            mul_mem' := by simp
+            inv_mem' := by simp
+          }
+          apply Exists.intro U
+          exact {
+              is_open := by simp
+              is_compact := by aesop
+              finite_level := by simp
+          }
+      }
+      apply Exists.intro automorphicForm
+      simp
 
 -- Let's write down an inverse
 -- For general n, it will only work for ρ the trivial representation, but we didn't
