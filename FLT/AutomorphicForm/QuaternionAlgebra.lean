4acc7cfc525b253ab0248d0938c63c3811c37aaf
monnerjahnl0 <s6lumonn@uni-bonn.de>
2024-06-20 17:57:10 +0200

    Add authors

--- FLT/AutomorphicForm/QuaternionAlgebra.lean
+++ FLT/AutomorphicForm/QuaternionAlgebra.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard
+Authors: Kevin Buzzard, Ludwig Monnerjahn, Hannah Scholz
 -/
 import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup
 import Mathlib.Geometry.Manifold.Instances.UnitsOfNormedAlgebra
