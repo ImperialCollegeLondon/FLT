c78efcb020b7e791954f4d38c3d539a0780b14d6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-11-06 11:26:45 +0000

    more work on the adele miniproject.

--- FLT/DedekindDomain/FiniteAdeleRing/BaseChange.lean
+++ FLT/DedekindDomain/FiniteAdeleRing/BaseChange.lean
@@ -1,4 +1,6 @@
-import Mathlib -- **TODO** fix when finished or if `exact?` is too slow
+--import Mathlib -- **TODO** fix when finished or if `exact?` is too slow
+import Mathlib.RingTheory.DedekindDomain.FiniteAdeleRing
+import Mathlib.NumberTheory.NumberField.Basic
 /-!
 
 # Base change of adele rings.
@@ -6,8 +8,9 @@ import Mathlib -- **TODO** fix when finished or if `exact?` is too slow
 If `A` is a Dedekind domain with field of fractions `K`, if `L/K` is a finite separable
 extension and if `B` is the integral closure of `A` in `L`, then `B` is also a Dedekind
 domain. Hence the rings of finite adeles `𝔸_K^∞` and `𝔸_L^∞` (defined using `A` and `B`)
-are defined, and there's a canonical map `L ⊗[K] 𝔸_K^∞ → 𝔸_L^∞`. The main theorem
-of this file is that it's an isomorphism. This result should be in mathlib.
+are defined. In this file we define the natural `K`-algebra map `𝔸_K^∞ → 𝔸_L^∞` and
+the natural `L`-algebra map `𝔸_K^∞ ⊗[K] L → 𝔸_L^∞`, and show that the latter map
+is an isomorphism.
 
 -/
 
@@ -34,11 +37,10 @@ variable [Algebra.IsIntegral A B]
 
 -- I can't find in mathlib the assertion that B is a finite A-moduie.
 -- It should follow from L/K finite.
+example : Module.Finite A B := by sorry -- I assume this is correct
 
 /-
-Conjecture: in this generality there's a natural isomorphism `L ⊗[K] 𝔸_K^∞ → 𝔸_L^∞`
-I've not found a reference for this but we can try following the usual
-references (which work for global fields). This is what we do below.
+In this generality there's a natural isomorphism `L ⊗[K] 𝔸_K^∞ → 𝔸_L^∞` .
 
 Update: Javier suggests p21 of
 https://math.berkeley.edu/~ltomczak/notes/Mich2022/LF_Notes.pdf
@@ -56,30 +58,19 @@ def comap (w : HeightOneSpectrum B) : HeightOneSpectrum A where
   isPrime := Ideal.comap_isPrime (algebraMap A B) w.asIdeal
   ne_bot := mt Ideal.eq_bot_of_comap_eq_bot w.ne_bot
 
--- lemma: pushforward of pullback is P^(ram index)
-lemma map_comap (w : HeightOneSpectrum B) :
-    (w.comap A).asIdeal.map (algebraMap A B) =
-    w.asIdeal ^ ((Ideal.ramificationIdx (algebraMap A B) (comap A w).asIdeal w.asIdeal)) := by
-  -- This must be standard? Maybe a hole in the library for Dedekind domains
-  -- or maybe I just missed it?
-  sorry
-
 open scoped algebraMap
 
-
-
 -- Need to know how the valuation `w` and its pullback are related on elements of `K`.
-def valuation_comap (w : HeightOneSpectrum B) (x : K) :
+lemma valuation_comap (w : HeightOneSpectrum B) (x : K) :
     (comap A w).valuation x =
     w.valuation (algebraMap K L x) ^
     (Ideal.ramificationIdx (algebraMap A B) (comap A w).asIdeal w.asIdeal) := by
-  -- This should follow from map_comap?
   sorry
 
 -- Say w is a finite place of L lying above v a finite places
 -- of K. Then there's a ring hom K_v -> L_w.
 variable {B L} in
-noncomputable def adicCompletion_comap (w : HeightOneSpectrum B) :
+noncomputable def adicCompletion_comap_ringHom (w : HeightOneSpectrum B) :
     (adicCompletion K (comap A w)) →+* (adicCompletion L w) :=
   letI : UniformSpace K := (comap A w).adicValued.toUniformSpace;
   letI : UniformSpace L := w.adicValued.toUniformSpace;
@@ -106,12 +97,12 @@ noncomputable local instance (w : HeightOneSpectrum B) :
   (algebraMap L (adicCompletion L w)).comp (algebraMap K L)
 
 variable {B L} in
-noncomputable def adicCompletion_alg_comap (w : HeightOneSpectrum B) :
+noncomputable def adicCompletion_comap_algHom (w : HeightOneSpectrum B) :
     letI : Algebra K (adicCompletion L w) := RingHom.toAlgebra <|
   (algebraMap L (adicCompletion L w)).comp (algebraMap K L);
     letI : Module K (adicCompletion L w) := Algebra.toModule
     (HeightOneSpectrum.adicCompletion K (comap A w)) →ₗ[K] (HeightOneSpectrum.adicCompletion L w) :=
-  sorry -- use `adicCompletion_comap` and prove it's a K-algebra homomorphism
+  sorry -- use `adicCompletion_comap_ringHom` and prove it's a K-algebra homomorphism
 
 end IsDedekindDomain.HeightOneSpectrum
 
