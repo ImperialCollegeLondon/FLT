fd4d33862bffa44cdc1fce2744ed60a9bdb935a3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-11-16 16:29:04 +0000

    doc typos

--- FLT/GlobalLanglandsConjectures/GLnDefs.lean
+++ FLT/GlobalLanglandsConjectures/GLnDefs.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 Kevin Buzzaed. All rights reserved.
+Copyright (c) 2024 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard, Jonas Bayer, Mario Carneiro
 -/
