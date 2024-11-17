bbee48f3b546d6f78f345971cf72233cbefec4f9
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-11-17 00:55:59 +0000

    bump mathlib (#220)

--- FLT/ForMathlib/MiscLemmas.lean
+++ FLT/ForMathlib/MiscLemmas.lean
@@ -69,6 +69,8 @@ variable {A : Type*} [SMul R A]
 variable {S : Type*} [τS : TopologicalSpace S] {f : S → R} (hf : Continuous f)
 variable {B : Type*} [SMul S B]
 
+open Topology
+
 -- note: use convert not exact to ensure typeclass inference doesn't try to find topology on B
 theorem induced_continuous_smul [τA : TopologicalSpace A] [ContinuousSMul R A] (g : B →ₑ[f] A)
     (hf : Continuous f) : @ContinuousSMul S B _ _ (TopologicalSpace.induced g τA) := by
