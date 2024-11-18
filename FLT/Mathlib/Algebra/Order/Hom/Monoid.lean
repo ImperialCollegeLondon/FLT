f75dbe67ab241fce165e21faada98a6ae697c87f
Yaël Dillies <yael.dillies@gmail.com>
2024-11-18 12:51:02 +0000

    reduce diff

--- FLT/Mathlib/Algebra/Order/Hom/Monoid.lean
+++ FLT/Mathlib/Algebra/Order/Hom/Monoid.lean
@@ -1,9 +1,5 @@
 import Mathlib.Algebra.Order.Hom.Monoid
 
-/-!
-This is https://github.com/leanprover-community/mathlib4/pull/18986
--/
-
 namespace OrderMonoidIso
 variable {α β : Type*} [OrderedCommMonoid α] [OrderedCommMonoid β]
 
@@ -12,9 +8,3 @@ def symm (e : α ≃*o β) : β ≃*o α where
   map_le_map_iff' := e.toOrderIso.symm.map_rel_iff
 
 end OrderMonoidIso
-
-/-- Any ordered group is isomorphic to the units of itself adjoined with `0`. -/
-@[simps! toFun]
-def OrderMonoidIso.unitsWithZero {α : Type*} [Group α] [Preorder α] : (WithZero α)ˣ ≃*o α where
-  toMulEquiv := WithZero.unitsWithZeroEquiv
-  map_le_map_iff' {a b} := by simp [WithZero.unitsWithZeroEquiv]; sorry
