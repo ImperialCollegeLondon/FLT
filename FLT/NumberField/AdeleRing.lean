1e397f26febe178adad2f581a2faf3ba0600aeb5
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-11-25 18:28:47 +0000

    add Samuel's work (#233)

--- FLT/NumberField/AdeleRing.lean
+++ FLT/NumberField/AdeleRing.lean
@@ -25,15 +25,33 @@ section Discrete
 
 open NumberField DedekindDomain
 
-def V: Set (InfiniteAdeleRing ℚ) := {f | ∀ v, f v ∈ (Metric.ball 0 1)}
-def W: Set (FiniteAdeleRing (𝓞 ℚ) ℚ) :=
-  {f | ∀ v , f v ∈ IsDedekindDomain.HeightOneSpectrum.adicCompletionIntegers ℚ v}
-def U: Set (NumberField.AdeleRing ℚ) := V ×ˢ W
+-- Should surely be in mathlib
+abbrev Rat.infinitePlace : InfinitePlace ℚ where
+  val := {
+    toFun := fun q ↦ |(q : ℝ)|
+    map_mul' := fun x y ↦ by
+      simp only [cast_mul]
+      exact abs_mul (x : ℝ) y
+    nonneg' := by
+      intro x
+      simp only [abs_nonneg]
+    eq_zero' := by
+      intro x
+      simp
+    add_le' := by
+      intro x y
+      simp only [cast_add]
+      exact abs_add_le (x : ℝ) y
+  }
+  property := ⟨Rat.castHom ℂ, by
+    ext x
+    simp
+    ⟩
 
 theorem Rat.AdeleRing.zero_discrete : ∃ U : Set (AdeleRing ℚ),
     IsOpen U ∧ (algebraMap ℚ (AdeleRing ℚ)) ⁻¹' U = {0} := by
-  use U
-  unfold U
+  use {f | ∀ v, f v ∈ (Metric.ball 0 1)} ×ˢ
+    {f | ∀ v , f v ∈ IsDedekindDomain.HeightOneSpectrum.adicCompletionIntegers ℚ v}
   refine ⟨sorry, ?_⟩
   apply subset_antisymm
   · intro x hx
@@ -46,23 +64,34 @@ theorem Rat.AdeleRing.zero_discrete : ∃ U : Set (AdeleRing ℚ),
     rw [Set.mem_prod] at hx
     obtain ⟨h1, h2⟩ := hx
     dsimp only at h1 h2
-    unfold V at h1
-    simp only [Metric.mem_ball, dist_zero_right, Set.mem_setOf_eq,
-      InfiniteAdeleRing.algebraMap_apply, UniformSpace.Completion.norm_coe] at h1
+    simp only [Metric.mem_ball, dist_zero_right, Set.mem_setOf_eq, InfiniteAdeleRing.algebraMap_apply, UniformSpace.Completion.norm_coe] at h1
+    simp only [Set.mem_setOf_eq] at h2
+    specialize h1 Rat.infinitePlace
+    simp at h1
+    change (|(x:ℝ)|) < 1 at h1
+    norm_cast at h1
+    have := padicNorm.not_int_of_not_padic_int
+    have intx: ∃ (y:ℤ), y = x
+    · by_contra h
+      push_neg at h
+      sorry
     sorry
   · intro x
     simp only [Set.mem_singleton_iff, Set.mem_preimage]
     rintro rfl
     simp only [map_zero]
-    suffices (0,0) ∈ V ×ˢ W by simpa
+    change (0, 0) ∈ _
     simp only [Prod.mk_zero_zero, Set.mem_prod, Prod.fst_zero, Prod.snd_zero]
     constructor
-    · sorry
-    · unfold W
-      simp only [Set.mem_setOf_eq]
+    · simp only [Metric.mem_ball, dist_zero_right, Set.mem_setOf_eq]
+      intro v
+      have : ‖(0:InfiniteAdeleRing ℚ) v‖ = 0
+      · simp only [norm_eq_zero]
+        rfl
+      simp [this, zero_lt_one]
+    · simp only [Set.mem_setOf_eq]
       intro v
       apply zero_mem
-
 
 theorem Rat.AdeleRing.discrete : ∀ q : ℚ, ∃ U : Set (AdeleRing ℚ),
     IsOpen U ∧ (algebraMap ℚ (AdeleRing ℚ)) ⁻¹' U = {q} := sorry
