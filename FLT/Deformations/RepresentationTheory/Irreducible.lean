3a247ee54b92ecca2664462a8e46fd43668fd119
Javier López-Contreras <javierlc2000@gmail.com>
2024-12-18 08:56:42 +0000

    fixes PR comments

--- FLT/Deformations/RepresentationTheory/Irreducible.lean
+++ FLT/Deformations/RepresentationTheory/Irreducible.lean
@@ -9,11 +9,26 @@ variable {k : Type*} [Field k]
 
 variable {W : Type*} [AddCommMonoid W] [Module k W]
 
-class Irreducible (ρ : Representation k G W) : Prop where
+/-!
+  IsIrreducible predicates that a given Representation ρ is irreducible (also known as simple),
+  meaning that any subrepresentation must be either the full one (⊤) or zero (⊥)
+
+  This notion is only well behaved when the representation is over a field k. If it were defined over
+  a ring A with a nontrivial ideal J, the subrepresentation JW would be a non trivial subrepresentation,
+  so ρ would never be irreducible.
+-/
+class IsIrreducible (ρ : Representation k G W) : Prop where
   irreducible : IsSimpleOrder (Subrepresentation ρ)
 
-class AbsolutelyIrreducible (ρ : Representation k G W) : Prop where
-  absolutelyIrreducible : ∀ k', ∀ _ : Field k', ∀ _ : Algebra k k',
-    ∀ _ : Function.Injective (algebraMap k k'), Irreducible (k' ⊗ᵣ' ρ)
+/-!
+  IsAbsolutelyIrreducible predicates that a given Representation ρ over a field k
+  is absolutely irreducible, meaning that all the possible base change extensions are irreducible.
+
+  This notion is only well behaved when the representation is over a field k. If it were defined over
+  a ring A with a nontrivial ideal J, the subrepresentation JW would be a non trivial subrepresentation,
+  so ρ would never be absolutely irreducible.
+-/
+class IsAbsolutelyIrreducible (ρ : Representation k G W) : Prop where
+  absolutelyIrreducible : ∀ k', ∀ _ : Field k', ∀ _ : Algebra k k', IsIrreducible (k' ⊗ᵣ' ρ)
 
 end Representation
--- FLT/Mathlib/RepresentationTheory/Basic.lean
+++ FLT/Mathlib/RepresentationTheory/Basic.lean
@@ -13,28 +13,26 @@ variable (ρ : Representation R G V) (𝓑 : Basis ι R V)
 
 omit [Module.Free R V] [Module.Finite R V] in
 @[simp]
-lemma comp_eq_mul (g h : G): ρ g ∘ₗ ρ h = ρ (g * h) := by
-  simp_all only [map_mul]
-  rfl
+lemma comp_def (g h : G): ρ g ∘ₗ ρ h = ρ g * ρ h := rfl
 
 noncomputable def gl_map_of_representation_of_basis
   : G →* Matrix.GeneralLinearGroup ι R where
     toFun g := {
       val := LinearMap.toMatrix 𝓑 𝓑 (ρ g)
       inv := LinearMap.toMatrix 𝓑 𝓑 (ρ g⁻¹)
-      val_inv := by rw [← toMatrix_comp, comp_eq_mul]; simp
-      inv_val := by rw [← toMatrix_comp, comp_eq_mul]; simp
+      val_inv := by rw [← toMatrix_comp, comp_def, ← map_mul]; simp
+      inv_val := by rw [← toMatrix_comp, comp_def, ← map_mul]; simp
     }
     map_one' := by aesop
     map_mul' := by rintro x y; simp [LinearMap.toMatrix_mul]; norm_cast
 
-noncomputable def tprod' (R' : Type*) [CommRing R'] [Algebra R R'] (ρ : Representation R G V)
-    : Representation R G (R' ⊗[R] V) where
-  toFun g := TensorProduct.map .id (ρ g)
-  map_one' := by simp only [map_one]; rw [← LinearMap.one_eq_id, TensorProduct.map_one]
+noncomputable def baseChange (R' : Type*) [CommRing R'] [Algebra R R'] (ρ : Representation R G V)
+    : Representation R' G (R' ⊗[R] V) where
+  toFun g := LinearMap.baseChange R' (ρ g)
+  map_one' := by aesop
   map_mul' := by aesop
 
 scoped notation ρ "⊗ᵣ" ρ' => tprod ρ ρ'
-scoped notation R' "⊗ᵣ'" ρ => tprod' R' ρ
+scoped notation R' "⊗ᵣ'" ρ => baseChange R' ρ
 
 end Representation
