ebbb6f2506888a21f3541cc71f1c5ba0181056e5
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-01-06 15:45:44 +0000

    feat: add sorried Rat.ringOfIntegersEquiv_eq_algebraMap

---
+++ FLT/Mathlib/NumberTheory/NumberField/Basic.lean
@@ -0,0 +1,7 @@
+import Mathlib.NumberTheory.NumberField.Basic
+
+open scoped NumberField
+
+theorem Rat.ringOfIntegersEquiv_eq_algebraMap (z : 𝓞 ℚ) :
+    (Rat.ringOfIntegersEquiv z : ℚ) = algebraMap (𝓞 ℚ) ℚ z := by
+  sorry
