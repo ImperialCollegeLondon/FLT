33bb9eb6971ebaa77694a1f9e621c0a42922fa39
Javier López-Contreras <javierlc2000@gmail.com>
2025-01-31 18:34:57 +0000

    Sorried CommAlgCat as neatly as I can

--- FLT/Mathlib/CategoryTheory/Comma/Over.lean
+++ FLT/Mathlib/CategoryTheory/Comma/Over.lean
@@ -1,11 +1,21 @@
--- TODO(jlcontreras): the name of this file in Mathlib is pretty bad, its called "Over" but also
--- contains the API for Under
-
 import Mathlib
+
+open CategoryTheory
+
+section Under
 
 universe u v
 
-instance CategoryTheory.Under.instConcreteCategory {C : Type u} [CategoryTheory.Category.{v, u} C]
-    {X : C} [CategoryTheory.ConcreteCategory C] :
-    CategoryTheory.ConcreteCategory (CategoryTheory.Under X) := by
-  sorry
+instance Subtype.instFunLike {F : Type*} {X Y} [DFunLike F X Y] (P : F → Prop) :
+    DFunLike (Subtype P) X Y where
+  coe f := f
+  coe_injective' _ _ h := Subtype.coe_injective (DFunLike.coe_injective h)
+
+instance Under.instConcreteCategory {C : Type u} [CategoryTheory.Category.{v, u} C]
+    {FC : C → C → Type*} {CC : C → Type*} [∀ X Y, FunLike (FC X Y) (CC X) (CC Y)]
+    {X : C} [ConcreteCategory C FC] :
+    ConcreteCategory (CategoryTheory.Under X) fun X Y => {f : FC X.right Y.right | ∀ x, f (X.hom x) = Y.hom x} where
+  hom f := ⟨ConcreteCategory.hom f.right, sorry⟩
+  ofHom f := Under.homMk (ConcreteCategory.ofHom f) (w := sorry)
+
+end Under
