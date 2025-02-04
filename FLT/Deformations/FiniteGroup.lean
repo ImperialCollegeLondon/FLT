b6ab09f922314ae98e8bab6b1a36f1a407dc9bd3
Javier López-Contreras <javierlc2000@gmail.com>
2025-02-04 14:36:11 +0000

    Feat(FiniteGroup.lean): Progress on G finite case, builds candidate. Missing condition: rhobar cannot be trivial

--- FLT/Deformations/FiniteGroup.lean
+++ FLT/Deformations/FiniteGroup.lean
@@ -38,6 +38,8 @@ section G_finite -- Section 3.1 Smit & Lenstra
 open Matrix Set MvPolynomial
 variable [Finite G]
 
+variable {ι : Type*} [DecidableEq ι] [Fintype ι]
+
 variable (𝓞 G) in
 noncomputable abbrev smitLenstraRingRelations1 (i : ι) : MvPolynomial (ι × ι × G) 𝓞 :=
   X (i, i, (1:G)) - C (1 : 𝓞)
@@ -50,39 +52,34 @@ variable (𝓞 G) in
 noncomputable abbrev smitLenstraRingRelations3 (i j : ι) (g h : G) : MvPolynomial (ι × ι × G) 𝓞 :=
     X (i, j, g * h) - ∑ᶠ (l : ι), (X (i, l, g)) * (X (l, j, h))
 
-variable (𝓞 G) in
-noncomputable abbrev smitLenstraRingRelations (ι : Type u) [Fintype ι] : Ideal (MvPolynomial (ι × ι × G) 𝓞) :=
+variable (𝓞 G ι) in
+noncomputable abbrev smitLenstraRingRelations : Ideal (MvPolynomial (ι × ι × G) 𝓞) :=
   let rel1 := {smitLenstraRingRelations1 𝓞 G i | (i : ι)}
   let rel2 := {smitLenstraRingRelations2 𝓞 G i j hneq | (i : ι) (j : ι) (hneq : i ≠ j)}
   let rel3 := {smitLenstraRingRelations3 𝓞 G i j g h | (i : ι) (j : ι) (g : G) (h : G)}
   Ideal.span (rel1 ∪ rel2 ∪ rel3)
 
 -- SmitLenstraRing is the ring 𝓞[G, n] given by Smit&Lenstra
-variable (𝓞 G) in
-noncomputable abbrev smitLenstraRing (ι : Type u) [Fintype ι] : Type u :=
+variable (𝓞 G ι) in
+noncomputable abbrev smitLenstraRing : Type _ :=
   (MvPolynomial (ι × ι × G) 𝓞) ⧸ smitLenstraRingRelations 𝓞 G ι
 
 local notation3:max 𝓞 "[" G ", " α "]" => smitLenstraRing 𝓞 G α
 local notation3:max "GL(" α ", " R ")" => (GeneralLinearGroup α R)
 
--- Choose any basis of V, this makes ρbar into a G →* GL_ι(𝓴 A)
-variable {ι : Type u} [DecidableEq ι] [Fintype ι]
-variable (𝓫 : Basis ι (𝓴 𝓞) V)
-noncomputable def pbar' := Representation.gl_map_of_basis ρbar 𝓫
-
-variable {A : 𝓒 𝓞}
+variable {A : Type*} [CommRing A] [Algebra 𝓞 A]
 
 noncomputable abbrev smitLenstraMap_mvpoly_to_matrix (f : 𝓞[G, ι] →ₐ[𝓞] A) (g : G) : Matrix ι ι A :=
   fun i j : ι ↦ f (Ideal.Quotient.mk _ (X (i, j, g)))
 
 @[simp]
 lemma smitLenstraMap_map_one_elementwise_ii {f : 𝓞[G, ι] →ₐ[𝓞] A} {i : ι} :
-    f (Ideal.Quotient.mk _ (X (i, i, 1))) = (1 : A.obj) := by
+    f (Ideal.Quotient.mk _ (X (i, i, 1))) = (1 : A) := by
   sorry
 
 @[simp]
 lemma smitLenstraMap_map_one_elementwise_ij {f : 𝓞[G, ι] →ₐ[𝓞] A} {i j : ι} (h : i ≠ j) :
-    f (Ideal.Quotient.mk _ (X (i, j, 1))) = (0 : A.obj) := by
+    f (Ideal.Quotient.mk _ (X (i, j, 1))) = (0 : A) := by
   sorry
 
 @[simp]
@@ -104,7 +101,7 @@ lemma smitLenstraMap_map_mul (f : 𝓞[G, ι] →ₐ[𝓞] A) (g h : G) :
   simp only [mul_apply]
   sorry
 
-def inverseSmitLenstraMap_eval_on_choice (ρ : G →* GL(ι, A)) : MvPolynomial (ι × ι × G) 𝓞 →ₐ[𝓞] A.obj where
+def inverseSmitLenstraMap_eval_on_choice (ρ : G →* GL(ι, A)) : MvPolynomial (ι × ι × G) 𝓞 →ₐ[𝓞] A where
   toFun F := F.eval₂ (algebraMap 𝓞 A) (fun ⟨i, j, g⟩ ↦ (ρ g).val i j)
   map_one' := by aesop
   map_mul' := by aesop
@@ -116,7 +113,7 @@ lemma inverseSmitLenstraMap_eval_independent_of_choice {ρ : G →* GL(ι, A)} :
     ∀ F ∈ smitLenstraRingRelations 𝓞 G ι, inverseSmitLenstraMap_eval_on_choice ρ F = 0 := by
   sorry
 
-noncomputable def inverseSmitLenstraMap_eval (ρ : G →* GL(ι, A)) : 𝓞[G, ι] →ₐ[𝓞] A.obj :=
+noncomputable def inverseSmitLenstraMap_eval (ρ : G →* GL(ι, A)) : 𝓞[G, ι] →ₐ[𝓞] A :=
   Ideal.Quotient.liftₐ
     (smitLenstraRingRelations 𝓞 G ι)
     (inverseSmitLenstraMap_eval_on_choice ρ)
@@ -148,6 +145,24 @@ noncomputable def smitLenstraMap : (𝓞[G, ι] →ₐ[𝓞] A) ≃ (G →* GL(�
     ext g i j
     simp_all only [MonoidHom.coe_mk, OneHom.coe_mk]
     sorry
+
+-- Choose any basis of V, this makes ρbar into a G →* GL_ι(𝓴 A)
+variable (𝓫 : Basis ι (𝓴 𝓞) V)
+noncomputable def ρbar' := Representation.gl_map_of_basis ρbar 𝓫
+
+noncomputable def smitLenstraCandidate_map : 𝓞[G, ι] →ₐ[𝓞] (𝓴 𝓞) :=
+  (smitLenstraMap (𝓞 := 𝓞) (G := G) (A := 𝓴 𝓞) (ι := ι)).symm (ρbar' ρbar 𝓫)
+
+noncomputable abbrev smitLenstraCandidate_maximalIdeal : Ideal (𝓞[G, ι]) :=
+  RingHom.ker (smitLenstraCandidate_map ρbar 𝓫)
+
+instance : (smitLenstraCandidate_maximalIdeal ρbar 𝓫).IsMaximal :=
+  RingHom.ker_isMaximal_of_surjective
+    (smitLenstraCandidate_map ρbar 𝓫)
+    (by sorry) -- For this to be true, ρbar needs to be nontrivial!
+
+noncomputable abbrev smitLenstraCandidate : Type _ :=
+  Localization.AtPrime (smitLenstraCandidate_maximalIdeal ρbar 𝓫)
 
 -- Proposition 2.5 in G Finite
 theorem Lift.functor_isCorepresentable_finite : (Lift.functor 𝓞 ρbar).IsCorepresentable := sorry
