bd53afc8285194ff362773fded5e9247d4a3d3fe
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-02-06 06:43:03 +0100

    chore: bump to v4.17.0-rc1

--- FLT/MathlibExperiments/Coalgebra/Sweedler.lean
+++ FLT/MathlibExperiments/Coalgebra/Sweedler.lean
@@ -31,18 +31,18 @@ variable [AddCommMonoid A] [Module R A] [Coalgebra R A]
 
 lemma exists_repr (x : A) :
     ∃ (I : Finset (A ⊗[R] A)) (x1 : A ⊗[R] A → A) (x2 : A ⊗[R] A → A),
-    Coalgebra.comul x = ∑ i in I, x1 i ⊗ₜ[R] x2 i := by
+    Coalgebra.comul x = ∑ i ∈ I, x1 i ⊗ₜ[R] x2 i := by
   classical
   have mem1 : comul x ∈ (⊤ : Submodule R (A ⊗[R] A)) := ⟨⟩
   rw [← TensorProduct.span_tmul_eq_top, mem_span_set] at mem1
-  obtain ⟨r, hr, (eq1 : ∑ i in r.support, (_ • _) = _)⟩ := mem1
+  obtain ⟨r, hr, (eq1 : ∑ i ∈ r.support, (_ • _) = _)⟩ := mem1
   choose a a' haa' using hr
   replace eq1 := calc _
-    comul x = ∑ i in r.support, r i • i := eq1.symm
-    _ = ∑ i in r.support.attach, (r i : R) • i.1 := Finset.sum_attach _ _ |>.symm
-    _ = ∑ i in r.support.attach, (r i • a i.2 ⊗ₜ[R] a' i.2) :=
+    comul x = ∑ i ∈ r.support, r i • i := eq1.symm
+    _ = ∑ i ∈ r.support.attach, (r i : R) • i.1 := Finset.sum_attach _ _ |>.symm
+    _ = ∑ i ∈ r.support.attach, (r i • a i.2 ⊗ₜ[R] a' i.2) :=
         Finset.sum_congr rfl fun i _ ↦ congr(r i.1 • $(haa' i.2)).symm
-    _ = ∑ i in r.support.attach, ((r i • a i.2) ⊗ₜ[R] a' i.2) :=
+    _ = ∑ i ∈ r.support.attach, ((r i • a i.2) ⊗ₜ[R] a' i.2) :=
         Finset.sum_congr rfl fun i _ ↦ TensorProduct.smul_tmul' _ _ _
   refine ⟨r.support, fun i ↦ if h : i ∈ r.support then r i • a h else 0,
     fun i ↦ if h : i ∈ r.support then a' h else 0, eq1 ▸ ?_⟩
@@ -60,17 +60,17 @@ noncomputable def Δ₂ (a : A) : A ⊗[R] A → A :=
   exists_repr a |>.choose_spec.choose_spec.choose
 
 lemma comul_repr (a : A) :
-    Coalgebra.comul a = ∑ i in ℐ a, Δ₁ a i ⊗ₜ[R] Δ₂ (R := R) a i :=
+    Coalgebra.comul a = ∑ i ∈ ℐ a, Δ₁ a i ⊗ₜ[R] Δ₂ (R := R) a i :=
   exists_repr a |>.choose_spec.choose_spec.choose_spec
 
 lemma sum_counit_tmul (a : A) {ι : Type*} (s : Finset ι) (x y : ι → A)
-    (repr : comul a = ∑ i in s, x i ⊗ₜ[R] y i) :
-    ∑ i in s, counit (R := R) (x i) ⊗ₜ y i = 1 ⊗ₜ[R] a := by
+    (repr : comul a = ∑ i ∈ s, x i ⊗ₜ[R] y i) :
+    ∑ i ∈ s, counit (R := R) (x i) ⊗ₜ y i = 1 ⊗ₜ[R] a := by
   simpa [repr, map_sum] using congr($(rTensor_counit_comp_comul (R := R) (A := A)) a)
 
 lemma sum_tmul_counit (a : A) {ι : Type*} (s : Finset ι) (x y : ι → A)
-    (repr : comul a = ∑ i in s, x i ⊗ₜ[R] y i) :
-    ∑ i in s, (x i) ⊗ₜ counit (R := R) (y i) = a ⊗ₜ[R] 1 := by
+    (repr : comul a = ∑ i ∈ s, x i ⊗ₜ[R] y i) :
+    ∑ i ∈ s, (x i) ⊗ₜ counit (R := R) (y i) = a ⊗ₜ[R] 1 := by
   simpa [repr, map_sum] using congr($(lTensor_counit_comp_comul (R := R) (A := A)) a)
 
 end Coalgebra
--- scripts/bump.sh
+++ scripts/bump.sh
@@ -2,7 +2,7 @@
 lake update mathlib
 # Update the doc-gen version inside the docbuild project
 cd docbuild
-lake update FLT
+MATHLIB_NO_CACHE_ON_UPDATE=1 lake update FLT
 rm lean-toolchain
 rm lake-manifest.json
 cd ..
