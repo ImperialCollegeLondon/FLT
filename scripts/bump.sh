f203164acbb4ba0cc351a87e37eb913edc4a3a68
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-02-06 09:44:09 +0100

    Update bump.sh

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -2,7 +2,7 @@
 lake update mathlib
 # Update the doc-gen version inside the docbuild project
 cd docbuild
-MATHLIB_NO_CACHE_ON_UPDATE=1 lake update FLT
+lake update FLT
 rm lean-toolchain
 rm lake-manifest.json
 cd ..
