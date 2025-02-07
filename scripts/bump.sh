1bd710597d8791b5eadd43f1e5cccefa06bd3bc4
Yaël Dillies <yael.dillies@gmail.com>
2025-02-07 13:33:03 +0100

    CI: generate doc-building repository in CI (#334)
    * CI: regenerate doc-building repository in CI
    * Don't have `docbuild` lying around always. Currently, the toolchain it gets updated to is independent of the toolchain FLT is on, causing issue every first few days of the month. Now, the toolchain with which the docs are built will always be the one from FLT. No one anymore can accidentally introduce a toolchain mismatch error in the repo by modifying/forgetting to modify the pinned toolchain in `docbuild/lakefile.toml`.
    * Add `scripts/build_docs.sh` to automate building the docs. This script creates a temporary `docbuild` repo, generates the docs, then deletes the temporary repo again.
    * Remove `scripts/bump.sh`. Now that `docbuild` doesn't exist outside of CI/building the docs locally with `build_docs.sh`, instructions to update the repo are simply "Run `lake update`".
    
    * Remove working directory
    
    * typo
    
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
    
    * MATHLIB_NO_CACHE_ON_UPDATE
    
    ---------
    
    Co-authored-by: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- scripts/bump.sh
+++
@@ -1,8 +0,0 @@
-# Update the mathlib version inside the main project
-lake update mathlib
-# Update the doc-gen version inside the docbuild project
-cd docbuild
-lake update FLT
-rm lean-toolchain
-rm lake-manifest.json
-cd ..
