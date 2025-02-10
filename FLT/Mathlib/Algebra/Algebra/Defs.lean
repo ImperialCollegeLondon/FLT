102307d682e5c288358d379dd1a671e9233d0f73
Javier López-Contreras <javierlc2000@gmail.com>
2025-02-10 00:34:47 +0000

    Nitpick

--- FLT/Mathlib/Algebra/Algebra/Defs.lean
+++ FLT/Mathlib/Algebra/Algebra/Defs.lean
@@ -3,5 +3,6 @@ import Mathlib.RingTheory.Ideal.Quotient.Defs
 
 variable {R : Type*} [CommRing R]
 
-instance idealQuotient_instAlgebra {X : Type*} [CommRing X] [Algebra R X] (a : Ideal X) : Algebra R (X ⧸ a) :=
+instance idealQuotient_instAlgebra {X : Type*} [CommRing X] [Algebra R X] (a : Ideal X)
+    : Algebra R (X ⧸ a) :=
   (RingHom.comp (Ideal.Quotient.mk a) (algebraMap R X)).toAlgebra
