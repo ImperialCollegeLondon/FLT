90bdad4dc591e10deb3ac5ef6cf7103f79cd7519
Javier López-Contreras <javierlc2000@gmail.com>
2025-02-10 11:26:15 +0000

    Deleted Quotient algebra, there was a missing import

--- FLT/Mathlib/Algebra/Algebra/Defs.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Algebra.Algebra.Defs
-import Mathlib.RingTheory.Ideal.Quotient.Defs
-
-variable {R : Type*} [CommRing R]
-
-instance idealQuotient_instAlgebra {X : Type*} [CommRing X] [Algebra R X] (a : Ideal X)
-    : Algebra R (X ⧸ a) :=
-  (RingHom.comp (Ideal.Quotient.mk a) (algebraMap R X)).toAlgebra
