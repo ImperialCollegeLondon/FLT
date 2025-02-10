abca1eb2715c84c6169fd9e0014477af55517684
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-02-10 09:29:56 +0000

    chore(Mathlib/Topology/Algebra/Group): add refactoring TODO

--- FLT/Mathlib/Topology/Algebra/Group/Quotient.lean
+++ FLT/Mathlib/Topology/Algebra/Group/Quotient.lean
@@ -4,6 +4,11 @@ import FLT.Mathlib.Topology.Algebra.ContinuousMonoidHom
 import FLT.Mathlib.Topology.Algebra.Module.Quotient
 import FLT.Mathlib.Topology.Algebra.Module.Equiv
 
+-- this proof doesn't work for multiplicative groups so should be
+-- refactored so that it does, and then toadditive'd.
+-- It should also really be using `comap` not `map` to conform with
+-- mathlib library decisions.
+-- FLT#341
 def QuotientAddGroup.continuousAddEquiv (G H : Type*) [AddCommGroup G] [AddCommGroup H] [TopologicalSpace G]
     [TopologicalSpace H] (G' : AddSubgroup G) (H' : AddSubgroup H) [G'.Normal] [H'.Normal]
     (e : G ≃ₜ+ H) (h : AddSubgroup.map e G' = H') :
