fa108a45e13847c86acb503cb71ab4dba3ae9161
Javier López-Contreras <javierlc2000@gmail.com>
2025-02-24 09:21:33 +0000

    Fixes PR Commit

--- FLT/Mathlib/RingTheory/LocalRing/Defs.lean
+++ FLT/Mathlib/RingTheory/LocalRing/Defs.lean
@@ -7,11 +7,11 @@ instance isLocalRing_of_quotient (I : Ideal R) [Nontrivial (R ⧸ I)] :
     IsLocalRing (R ⧸ I) where
   exists_pair_ne := exists_pair_ne (R ⧸ I)
   isUnit_or_isUnit_of_add_one {a b} h := by
-    induction a using Quotient.inductionOn
-    rename_i a₀
-    obtain ha | hb := IsLocalRing.isUnit_or_isUnit_of_add_one (show a₀ + (1 - a₀) = 1 by ring)
-    · left
-      exact RingHom.isUnit_map (Ideal.Quotient.mk I) ha
-    · right
-      rw [eq_sub_of_add_eq' h]
-      exact RingHom.isUnit_map (Ideal.Quotient.mk I) hb
+    induction a using Quotient.inductionOn with
+    | h a₀ =>
+      obtain ha | hb := IsLocalRing.isUnit_or_isUnit_of_add_one (show a₀ + (1 - a₀) = 1 by ring)
+      · left
+        exact RingHom.isUnit_map (Ideal.Quotient.mk I) ha
+      · right
+        rw [eq_sub_of_add_eq' h]
+        exact RingHom.isUnit_map (Ideal.Quotient.mk I) hb
