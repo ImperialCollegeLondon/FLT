ea1ec98ca0d7dc57ab103b24ce92b48f922e6266
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2025-03-19 01:36:11 +0100

    chore: bump mathlib

--- FLT/Mathlib/Data/Finset/Lattice/Fold.lean
+++ FLT/Mathlib/Data/Finset/Lattice/Fold.lean
@@ -1,17 +1,1 @@
 import Mathlib.Data.Finset.Lattice.Fold
-
- theorem Finset.le_sup_dite_pos {α β : Type*} [SemilatticeSup α] [OrderBot α] {s : Finset β}
-    (p : β → Prop) [DecidablePred p] {f : (b : β) → p b → α} {g : (b : β) → ¬p b → α} {b : β}
-    (h₀ : b ∈ s) (h₁ : p b) :
-    f b h₁ ≤ s.sup fun i => if h : p i then f i h else g i h := by
-  have : f b h₁ = (fun i => if h : p i then f i h else g i h) b := by simp [h₁]
-  rw [this]
-  apply Finset.le_sup h₀
-
-  theorem Finset.le_sup_dite_neg {α β : Type*} [SemilatticeSup α] [OrderBot α] {s : Finset β}
-    (p : β → Prop) [DecidablePred p] {f : (b : β) → p b → α} {g : (b : β) → ¬p b → α} {b : β}
-    (h₀ : b ∈ s) (h₁ : ¬p b) :
-    g b h₁ ≤ s.sup fun i => if h : p i then f i h else g i h := by
-  have : g b h₁ = (fun i => if h : p i then f i h else g i h) b := by simp [h₁]
-  rw [this]
-  apply Finset.le_sup h₀
