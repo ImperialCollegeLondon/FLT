925eb53ef683bced7965510b17e7cd1fccb13e1b
Matthew Jasper <mjjasper1@gmail.com>
2025-04-06 22:49:37 +0100

    Valuation subrings are closed
    * Add Valued.integer_isClosed
    * Add Valued.valuationSubring_isClosed

--- FLT/Mathlib/Topology/Algebra/Valued/ValuationTopology.lean
+++ FLT/Mathlib/Topology/Algebra/Valued/ValuationTopology.lean
@@ -8,3 +8,16 @@ lemma Valued.isUnit_valuationSubring_iff {Γ₀ : Type*} [LinearOrderedCommGroup
     IsUnit x ↔ Valued.v (x.val : F) = 1 := by
   convert Valuation.Integers.isUnit_iff_valuation_eq_one _
   exact Valuation.integer.integers _
+
+/-- The unit ball of a valued ring is open. -/
+theorem Valued.integer_isClosed ( R : Type* ) {Γ₀ : Type*} [Ring R]
+    [LinearOrderedCommGroupWithZero Γ₀] [_i : Valued R Γ₀] : IsClosed (_i.v.integer : Set R) := by
+  rw [← Subring.coe_toAddSubgroup]
+  apply AddSubgroup.isClosed_of_isOpen
+  apply integer_isOpen
+
+/-- The valuation subring of a valued field is closed. -/
+theorem Valued.valuationSubring_isClosed {Γ₀ : Type*} [LinearOrderedCommGroupWithZero Γ₀]
+    [hv : Valued F Γ₀] :
+    IsClosed (hv.v.valuationSubring : Set F) :=
+  integer_isClosed F
