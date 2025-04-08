3facd23a0138596404807d5f0061883e4b3e0a25
Javier López-Contreras <javierlc2000@gmail.com>
2025-04-08 10:21:55 +0000

    Progress: completes primitive element step

--- FLT/DedekindDomain/FiniteAdeleRing/BaseChange.lean
+++ FLT/DedekindDomain/FiniteAdeleRing/BaseChange.lean
@@ -423,35 +423,57 @@ variable [DecidableEq (v.adicCompletion K)]
 
 variable (α : L) (hα : K⟮α⟯ = ⊤) (f : K[X]) (hf : f = minpoly K α)
 
+omit [FiniteDimensional K L] in
 lemma aeval_α_surj (α : L) (hα : K⟮α⟯ = ⊤) (f : K[X]) (hf : f = minpoly K α)
     : Function.Surjective (aeval α (R := K)) := by
   intro l
   have l_in : l ∈ K⟮α⟯ := by rw [hα]; simp
   have ⟨r, s, hrs⟩ := (IntermediateField.mem_adjoin_simple_iff K l).mp l_in
-  have hrs' : l * (aeval α) s = (aeval α) r := by
-    letI : Invertible ((aeval α) s) := sorry
-    rw [hrs, div_mul_cancel_of_invertible]
-  have ctt : (EuclideanDomain.gcd f s).degree = 0 := by
-    have coprime : IsCoprime f s := sorry
-    sorry
-  let ⟨a, b⟩ := EuclideanDomain.xgcd f s
-  let hc := eq_C_of_degree_eq_zero ctt
-  have bezout : (EuclideanDomain.gcd f s) = f * a + s * b := sorry
-  have bezout' : (aeval α) (EuclideanDomain.gcd f s) = (aeval α) (s) * (aeval α) (b) := by
-    rw [bezout, hf, aeval_add, aeval_mul,
-      (minpoly.eq_iff_aeval_eq_zero (p := minpoly K α)
-        (minpoly.irreducible (Algebra.IsSeparable.isIntegral K α))
-        (minpoly.monic (Algebra.IsSeparable.isIntegral K α))
-      ).mp rfl, zero_mul, zero_add, aeval_mul]
-  simp only [aeval_C, aeval_mul, aeval_add] at bezout'
-  letI : Invertible ((EuclideanDomain.gcd f s).coeff 0) := sorry
-  letI : Invertible (algebraMap K L ((EuclideanDomain.gcd f s).coeff 0)) := sorry
-  use (EuclideanDomain.gcd f s).constantCoeff⁻¹ • (r * b)
-  simp only [eval_map_algebraMap, AlgHom.mk_coe, constantCoeff_apply, map_smul, aeval_mul]
-  rw [← hrs', mul_assoc l, ← bezout']
-  nth_rw 2 [hc]
-  rw [aeval_C, ← algebraMap_smul (A := L), smul_eq_mul, mul_comm, mul_assoc, map_inv₀ (f := algebraMap K L),
-    mul_inv_cancel_of_invertible, mul_one]
+  by_cases degen : ((aeval α) s) = 0
+  . have : l = 0 := by rw [degen, div_zero] at hrs; exact hrs
+    use C 0
+    rw [aeval_C, map_zero, this]
+  . letI : Invertible ((aeval α) s) := by exact invertibleOfNonzero degen
+    have hrs' : l * (aeval α) s = (aeval α) r := by
+      rw [hrs, div_mul_cancel_of_invertible]
+    have ctt : (EuclideanDomain.gcd f s).degree = 0 := by
+      have coprime : IsCoprime f s := by
+        rw [hf]
+        obtain cop|dvd := (minpoly.irreducible (Algebra.IsSeparable.isIntegral K α)).isCoprime_or_dvd s
+        . exact cop
+        . by_contra
+          exact degen (minpoly.dvd_iff.mp dvd)
+      have h_unit : IsUnit (EuclideanDomain.gcd f s) := EuclideanDomain.gcd_isUnit_iff.mpr coprime
+      exact Polynomial.degree_eq_zero_of_isUnit h_unit
+    let a := EuclideanDomain.gcdA f s
+    let b := EuclideanDomain.gcdB f s
+    let hc := eq_C_of_degree_eq_zero ctt
+    have bezout : (EuclideanDomain.gcd f s) = f * a + s * b := by
+      unfold a b
+      exact EuclideanDomain.gcd_eq_gcd_ab f s
+    have bezout' : (aeval α) (EuclideanDomain.gcd f s) = (aeval α) (s) * (aeval α) (b) := by
+      rw [bezout, hf, aeval_add, aeval_mul,
+        (minpoly.eq_iff_aeval_eq_zero (p := minpoly K α)
+          (minpoly.irreducible (Algebra.IsSeparable.isIntegral K α))
+          (minpoly.monic (Algebra.IsSeparable.isIntegral K α))
+        ).mp rfl, zero_mul, zero_add, aeval_mul]
+    simp only [aeval_C, aeval_mul, aeval_add] at bezout'
+    letI : Invertible ((EuclideanDomain.gcd f s).coeff 0) := invertibleOfNonzero (by
+        intro hc
+        have h : (EuclideanDomain.gcd f s) = C ((EuclideanDomain.gcd f s).coeff 0) :=
+          Polynomial.eq_C_of_degree_eq_zero ctt
+        rw [hc, C_0] at h
+        rw [h, degree_zero] at ctt
+        exact WithBot.zero_ne_bot ctt.symm
+      )
+    letI : Invertible (algebraMap K L ((EuclideanDomain.gcd f s).coeff 0)) := by
+      exact Invertible.map (algebraMap K L) ((EuclideanDomain.gcd f s).coeff 0)
+    use (EuclideanDomain.gcd f s).constantCoeff⁻¹ • (r * b)
+    simp only [eval_map_algebraMap, AlgHom.mk_coe, constantCoeff_apply, map_smul, aeval_mul]
+    rw [← hrs', mul_assoc l, ← bezout']
+    nth_rw 2 [hc]
+    rw [aeval_C, ← algebraMap_smul (A := L), smul_eq_mul, mul_comm, mul_assoc, map_inv₀ (f := algebraMap K L),
+      mul_inv_cancel_of_invertible, mul_one]
 
 omit [FiniteDimensional K L] [Algebra.IsSeparable K L] [DecidableEq K] in
 lemma aeval_α_ker (α : L) (f : K[X]) (hf : f = minpoly K α) : RingHom.ker (aeval α (R := K)) = Ideal.span {f} := by
@@ -470,8 +492,8 @@ noncomputable def polynomialPrimitiveElement_otimes (α : L) (hα : K⟮α⟯ = 
 
 def baseChangePolynomialQuotient :
   (K[X] ⧸ Ideal.span {f}) ⊗[K] (v.adicCompletion K) ≃
-      (v.adicCompletion K)[X] ⧸ Ideal.span {f.map (algebraMap K (v.adicCompletion K))} :=
-  by exact?
+      (v.adicCompletion K)[X] ⧸ Ideal.span {f.map (algebraMap K (v.adicCompletion K))} := by
+    sorry
 
 variable (factors : Multiset (v.adicCompletion K)[X])
     (factors_prime : ∀ factor : factors.toFinset, Irreducible factor.1)
