4fbc18b65532a207a2e044330b57412ac4d36b41
Matthew Jasper <mjjasper1@gmail.com>
2025-04-08 22:24:46 +0100

    clean up some more proofs, fix some names

--- FLT/DedekindDomain/AdicValuation.lean
+++ FLT/DedekindDomain/AdicValuation.lean
@@ -1,7 +1,6 @@
 import FLT.Mathlib.Topology.Algebra.Valued.ValuationTopology
 import Mathlib.Topology.Algebra.Valued.NormedValued
 import Mathlib.RingTheory.DedekindDomain.AdicValuation
-import Mathlib.RingTheory.DedekindDomain.Dvr
 import Mathlib.Algebra.Algebra.Subalgebra.Pi
 import Mathlib.Algebra.Order.GroupWithZero.Canonical
 
@@ -57,7 +56,7 @@ lemma exists_approxInverse { a b : A } { n : ℕ } ( hnz : a ≠ 0 )
       Ideal.dvd_span_singleton, neg_mem_iff]
 
 open scoped Multiplicative in
-lemma mul_int_lower_bound { x y : ℤₘ₀ } ( hx : x ≠ 0 ) ( hy : y ≠ 0) :
+lemma exists_ofAdd_lt_and_ofAdd_lt { x y : ℤₘ₀ } ( hx : x ≠ 0 ) ( hy : y ≠ 0) :
     ∃ (k : ℕ), (Multiplicative.ofAdd (-(k : ℤ))) < x ∧ (Multiplicative.ofAdd (-(k : ℤ))) < y := by
   let u := WithZero.unzero hx
   let v := WithZero.unzero hy
@@ -79,7 +78,9 @@ lemma mul_int_lower_bound { x y : ℤₘ₀ } ( hx : x ≠ 0 ) ( hy : y ≠ 0) :
     rfl
   decide
 
-lemma approx_of_valued_le_one { x : (WithVal (v.valuation K)) }
+/-- An element of `K` with valuation ≤ 1 can be approximated by an element of `A`.
+  -/
+lemma exists_adicValued_sub_lt_of_adicValued_le_one { x : (WithVal (v.valuation K)) }
     ( γ : (WithZero (Multiplicative ℤ))ˣ ) ( hx : Valued.v x ≤ 1 ) :
     ∃a, Valued.v ((algebraMap A K a) - (x : v.adicCompletion K)) < γ.val := by
   obtain ⟨⟨n, d, hd⟩, hnd⟩ := IsLocalization.surj (nonZeroDivisors A) x
@@ -104,7 +105,7 @@ lemma approx_of_valued_le_one { x : (WithVal (v.valuation K)) }
   let hu : Valued.v ((algebraMap A (WithVal (v.valuation K)) d)) * γ.val ≠ 0 := by
     rw [mul_ne_zero_iff]
     exact ⟨hv, γ.ne_zero⟩
-  obtain ⟨k, hku, hkv⟩ := mul_int_lower_bound hu hv
+  obtain ⟨k, hku, hkv⟩ := exists_ofAdd_lt_and_ofAdd_lt hu hv
   simp only [WithVal, HeightOneSpectrum.adicValued_apply,
     HeightOneSpectrum.valuation_of_algebraMap, HeightOneSpectrum.intValuation_apply] at hkv
   obtain ⟨a, hval⟩ := exists_approxInverse v (nonZeroDivisors.ne_zero hd) hkv.le hge
@@ -119,22 +120,20 @@ lemma approx_of_valued_le_one { x : (WithVal (v.valuation K)) }
       HeightOneSpectrum.intValuation_apply]
   apply lt_of_le_of_lt hval hku
 
+/-- The closure of `A` in `K_v` is `O_[K_v]`. -/
 theorem closureAlgebraMapIntegers_eq_integers :
     closure (algebraMap A (v.adicCompletion K)).range =
     SetLike.coe (v.adicCompletionIntegers K) := by
-  have hopen : IsOpen (SetLike.coe (v.adicCompletionIntegers K)) := by
-    apply Valued.integer_isOpen
-  let f := fun (k : WithVal (v.valuation K)) => (k : v.adicCompletion K)
-  have hd : DenseRange f := by
-    apply UniformSpace.Completion.denseRange_coe
   apply subset_antisymm
   . apply closure_minimal _ Valued.valuationSubring_isClosed
     rintro b ⟨a, rfl⟩
     exact HeightOneSpectrum.coe_mem_adicCompletionIntegers v a
+  let f := fun (k : WithVal (v.valuation K)) => (k : v.adicCompletion K)
   suffices h : closure (algebraMap A (HeightOneSpectrum.adicCompletion K v)).range =
       closure (f '' (f ⁻¹' (HeightOneSpectrum.adicCompletionIntegers K v))) by
     rw [h]
-    exact DenseRange.subset_closure_image_preimage_of_isOpen hd hopen
+    exact DenseRange.subset_closure_image_preimage_of_isOpen
+      UniformSpace.Completion.denseRange_coe (Valued.valuationSubring_isOpen _)
   apply subset_antisymm
   . apply closure_mono
     rintro b ⟨a, rfl⟩
@@ -142,27 +141,26 @@ theorem closureAlgebraMapIntegers_eq_integers :
     rw [Set.mem_preimage, SetLike.mem_coe]
     constructor
     . exact HeightOneSpectrum.coe_algebraMap_mem A K v a
-    unfold f
     rw [HeightOneSpectrum.algebraMap_adicCompletion, Function.comp_apply]
   apply closure_minimal _ isClosed_closure
   rintro k ⟨x, hx, rfl⟩
-  rw [mem_closure_iff_nhds_zero]
-  intro U hU
-  rw [Valued.mem_nhds] at hU
   unfold f at hx
   rw [Set.mem_preimage, SetLike.mem_coe, HeightOneSpectrum.mem_adicCompletionIntegers,
       Valued.valuedCompletion_apply, HeightOneSpectrum.adicValued_apply'] at hx
+  rw [mem_closure_iff_nhds_zero]
+  intro U hU
+  rw [Valued.mem_nhds] at hU
   obtain ⟨γ, hγ⟩ := hU
-  obtain ⟨a, ha⟩ := approx_of_valued_le_one K v γ hx
+  obtain ⟨a, ha⟩ := exists_adicValued_sub_lt_of_adicValued_le_one K v γ hx
   use algebraMap A K a
   constructor
   . use a
     rfl
   apply hγ
-  simp only [sub_zero, Set.mem_setOf_eq]
-  exact ha
+  simpa
 
-theorem denseRange_of_integerAlgerbaMap :
+/-- A is dense in `O_[K_v]`. -/
+theorem denseRange_of_integerAlgebraMap :
     DenseRange (algebraMap A (v.adicCompletionIntegers K)) := by
   rw [denseRange_iff_closure_range]
   ext x
@@ -175,7 +173,8 @@ theorem denseRange_of_integerAlgerbaMap :
   simp only [RingHom.coe_range, ← Set.range_comp']
   rfl
 
-theorem completionInteger_approx_eq_integer ( x : v.adicCompletionIntegers K )
+/-- An element of `O_[K_v]` can be approximated by an element of `A`. -/
+theorem exists_adicValued_sub_lt_of_adicCompletionInteger ( x : v.adicCompletionIntegers K )
     ( γ : (WithZero (Multiplicative ℤ))ˣ ) :
     ∃a, Valued.v ((algebraMap A K a) - (x : v.adicCompletion K)) < γ.val := by
   have h := closureAlgebraMapIntegers_eq_integers K v
@@ -191,7 +190,9 @@ theorem completionInteger_approx_eq_integer ( x : v.adicCompletionIntegers K )
   rw [HeightOneSpectrum.algebraMap_adicCompletion, Function.comp_apply] at ha
   rwa [ha]
 
-theorem prodCompletionInteger_approx_eq_integer {ι : Type*} (s : Finset ι)
+/-- An element of `∏_{v ∈ s} O_[K_v]`, with `s` finite, can be approximated by an element of `A`.
+  -/
+theorem exists_forall_adicValued_sub_lt {ι : Type*} (s : Finset ι)
     (e : ι → (WithZero (Multiplicative ℤ))ˣ ) (valuation : ι → HeightOneSpectrum A)
     (injective : Function.Injective valuation)
     (x : (i : ι) → (valuation i).adicCompletionIntegers K) :
@@ -215,16 +216,20 @@ theorem prodCompletionInteger_approx_eq_integer {ι : Type*} (s : Finset ι)
       apply le_of_eq (Int.neg_neg (WithZero.unzero (e i).ne_zero))
     . norm_cast
     apply Units.zero_lt
-  have hinj : ∀ i ∈ s, ∀ j ∈ s, i ≠ j → (fun i ↦ (valuation i).asIdeal) i ≠ (fun i ↦ (valuation i).asIdeal) j := by
+  have hinj : ∀ i ∈ s, ∀ j ∈ s, i ≠ j →
+      (fun i ↦ (valuation i).asIdeal) i ≠ (fun i ↦ (valuation i).asIdeal) j := by
     intro i hi j hj
     contrapose!
     intro hij
     apply injective
     apply HeightOneSpectrum.ext hij
-  let f (i : s) : A := (completionInteger_approx_eq_integer K (valuation i) (x i) (e i)).choose
+  let f (i : s) : A :=
+    (exists_adicValued_sub_lt_of_adicCompletionInteger K (valuation i) (x i) (e i)).choose
   have hf : ∀ (i : s), Valued.v (((algebraMap A K) (f i)) - (x i : (valuation i).adicCompletion K))
       < (e i) :=
-    fun (i : s) => (completionInteger_approx_eq_integer K (valuation i) (x i) (e i)).choose_spec
+    fun (i : s) =>
+      (exists_adicValued_sub_lt_of_adicCompletionInteger K (valuation i) (x i) (e i)).choose_spec
+
   obtain ⟨a, ha⟩ := IsDedekindDomain.exists_forall_sub_mem_ideal (s := s)
     (fun i => (valuation i).asIdeal) e' (fun i hi => (valuation i).prime) hinj f
   use a
@@ -232,18 +237,17 @@ theorem prodCompletionInteger_approx_eq_integer {ι : Type*} (s : Finset ι)
   specialize ha i hi
   specialize hf ⟨i, hi⟩
   rw [← Ideal.dvd_span_singleton, ← HeightOneSpectrum.intValuation_le_pow_iff_dvd,
-      ← HeightOneSpectrum.intValuation_apply,
-      ← HeightOneSpectrum.valuation_of_algebraMap (K := K),
-      ← HeightOneSpectrum.valuedAdicCompletion_eq_valuation,
-      algebraMap.coe_sub] at ha
-  apply lt_of_le_of_lt _ (Valuation.map_add_lt _ (ha.trans_lt (he' i)) hf)
+      ← HeightOneSpectrum.intValuation_apply, ← HeightOneSpectrum.valuation_of_algebraMap (K := K),
+      ← HeightOneSpectrum.valuedAdicCompletion_eq_valuation, algebraMap.coe_sub] at ha
+  refine lt_of_le_of_lt ?_ (Valuation.map_add_lt _ (ha.trans_lt (he' i)) hf)
   apply le_of_eq
   congr
   rw [add_sub, sub_eq_sub_iff_add_eq_add, add_right_cancel_iff,
     add_comm_sub, add_sub, eq_sub_iff_add_eq]
   rfl
 
-theorem closureAlgebraMapIntergers_eq_prodIntegers {ι : Type*}
+/-- The closure of `A` in `∏_{v ∈ s} K_v` is `∏_{v ∈ s} O_[K_v]`. `s` may be infinite. -/
+theorem closureAlgebraMapIntegers_eq_prodIntegers {ι : Type*}
     (valuation : ι → HeightOneSpectrum A) (injective : Function.Injective valuation) :
     closure (SetLike.coe (algebraMap A ((i : ι) → (valuation i).adicCompletion K)).range) =
     (Set.pi Set.univ (fun (i : ι) ↦ HeightOneSpectrum.adicCompletionIntegers K (valuation i))) := by
@@ -264,8 +268,7 @@ theorem closureAlgebraMapIntergers_eq_prodIntegers {ι : Type*}
   obtain ⟨g, hg⟩ := Classical.axiomOfChoice
     (fun w => (Valued.is_topological_valuation (t w)).mp (htn w))
   obtain ⟨a, ha⟩ :=
-    prodCompletionInteger_approx_eq_integer K I g
-      valuation injective (fun w => ⟨f w, hf w ⟨⟩⟩)
+    exists_forall_adicValued_sub_lt K I g valuation injective (fun w => ⟨f w, hf w ⟨⟩⟩)
   use algebraMap A _ a
   constructor
   . rw [RingHom.coe_range]
