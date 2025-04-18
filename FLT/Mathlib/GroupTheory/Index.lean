33977734a4cd4608d7905abcd2884b5f1c744275
Yaël Dillies <yael.dillies@gmail.com>
2025-04-18 08:34:03 +0000

    Bump mathlib

--- FLT/Mathlib/GroupTheory/Index.lean
+++ FLT/Mathlib/GroupTheory/Index.lean
@@ -1,5 +1,4 @@
 import Mathlib.GroupTheory.Index
-import Mathlib.Algebra.GroupWithZero.Subgroup
 
 /-!
 # TODO
@@ -31,17 +30,4 @@ instance FiniteRelIndex.to_finiteIndex_subgroupOf [H.FiniteRelIndex K] :
     (H.subgroupOf K).FiniteIndex where
   index_ne_zero := relIndex_ne_zero
 
-@[to_additive]
-lemma index_map_of_bijective (S : Subgroup G) (hf : Bijective f) : (S.map f).index = S.index :=
-  index_map_eq _ hf.2 (by rw [f.ker_eq_bot_iff.2 hf.1]; exact bot_le)
-
 end Subgroup
-
-namespace AddSubgroup
-variable {G A : Type*} [Group G] [AddGroup A] [DistribMulAction G A]
-
--- TODO: Why does making this lemma simp make `NumberTheory.Padic.PadicIntegers` time out?
-lemma index_smul (a : G) (S : AddSubgroup A) : (a • S).index = S.index :=
-  index_map_of_bijective _ (MulAction.bijective _)
-
-end AddSubgroup
