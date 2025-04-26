4b34bbc022f41a0e88de84aecf742918db260f30
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-04-26 13:44:08 +0100

    it shakes!

--- FLT/Mathlib/Algebra/Module/Submodule/Basic.lean
+++ FLT/Mathlib/Algebra/Module/Submodule/Basic.lean
@@ -1,3 +1,4 @@
 import Mathlib.Algebra.Module.Submodule.Basic
+-- when this file is removed, we can remove it from noshake.json
 
 @[gcongr] protected alias ⟨_, GCongr.Submodule.toAddSubgroup_le⟩ := Submodule.toAddSubgroup_le
