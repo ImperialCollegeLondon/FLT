707083c37b64d641083d91120645994af451e32a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-04-30 10:18:39 +0100

    details about why we need it.

--- FLT/Assumptions/Odlyzko.lean
+++ FLT/Assumptions/Odlyzko.lean
@@ -1,4 +1,6 @@
 import Mathlib.NumberTheory.NumberField.Discriminant.Defs
+import Mathlib.NumberTheory.NumberField.Embeddings
+
 /-!
 
 # Bounds for root discriminants of number fields
@@ -12,13 +14,30 @@ In the proof of FLT being formalized, we need stronger bounds which do not follo
 from Minkowski's elementary argument. By analysing the behaviour of the zeros
 of the zeta function of `K`, Odlyzko was able to give stronger bounds. These bounds
 were improved by Poitou in his 1977 paper "Sur les petits discriminants".
-Equation 26 of the paper implies the below bound.
+Equation 26 of the paper implies the below bound, and an explicit reference for
+it is the table on p17 of the paper (in fact 8.25 can be beefed up to 9.3, but
+we do not need this).
 
-- [Stephen Lack and Paweł Sobociński, Adhesive Categories][adhesive2004]
+## Why do we need this?
 
-https://www.numdam.org/item/SDPP_1976-1977__18_1_A6_0/
+The original proof by Wiles and Taylor-Wiles of FLT contains a crucial step
+where they "switch to the prime 3", and apply the Langlands--Tunnell theorem
+to deduce that the 3-torsion in an elliptic curve over ℚ, if irreducible,
+is modular. In the argument we are formalizing, we switch to 3 at a different
+point. The advantage of this is that it avoids the Langlands-Tunnell theorem
+(which needs non-Galois cubic cyclic base change); the analysis we need is
+easier. We switch to 3 only after we have constructed a compatible family
+of Galois representations with conductor 2 from the Frey curve (using hard
+modularity lifting theorems), and we use this bound below to demonstrate
+that such a family cannot exist. The argument involves a careful analysis of
+the 3-torsion in the 3-adic specialization of the family; it is analogous to
+Fontaine's proof that there is no nontrivial abelian variety over ℤ.
 
-Equation 26 apparently implies the below.
+## References
+
+- [Georges Poitou, Sur les petits discriminants][poitou_odlyzko_bounds]
+
+Currently available at https://www.numdam.org/item/SDPP_1976-1977__18_1_A6_0/
 
 -/
 open Polynomial NumberField Module
