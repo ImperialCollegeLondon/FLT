907e4d921bb75a35b10182e7ab8271346ca9fae0
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-05-10 17:12:55 +0100

    remove local stuff from FLT.lean 1

--- FLT.lean
+++ FLT.lean
@@ -35,7 +35,6 @@ import FLT.Mathlib.Algebra.Algebra.Hom
 import FLT.Mathlib.Algebra.Algebra.Pi
 import FLT.Mathlib.Algebra.Algebra.Tower
 import FLT.Mathlib.Algebra.BigOperators.Group.Finset.Basic
-import FLT.Mathlib.Algebra.FixedPoints.Group
 import FLT.Mathlib.Algebra.IsQuaternionAlgebra
 import FLT.Mathlib.Algebra.Module.LinearMap.Defs
 import FLT.Mathlib.Algebra.Module.Submodule.Basic
