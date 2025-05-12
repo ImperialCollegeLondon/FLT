92a01f87a78c25360afa3e7530e3323f123bb8c4
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-05-12 11:27:33 +0100

    Add copyright header

--- FLT/Mathlib/NumberTheory/RamificationInertia/Basic.lean
+++ FLT/Mathlib/NumberTheory/RamificationInertia/Basic.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2025 Matthew Jasper. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Matthew Jasper
+-/
 import Mathlib.NumberTheory.RamificationInertia.Basic
 
 namespace Ideal
