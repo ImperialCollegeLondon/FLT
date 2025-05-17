a5b71153a640ae5a6fe93e90799707385f889284
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2025-05-17 06:09:03 +0100

    Delete FLT/Patching/Utils/CompleteRing.lean

--- FLT/Patching/Utils/CompleteRing.lean
+++
@@ -1,7 +0,0 @@
-import Mathlib.RingTheory.LocalRing.MaximalIdeal.Basic
-
-class IsCompleteRing (R) [CommRing R] (I : Ideal R) : Prop where
-  cond : (sorry : Prop)
-
-abbrev IsCompleteLocalRing (R) [CommRing R] [LocalRing R] :=
-  IsCompleteRing R (LocalRing.maximalIdeal R)
