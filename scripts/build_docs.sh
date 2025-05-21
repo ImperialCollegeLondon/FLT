a25a75c10f4a56019d265e3002abf98cdf5df9f5
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-05-21 11:30:23 +0100

    chore: try to stop CI randomly taking an hour

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -39,7 +39,11 @@ sed -i s/TOOLCHAIN/`grep -oP 'v4\..*' lean-toolchain`/ docbuild/lakefile.toml
 cd docbuild
 
 # Disable an error message due to a non-blocking bug. See Zulip
-MATHLIB_NO_CACHE_ON_UPDATE=1 ~/.elan/bin/lake update FLT
+# (note added by kmb : no idea what that "See Zulip" is referring to but Bhavik
+# suggests that we should be updating doc-gen4 rather than FLT here in order to prevent
+# the "build project API documentation" step of CI from taking 42 minutes on
+# e.g. PR 523 despite not bumping mathlib)
+MATHLIB_NO_CACHE_ON_UPDATE=1 ~/.elan/bin/lake update doc-gen4
 
 # Build the docs
 ~/.elan/bin/lake build FLT:docs
