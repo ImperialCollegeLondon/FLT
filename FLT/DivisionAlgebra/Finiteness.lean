965b8445580810d595063fe1de7afba268ac29bc
WilliamCoram <williamecoram@gmail.com>
2025-05-21 13:52:10 +0100

    update after review

--- FLT/DivisionAlgebra/Finiteness.lean
+++ FLT/DivisionAlgebra/Finiteness.lean
@@ -63,8 +63,9 @@ noncomputable abbrev incl : Dˣ →* D_𝔸ˣ :=
 
 namespace Aux
 
-lemma existsE : ∃ E : Set (D_𝔸), IsCompact E ∧ ∀ x ∈ ringHaarChar_ker D_𝔸, ∃ e₁ ∈ E, ∃ e₂ ∈ E,
-    e₁ ≠ e₂ ∧ x * e₁ - x * e₂ ∈ Set.range (Algebra.TensorProduct.includeLeft : D →ₐ[K] D_𝔸) :=
+lemma existsE : ∃ E : Set (D_𝔸), IsCompact E ∧
+    ∀ φ : D_𝔸 ≃ₜ+ D_𝔸, addEquivAddHaarChar φ = 1 → ∃ e₁ ∈ E, ∃ e₂ ∈ E,
+    e₁ ≠ e₂ ∧ φ e₁ - φ e₂ ∈ Set.range (Algebra.TensorProduct.includeLeft : D →ₐ[K] D_𝔸) :=
   sorry
 
 /-- An auxiliary set E used in the proof of Fukisaki's lemma. -/
@@ -72,15 +73,17 @@ def E : Set D_𝔸 := (existsE K D).choose
 
 lemma E_compact : IsCompact (E K D) := (existsE K D).choose_spec.1
 
-lemma E_noninjective_left : ∀ x ∈ ringHaarChar_ker D_𝔸,
+lemma E_noninjective_left {x : D_𝔸ˣ} (h : x ∈ ringHaarChar_ker D_𝔸) :
     ∃ e₁ ∈ E K D, ∃ e₂ ∈ E K D, e₁ ≠ e₂ ∧
     x * e₁ - x * e₂ ∈ Set.range (Algebra.TensorProduct.includeLeft : D →ₐ[K] D_𝔸) :=
-  (existsE K D).choose_spec.2
+  (existsE K D).choose_spec.2 (ContinuousAddEquiv.mulLeft x) h
 
-lemma E_noninjective_right : ∀ x ∈ ringHaarChar_ker D_𝔸,
+lemma E_noninjective_right {x : D_𝔸ˣ} (h : x ∈ ringHaarChar_ker D_𝔸) :
     ∃ e₁ ∈ E K D, ∃ e₂ ∈ E K D, e₁ ≠ e₂ ∧
     e₁ * x⁻¹ - e₂ * x⁻¹  ∈ Set.range (Algebra.TensorProduct.includeLeft : D →ₐ[K] D_𝔸) := by
-  sorry
+  let φ : D_𝔸 ≃ₜ+ D_𝔸 := ContinuousAddEquiv.mulRight x⁻¹
+  have hφ : addEquivAddHaarChar φ = 1 := sorry
+  exact (existsE K D).choose_spec.2 φ hφ
 
 open scoped Pointwise in
 /-- An auxiliary set X used in the proof of Fukisaki's lemma. Defined as E - E. -/
@@ -100,51 +103,29 @@ lemma Y_compact : IsCompact (Y K D) := by
 
 lemma X_meets_kernel {β : D_𝔸ˣ} (hβ : β ∈ ringHaarChar_ker D_𝔸) :
     ∃ x ∈ X K D, ∃ d ∈ Set.range (incl K D : Dˣ → D_𝔸ˣ), β * x = d := by
-  obtain ⟨e1, he1, e2, he2, noteq, b, hb⟩ := E_noninjective_left K D β hβ
-  use (e1 - e2)
-  constructor
-  · simpa only using (Set.sub_mem_sub he1 he2)
-  · have : IsUnit b := by
-      simp_rw [← mul_sub_left_distrib, Algebra.TensorProduct.includeLeft_apply] at hb
-      have h1 : ↑β * (e1 - e2) ≠ 0 := by
-        simpa only [ne_eq, not_not, Units.mul_right_eq_zero] using (sub_ne_zero_of_ne noteq)
-      simp only [isUnit_iff_ne_zero, ne_eq]
-      have h2 : b ⊗ₜ[K] (1 : AdeleRing (𝓞 K) K) ≠ 0 → b ≠ 0 := by
-        intro h
-        contrapose h
-        simp only [ne_eq, not_not] at h ⊢
-        simp only [h, TensorProduct.zero_tmul]
-      simpa only [ne_eq] using h2 (Ne.symm (Lean.Grind.ne_of_ne_of_eq_right hb (id (Ne.symm h1))))
-    obtain ⟨b1 , hb1⟩ := this
-    simp only [Set.mem_range, exists_exists_eq_and, Units.coe_map, RingHom.toMonoidHom_eq_coe,
-      MonoidHom.coe_coe]
-    use b1
-    simp only [mul_sub_left_distrib, (Eq.symm hb), Algebra.TensorProduct.includeLeft_apply, hb1,
-      Algebra.TensorProduct.includeLeftRingHom_apply]
+  obtain ⟨e1, he1, e2, he2, noteq, b, hb⟩ := E_noninjective_left K D hβ
+  refine ⟨e1 - e2, by simpa only using (Set.sub_mem_sub he1 he2), ?_⟩
+  obtain ⟨b1, rfl⟩ : IsUnit b := by
+    simp_rw [← mul_sub_left_distrib, Algebra.TensorProduct.includeLeft_apply] at hb
+    have h1 : ↑β * (e1 - e2) ≠ 0 := by
+      simpa only [ne_eq, not_not, Units.mul_right_eq_zero] using (sub_ne_zero_of_ne noteq)
+    simp only [isUnit_iff_ne_zero, ne_eq]
+    rintro rfl
+    simp only [← hb, TensorProduct.zero_tmul, ne_eq, not_true_eq_false] at h1
+  exact ⟨incl K D b1, ⟨b1, rfl⟩, by simpa [mul_sub] using hb.symm⟩
 
 lemma X_meets_kernel' {β : D_𝔸ˣ} (hβ : β ∈ ringHaarChar_ker D_𝔸) :
     ∃ x ∈ X K D, ∃ d ∈ Set.range (incl K D : Dˣ → D_𝔸ˣ), x * β⁻¹ = d := by
-  obtain ⟨e1, he1, e2, he2, noteq, b, hb⟩ := E_noninjective_right K D β hβ
-  use (e1 - e2)
-  constructor
-  · simpa only using (Set.sub_mem_sub he1 he2)
-  · have : IsUnit b := by
-      simp_rw [← mul_sub_right_distrib, Algebra.TensorProduct.includeLeft_apply] at hb
-      have h1 : (e1 - e2) * ↑β⁻¹ ≠ 0 := by
-        simpa only [ne_eq, Units.mul_left_eq_zero] using (sub_ne_zero_of_ne noteq)
-      simp only [isUnit_iff_ne_zero, ne_eq]
-      have h2 : b ⊗ₜ[K] (1 : AdeleRing (𝓞 K) K) ≠ 0 → b ≠ 0 := by
-        intro h
-        contrapose h
-        simp only [ne_eq, not_not] at h ⊢
-        simp only [h, TensorProduct.zero_tmul]
-      simpa only [ne_eq] using h2 (Ne.symm (Lean.Grind.ne_of_ne_of_eq_right hb (id (Ne.symm h1))))
-    obtain ⟨b1 , hb1⟩ := this
-    simp only [Set.mem_range, exists_exists_eq_and, Units.coe_map, RingHom.toMonoidHom_eq_coe,
-      MonoidHom.coe_coe]
-    use b1
-    simp only [mul_sub_right_distrib, (Eq.symm hb), Algebra.TensorProduct.includeLeft_apply, hb1,
-      Algebra.TensorProduct.includeLeftRingHom_apply]
+  obtain ⟨e1, he1, e2, he2, noteq, b, hb⟩ := E_noninjective_right K D hβ
+  refine ⟨e1 - e2, by simpa only using (Set.sub_mem_sub he1 he2), ?_⟩
+  obtain ⟨b1, rfl⟩ : IsUnit b := by
+    simp_rw [← mul_sub_right_distrib, Algebra.TensorProduct.includeLeft_apply] at hb
+    have h1 : (e1 - e2) * ↑β⁻¹ ≠ 0 := by
+      simpa only [ne_eq, Units.mul_left_eq_zero] using (sub_ne_zero_of_ne noteq)
+    simp only [isUnit_iff_ne_zero, ne_eq]
+    rintro rfl
+    simp only [← hb, TensorProduct.zero_tmul, ne_eq, not_true_eq_false] at h1
+  exact ⟨incl K D b1, ⟨b1, rfl⟩, by simpa [sub_mul] using hb.symm⟩
 
 /-- An auxiliary set T used in the proof of Fukisaki's lemma. Defined as Y ∩ Dˣ. -/
 def T : Set D_𝔸ˣ := ((↑) : D_𝔸ˣ → D_𝔸) ⁻¹' (Y K D) ∩ Set.range ((incl K D : Dˣ → D_𝔸ˣ))
@@ -164,24 +145,9 @@ lemma T_finite : Set.Finite (T K D) := by
       sorry
   have h1 : Units.val '' T K D ⊆ (Y K D) ∩
       (Set.range (Algebra.TensorProduct.includeLeft : D →ₐ[K] D_𝔸)) := by
-    simp only [Set.subset_inter_iff, Set.image_subset_iff]
-    constructor
-    · simp only [T, Set.inter_subset_left]
-    · refine Set.image_subset_iff.mp ?_
-      rw [T]
-      have h2 : Units.val '' (Set.range (incl K D : Dˣ → D_𝔸ˣ)) ⊆
-          (Set.range (Algebra.TensorProduct.includeLeft : D →ₐ[K] D_𝔸)) := by
-        simp only [Set.image_subset_iff]
-        refine Set.range_subset_iff.mpr ?_
-        intro y
-        simp only [Set.mem_preimage, Units.coe_map, RingHom.toMonoidHom_eq_coe, MonoidHom.coe_coe,
-          Algebra.TensorProduct.includeLeftRingHom_apply, Set.mem_range,
-          Algebra.TensorProduct.includeLeft_apply, exists_apply_eq_apply]
-      simp only [Set.image_inter Units.ext]
-      exact (Set.Subset.trans Set.inter_subset_right h2)
+    rintro _ ⟨t, ⟨ht1, d, rfl⟩, rfl⟩
+    exact ⟨ht1, d, rfl⟩
   exact Set.Finite.of_finite_image (Set.Finite.subset h h1) (Function.Injective.injOn Units.ext)
-
-
 
 open scoped Pointwise in
 /-- An auxiliary set C used in the proof of Fukisaki's lemma. Defined as T⁻¹X × X. -/
