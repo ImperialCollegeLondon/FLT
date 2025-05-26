d4a218c5be693a86a4de95312127dfbb81384219
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-05-26 22:39:23 +0100

    more lint

--- FLT/AutomorphicForm/GL2/HeckeOperators/Matrix.lean
+++ FLT/AutomorphicForm/GL2/HeckeOperators/Matrix.lean
@@ -11,6 +11,8 @@ namespace Matrix.GeneralLinearGroup
 
 variable {n R : Type*} [DecidableEq n] [Fintype n] [CommRing R]
 
+/-- The invertible diagonal matrix associated to a vector of units (the diagonal entries).
+-/
 def diagonal (d : n → Rˣ) : GL n R :=
   ⟨.diagonal <| fun i ↦ d i, .diagonal <| fun i ↦ ((d i)⁻¹ : Rˣ),
     by simp, by simp⟩
@@ -24,10 +26,12 @@ variable {A : Type*} [CommRing A] [IsDedekindDomain A] (K : Type*) [Field K] [Al
 
 namespace HeightOneSpectrum
 
+/-- A uniformiser associated to `v` in the integers of the completion `Kᵥ`. -/
 noncomputable def adicCompletionIntegersUniformizer (v : HeightOneSpectrum A) :
     v.adicCompletionIntegers K :=
   algebraMap A (v.adicCompletionIntegers K) v.intValuation_exists_uniformizer.choose
 
+/-- A uniformiser associated to `v` in the completion `Kᵥ`. -/
 noncomputable def adicCompletionUniformizer (v : HeightOneSpectrum A) :
     v.adicCompletion K :=
   algebraMap K (v.adicCompletion K) (v.valuation_exists_uniformizer K).choose
@@ -46,22 +50,23 @@ lemma adicCompletionUniformizer_ne_zero (v : HeightOneSpectrum A) :
   rw [adicCompletionUniformizer_spec] at h
   simp at h
 
+/-- A uniformiser associated to `v` in the units of the completion `Kᵥ`. -/
 noncomputable def adicCompletionUniformizerUnit (v : HeightOneSpectrum A) :
     (v.adicCompletion K)ˣ :=
   .mk0 (v.adicCompletionUniformizer K) <| v.adicCompletionUniformizer_ne_zero K
 
-/--
-The diagonal matrix `(ϖ 0; 0 1)` as a 2x2 matrix in `M_2(𝓞ᵥ)`. Do we even want this?
--/
-noncomputable def pi_zero_zero_one_int (v : HeightOneSpectrum A) :
-    Matrix (Fin 2) (Fin 2) (v.adicCompletionIntegers K) :=
-  .diagonal
-  ![algebraMap A (v.adicCompletionIntegers K) v.intValuation_exists_uniformizer.choose, 1]
+-- /--
+-- The diagonal matrix `(ϖ 0; 0 1)` as a 2x2 matrix in `M_2(𝓞ᵥ)`. Do we even want this?
+-- -/
+-- noncomputable def pi_zero_zero_one_int (v : HeightOneSpectrum A) :
+--     Matrix (Fin 2) (Fin 2) (v.adicCompletionIntegers K) :=
+--   .diagonal
+--   ![algebraMap A (v.adicCompletionIntegers K) v.intValuation_exists_uniformizer.choose, 1]
 
-noncomputable def pi_zero_zero_one (v : HeightOneSpectrum A) :
-    GL (Fin 2) (v.adicCompletion K) :=
-  .diagonal
-  ![.mk0 (v.adicCompletionUniformizer K) <| v.adicCompletionUniformizer_ne_zero K, 1]
+-- noncomputable def pi_zero_zero_one (v : HeightOneSpectrum A) :
+--     GL (Fin 2) (v.adicCompletion K) :=
+--   .diagonal
+--   ![.mk0 (v.adicCompletionUniformizer K) <| v.adicCompletionUniformizer_ne_zero K, 1]
 
 end HeightOneSpectrum
 
