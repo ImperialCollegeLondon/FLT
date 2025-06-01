415070cf8895cbc16e266afb04206aa43aa5df23
DavidLedvinka <50644608+DavidLedvinka@users.noreply.github.com>
2025-06-01 06:23:48 -0400

    feat: Submonoid.units_isOpen (#595)
    * proof finished
    
    * add leanok
    
    * remove assumption

--- FLT/Mathlib/Topology/Algebra/Group/Units.lean
+++ FLT/Mathlib/Topology/Algebra/Group/Units.lean
@@ -3,9 +3,9 @@ import Mathlib.Algebra.Group.Submonoid.Units
 import Mathlib.Topology.Algebra.Constructions
 import Mathlib.Topology.Algebra.ContinuousMonoidHom
 
-lemma Submonoid.units_isOpen {M : Type*} [TopologicalSpace M] [Monoid M] [ContinuousMul M]
-    {U : Submonoid M} (hU : IsOpen (U : Set M)) : IsOpen (U.units : Set Mˣ) := by
-  sorry -- FLT#587
+lemma Submonoid.units_isOpen {M : Type*} [TopologicalSpace M] [Monoid M]
+  {U : Submonoid M} (hU : IsOpen (U : Set M)) : IsOpen (U.units : Set Mˣ) :=
+  (hU.preimage Units.continuous_val).inter (hU.preimage Units.continuous_coe_inv)
 
 lemma Submonoid.units_isCompact {M : Type*} [TopologicalSpace M] [Monoid M] [ContinuousMul M]
     [T2Space M] {U : Submonoid M} (hU : IsCompact (U : Set M)) : IsCompact (U.units : Set Mˣ) := by
