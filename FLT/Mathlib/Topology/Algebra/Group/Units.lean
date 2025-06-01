32a39a6488ab46b1639124e4634f3f09f8421937
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-06-01 00:36:06 -0400

    remove assumption

--- FLT/Mathlib/Topology/Algebra/Group/Units.lean
+++ FLT/Mathlib/Topology/Algebra/Group/Units.lean
@@ -3,8 +3,8 @@ import Mathlib.Algebra.Group.Submonoid.Units
 import Mathlib.Topology.Algebra.Constructions
 import Mathlib.Topology.Algebra.ContinuousMonoidHom
 
-lemma Submonoid.units_isOpen {M : Type*} [TopologicalSpace M] [Monoid M] [ContinuousMul M]
-    {U : Submonoid M} (hU : IsOpen (U : Set M)) : IsOpen (U.units : Set Mˣ) :=
+lemma Submonoid.units_isOpen {M : Type*} [TopologicalSpace M] [Monoid M]
+  {U : Submonoid M} (hU : IsOpen (U : Set M)) : IsOpen (U.units : Set Mˣ) :=
   (hU.preimage Units.continuous_val).inter (hU.preimage Units.continuous_coe_inv)
 
 lemma Submonoid.units_isCompact {M : Type*} [TopologicalSpace M] [Monoid M] [ContinuousMul M]
