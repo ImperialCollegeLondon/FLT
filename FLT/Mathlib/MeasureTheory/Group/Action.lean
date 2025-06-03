db0b7699e086efe32afb76d11c5902c42f3c428d
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-06-03 06:08:44 -0400

    finished?

--- FLT/Mathlib/MeasureTheory/Group/Action.lean
+++ FLT/Mathlib/MeasureTheory/Group/Action.lean
@@ -34,6 +34,26 @@ lemma _root_.MeasurableEmbedding.IsMulLeftInvariant_comap {G H : Type*}
     rw [this, ← map_apply (by fun_prop), IsMulLeftInvariant.map_mul_left_eq_self]
     exact hφ.measurableSet_image.mpr hs
 
+@[to_additive]
+lemma _root_.MeasurableEmbedding.IsMulRightInvariant_comap {G H : Type*}
+    [Group G] [MeasurableSpace G] [MeasurableMul G]
+    [Group H] [MeasurableSpace H] [MeasurableMul H]
+    {φ : G →* H} (hφ : MeasurableEmbedding φ) (μ : Measure H) [IsMulRightInvariant μ] :
+    IsMulRightInvariant (comap φ μ) where
+  map_mul_right_eq_self g := by
+    ext s hs
+    rw [map_apply (by fun_prop) hs]
+    repeat rw [MeasurableEmbedding.comap_apply hφ]
+    have : φ '' ((fun x ↦ x * g) ⁻¹' s) = (fun x ↦ x * φ g) ⁻¹' (φ '' s) := by
+      ext
+      constructor
+      · rintro ⟨y, hy, rfl⟩
+        exact ⟨y * g, hy, by simp⟩
+      · intro ⟨y, yins, hy⟩
+        exact ⟨y * g⁻¹, by simp [yins], by simp [hy]⟩
+    rw [this, ← map_apply (by fun_prop), IsMulRightInvariant.map_mul_right_eq_self]
+    exact hφ.measurableSet_image.mpr hs
+
 end MeasurableEmbeddingComap
 
 open Subgroup Set
@@ -47,28 +67,31 @@ variable {G α : Type*} [Group G] [MeasurableSpace G] [MeasurableSpace α]
 instance [MeasurableMul₂ G] : MeasurableMul₂ H where measurable_mul := by measurability
 
 @[to_additive]
-instance [MeasurableInv G] : MeasurableInv H where measurable_inv := sorry
+instance [MeasurableInv G] : MeasurableInv H where
+  measurable_inv := Measurable.subtype_mk (by measurability)
 
 variable [MeasurableMul G]
 
 @[to_additive]
 instance : MeasurableMul H where
   measurable_mul_const c := by measurability
-  measurable_const_mul c := sorry -- https://github.com/ImperialCollegeLondon/FLT/issues/276
+  measurable_const_mul c := Measurable.subtype_mk (by measurability)
 
 @[to_additive]
-instance (μ : Measure G) [μ.IsMulLeftInvariant] :
-    (μ.comap Subtype.val : Measure H).IsMulLeftInvariant where
-  map_mul_left_eq_self g := sorry -- https://github.com/ImperialCollegeLondon/FLT/issues/276
+lemma isMulLeftInvariant_subtype_coe (μ : Measure G) [μ.IsMulLeftInvariant]
+  (hH : MeasurableSet (H : Set G)) : (μ.comap Subtype.val : Measure H).IsMulLeftInvariant :=
+  have hφ : MeasurableEmbedding H.subtype := MeasurableEmbedding.subtype_coe hH
+  hφ.IsMulLeftInvariant_comap μ
 
 @[to_additive]
-instance (μ : Measure G) [μ.IsMulRightInvariant] :
-    (μ.comap Subtype.val : Measure H).IsMulRightInvariant where
-  map_mul_right_eq_self g := sorry -- https://github.com/ImperialCollegeLondon/FLT/issues/276
+lemma isMulRightInvariant_subtype_coe (μ : Measure G) [μ.IsMulRightInvariant]
+    (hH : MeasurableSet (H : Set G)) : (μ.comap Subtype.val : Measure H).IsMulRightInvariant :=
+  have hφ : MeasurableEmbedding H.subtype := MeasurableEmbedding.subtype_coe hH
+  MeasurableEmbedding.IsMulRightInvariant_comap hφ μ
 
 @[to_additive index_mul_addHaar_addSubgroup]
 lemma index_mul_haar_subgroup [H.FiniteIndex] (hH : MeasurableSet (H : Set G)) (μ : Measure G)
-    [μ.IsMulLeftInvariant] : H.index * μ H = μ univ := by
+    [hμ : μ.IsMulLeftInvariant] : H.index * μ H = μ univ := by
   obtain ⟨s, hs, -⟩ := H.exists_isComplement_left 1
   have hs' : Finite s := hs.finite_left_iff.mpr inferInstance
   calc
@@ -81,8 +104,10 @@ lemma index_mul_haar_subgroup [H.FiniteIndex] (hH : MeasurableSet (H : Set G)) (
 @[to_additive index_mul_addHaar_addSubgroup_eq_addHaar_addSubgroup]
 lemma index_mul_haar_subgroup_eq_haar_subgroup [H.IsFiniteRelIndex K] (hHK : H ≤ K)
     (hH : MeasurableSet (H : Set G)) (hK : MeasurableSet (K : Set G)) (μ : Measure G)
-    [μ.IsMulLeftInvariant] : H.relindex K * μ H = μ K := by
-  have := index_mul_haar_subgroup (H := H.subgroupOf K) (measurable_subtype_coe hH)
+    [μ.IsMulLeftInvariant] :
+    H.relindex K * μ H = μ K := by
+  haveI := isMulLeftInvariant_subtype_coe μ hK
+  have := index_mul_haar_subgroup (H := H.subgroupOf K) (measurable_subtype_coe hH) (hμ := this)
     (μ.comap Subtype.val)
   simp only at this
   rw [MeasurableEmbedding.comap_apply, MeasurableEmbedding.comap_apply] at this
