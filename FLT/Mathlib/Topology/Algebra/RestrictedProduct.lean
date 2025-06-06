e1521a044ddcf75456cbb5a4e4bf969620b10571
Matthew Jasper <mjjasper1@gmail.com>
2025-06-06 01:44:25 +0100

    Fix simp lemmas

--- FLT/Mathlib/Topology/Algebra/RestrictedProduct.lean
+++ FLT/Mathlib/Topology/Algebra/RestrictedProduct.lean
@@ -467,23 +467,32 @@ lemma mem_coset_and_mulSupport_subset_of_isProductAt
     simp only [smul_eq_mul, mul_assoc, mul_inv_cancel_left, mul_right_inj, hcomm]⟩,
     mulSupport_mul_subset huᵢ hg⟩
 
+end RestrictedProduct
+
+end supports
+
 section flatten
+
+namespace RestrictedProduct
 
 variable {ι₂ : Type*} {𝒢 : Filter ι₂} {f : ι → ι₂} (C)
 
 variable (hf : Filter.Tendsto f ℱ 𝒢) in
 /-- The canonical map from a restricted product of products over fibres of a map on indexing sets
 to the restricted product over the original indexing set. -/
-@[simps!]
 def flatten : Πʳ j, [Π (i : f ⁻¹' {j}), G i, Set.pi Set.univ (fun (i : f ⁻¹' {j}) => C i)]_[𝒢] →
     Πʳ i, [G i, C i]_[ℱ] :=
   map _ G f hf (fun i x ↦ x ⟨i, rfl⟩) (by filter_upwards with x y hy using hy ⟨x, rfl⟩ trivial)
+
+@[simp]
+lemma flatten_apply (hf : Filter.Tendsto f ℱ 𝒢) (x) (i : ι) :
+    flatten C hf x i = x (f i) ⟨i, rfl⟩ :=
+  rfl
 
 variable (hf : Filter.comap f 𝒢 = ℱ)
 
 /-- The canonical bijection from a restricted product of products over fibres of a map on indexing
 sets to the restricted product over the original indexing set. -/
-@[simps!]
 def flatten_equiv :
     Πʳ j, [Π (i : f ⁻¹' {j}), G i, Set.pi Set.univ (fun (i : f ⁻¹' {j}) => C i)]_[𝒢] ≃
     Πʳ i, [G i, C i]_[ℱ] where
@@ -497,11 +506,20 @@ def flatten_equiv :
     rfl
   right_inv x := by ext i; rfl
 
+@[simp]
+lemma flatten_equiv_apply (x) (i : ι) :
+    flatten_equiv C hf x i = x (f i) ⟨i, rfl⟩ :=
+  rfl
+
+@[simp]
+lemma flatten_equiv_symm_apply (x) (i : ι₂) (j : f ⁻¹' {i}) :
+    (flatten_equiv C hf).symm x i j = x j.1 :=
+  rfl
+
 variable [Π i, TopologicalSpace (G i)]
 
 /-- The canonical homeomorphism from a restricted product of products over fibres of a map on
 indexing sets to the restricted product over the original indexing set. -/
-@[simps!]
 def flatten_homeomorph :
     Πʳ j, [Π (i : f ⁻¹' {j}), G i, Set.pi Set.univ (fun (i : f ⁻¹' {j}) => C i)]_[𝒢] ≃ₜ
     Πʳ i, [G i, C i]_[ℱ] where
@@ -539,11 +557,20 @@ def flatten_homeomorph :
       exact continuous_apply i
     apply (continuous_inclusion hT).comp hc
 
+@[simp]
+lemma flatten_homeomorph_apply (x) (i : ι) :
+    flatten_homeomorph C hf x i = x (f i) ⟨i, rfl⟩ :=
+  rfl
+
+@[simp]
+lemma flatten_homeomorph_symm_apply (x) (i : ι₂) (j : f ⁻¹' {i}) :
+    (flatten_homeomorph C hf).symm x i j = x j.1 :=
+  rfl
+
 variable (hf : Filter.Tendsto f Filter.cofinite Filter.cofinite)
 
 /-- The equivalence given by `flatten` when both restricted products are over the cofinite
 filter. -/
-@[simps!]
 def flatten_equiv' :
     Πʳ j, [Π (i : f ⁻¹' {j}), G i, Set.pi Set.univ (fun (i : f ⁻¹' {j}) => C i)] ≃
     Πʳ i, [G i, C i] :=
@@ -551,14 +578,37 @@ def flatten_equiv' :
     apply le_antisymm (Filter.comap_cofinite_le f) (Filter.map_le_iff_le_comap.mp hf)
   flatten_equiv C hf
 
+omit [(i : ι) → TopologicalSpace (G i)] in
+@[simp]
+lemma flatten_equiv'_apply (x) (i : ι) :
+    flatten_equiv' C hf x i = x (f i) ⟨i, rfl⟩ :=
+  rfl
+
+omit [(i : ι) → TopologicalSpace (G i)] in
+@[simp]
+lemma flatten_equiv'_symm_apply (x) (i : ι₂) (j : f ⁻¹' {i}) :
+    (flatten_equiv' C hf).symm x i j = x j.1 :=
+  rfl
+
 /-- The homeomorphism given by `flatten` when both restricted products are over the cofinite
 filter and there's a topology on the factors. -/
-@[simps!]
 def flatten_homeomorph' :
     Πʳ j, [Π (i : f ⁻¹' {j}), G i, Set.pi Set.univ (fun (i : f ⁻¹' {j}) => C i)] ≃ₜ
     Πʳ i, [G i, C i] :=
   have hf : Filter.comap f Filter.cofinite = Filter.cofinite := by
     apply le_antisymm (Filter.comap_cofinite_le f) (Filter.map_le_iff_le_comap.mp hf)
   flatten_homeomorph C hf
+
+@[simp]
+lemma flatten_homeomorph'_apply (x) (i : ι) :
+    flatten_homeomorph' C hf x i = x (f i) ⟨i, rfl⟩ :=
+  rfl
+
+@[simp]
+lemma flatten_homeomorph'_symm_apply (x) (i : ι₂) (j : f ⁻¹' {i}) :
+    (flatten_homeomorph' C hf).symm x i j = x j.1 :=
+  rfl
+
+end RestrictedProduct
 
 end flatten
