ed3e3fa139bfdfcb4e888fd6703d64e440048ccb
grunweg <rothgang@math.uni-bonn.de>
2025-06-07 22:22:32 +0200

    chore: bump mathlib (#612)
    Co-authored-by: Kevin Buzzard <k.buzzard@imperial.ac.uk>

--- FLT/Patching/Utils/Depth.lean
+++ FLT/Patching/Utils/Depth.lean
@@ -1,8 +1,9 @@
 import Mathlib.RingTheory.Flat.FaithfullyFlat.Basic
-import Mathlib.RingTheory.Regular.RegularSequence
-import Mathlib.RingTheory.TensorProduct.Free
-import Mathlib.RingTheory.Support
 import Mathlib.RingTheory.KrullDimension.NonZeroDivisors
+import Mathlib.RingTheory.Regular.RegularSequence
+import Mathlib.RingTheory.Spectrum.Prime.Topology
+import Mathlib.RingTheory.Support
+import Mathlib.RingTheory.TensorProduct.Free
 
 variable (R S M : Type*) [CommRing R] [CommRing S] [IsLocalRing R] [IsLocalRing S]
 variable [AddCommGroup M] [Module R M] [Module S M] [Algebra R S] [IsScalarTower R S M]
