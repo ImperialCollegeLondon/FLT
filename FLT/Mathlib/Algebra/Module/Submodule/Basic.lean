6b2a1e220c54c270e14fc3ab07c48c3295d9d364
Matthew Jasper <mjjasper1@gmail.com>
2025-06-12 23:50:12 +0100

    Prove A^\infty L has the A^\infty K-module topology

--- FLT/Mathlib/Algebra/Module/Submodule/Basic.lean
+++ FLT/Mathlib/Algebra/Module/Submodule/Basic.lean
@@ -1,10 +1,10 @@
 import Mathlib.Algebra.Module.Submodule.Basic
 import Mathlib.Algebra.Ring.Subring.Basic
 
-variable {R S : Type*} [CommRing R]
+variable {R S : Type*} [Ring R]
 
 /-- If `S` is a subring of `R`, then `S` can be viewed as an `S`-submodule of `R`. -/
-def Subring.toSubmodule {R : Type*} [CommRing R] (S : Subring R) : Submodule S R where
+def Subring.toSubmodule {R : Type*} [Ring R] (S : Subring R) : Submodule S R where
   __ := S
   smul_mem' x y h := by
     rw [smul_def, smul_eq_mul]
