1230f0a08cc8f642e1ff7b9fd3ee56962b5afc81
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2025-07-12 20:27:50 +0200

    Update scripts/upstreaming_dashboard.py
    Co-authored-by: Yaël Dillies <yael.dillies@gmail.com>

--- scripts/upstreaming_dashboard.py
+++ scripts/upstreaming_dashboard.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2024 Yaël Dillies. All rights reserved.
+# Copyright (c) 2024 Yaël Dillies, Javier Lopez-Contreras. All rights reserved.
 # Released under Apache 2.0 license as described in the file LICENSE.
 
 """
