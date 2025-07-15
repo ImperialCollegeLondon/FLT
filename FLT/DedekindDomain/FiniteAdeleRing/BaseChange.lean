37c242f7014109357fd6c4b9e2499e16d1040537
Matthew Jasper <mjjasper1@gmail.com>
2025-07-15 22:51:55 +0100

    Prove A^\infty L has the A^\infty K-module topology

--- FLT/DedekindDomain/FiniteAdeleRing/BaseChange.lean
+++ FLT/DedekindDomain/FiniteAdeleRing/BaseChange.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kevin Buzzard
+Authors: Kevin Buzzard, Matthew Jasper
 -/
 import FLT.Mathlib.Algebra.Algebra.Bilinear
 import FLT.Mathlib.Algebra.Algebra.Pi
@@ -9,6 +9,7 @@ import FLT.Mathlib.Algebra.Module.Submodule.Basic
 import FLT.Mathlib.NumberTheory.RamificationInertia.Basic
 import FLT.Mathlib.Topology.Algebra.Module.Equiv
 import FLT.Mathlib.Topology.Algebra.Module.ModuleTopology
+import FLT.Mathlib.Topology.Algebra.RestrictedProduct.Module
 import FLT.Mathlib.Topology.Algebra.UniformRing
 import FLT.Mathlib.Topology.Algebra.Valued.ValuationTopology
 import FLT.Mathlib.Topology.Algebra.Valued.WithVal
@@ -61,20 +62,35 @@ open IsDedekindDomain HeightOneSpectrum
 
 open scoped TensorProduct -- ⊗ notation for tensor product
 
+lemma tendsTo_comap_confinite [FaithfulSMul A B] :
+    Filter.Tendsto (comap A (B:=B)) Filter.cofinite Filter.cofinite :=
+  have : FaithfulSMul A (FractionRing B) := FractionRing.instFaithfulSMul A B
+  letI : Algebra (FractionRing A) (FractionRing B) :=
+    FractionRing.liftAlgebra A (FractionRing B)
+  (Filter.Tendsto.cofinite_of_finite_preimage_singleton <|
+    Extension.finite A (FractionRing A) (FractionRing B) B)
+
+omit [IsIntegralClosure B A L] [FiniteDimensional K L] in
+lemma confinite_mapsTo_adicCompletionComapSemialgHom :
+    ∀ᶠ (w : HeightOneSpectrum B) in Filter.cofinite,
+    Set.MapsTo (adicCompletionComapSemialgHom A K L B (comap A w) w rfl)
+      (adicCompletionIntegers K (comap A w)) (adicCompletionIntegers L w) := by
+  apply Filter.Eventually.of_forall
+  intro w
+  have := adicCompletionComapSemialgHom.mapadicCompletionIntegers A K L B (comap A w) w rfl
+  exact Set.image_subset_iff.1 this
+
 /-- The ring homomorphism `𝔸_K^∞ → 𝔸_L^∞` for `L/K` an extension of number fields. -/
 noncomputable def FiniteAdeleRing.mapRingHom :
-    FiniteAdeleRing A K →+* FiniteAdeleRing B L := RestrictedProduct.mapAlongRingHom
-  (fun (v : HeightOneSpectrum A) ↦ v.adicCompletion K)
-  (fun (w : HeightOneSpectrum B) ↦ w.adicCompletion L)
-  (HeightOneSpectrum.comap A)
-  (Filter.Tendsto.cofinite_of_finite_preimage_singleton <| Extension.finite A K L B)
-  (fun w ↦ adicCompletionComapSemialgHom A K L B (w.comap A) w rfl)
-  (by
-    apply Filter.Eventually.of_forall
-    intro w
-    have : FaithfulSMul A B := FaithfulSMul.of_field_isFractionRing A B K L
-    have := adicCompletionComapSemialgHom.mapadicCompletionIntegers A K L B (comap A w) w rfl
-    exact Set.image_subset_iff.1 this)
+    FiniteAdeleRing A K →+* FiniteAdeleRing B L :=
+  have : FaithfulSMul A B := FaithfulSMul.of_field_isFractionRing A B K L
+  RestrictedProduct.mapAlongRingHom
+    (fun (v : HeightOneSpectrum A) ↦ v.adicCompletion K)
+    (fun (w : HeightOneSpectrum B) ↦ w.adicCompletion L)
+    (HeightOneSpectrum.comap A)
+    (tendsTo_comap_confinite A B)
+    (fun w ↦ adicCompletionComapSemialgHom A K L B (w.comap A) w rfl)
+    (confinite_mapsTo_adicCompletionComapSemialgHom A K L B)
 
 /-- The ring homomorphism `𝔸_K^∞ → 𝔸_L^∞` for `L/K` an extension of number fields,
 as a morphism lying over the canonical map `K → L`. -/
@@ -92,20 +108,38 @@ noncomputable
 instance BaseChange.algebra : Algebra (FiniteAdeleRing A K) (FiniteAdeleRing B L) :=
   RingHom.toAlgebra (FiniteAdeleRing.mapRingHom A K L B)
 
+attribute [local instance 9999] Algebra.toSMul in
+noncomputable instance baseChangeAlgebra : Algebra K (FiniteAdeleRing B L) :=
+  Algebra.compHom (FiniteAdeleRing B L) (algebraMap K L)
+
+instance : IsScalarTower K L (FiniteAdeleRing B L) :=
+  IsScalarTower.of_algebraMap_eq' rfl
+
+attribute [local instance 9999] Algebra.toSMul in
+instance : IsScalarTower K (FiniteAdeleRing A K) (FiniteAdeleRing B L) := by
+  apply IsScalarTower.of_algebraMap_eq
+  intro x
+  nth_rw 2 [RingHom.algebraMap_toAlgebra]
+  symm
+  exact SemialgHom.commutes (FiniteAdeleRing.mapSemialgHom A K L B) x
+
+omit [IsIntegralClosure B A L] [FiniteDimensional K L] in
+lemma BaseChange.algebraMap_apply (w : HeightOneSpectrum B) (x : FiniteAdeleRing A K) :
+    letI := comap_algebra A K L B (w := w) rfl
+    algebraMap _ (FiniteAdeleRing B L) x w = algebraMap _ _ (x (comap A w)) :=
+  rfl
+
+omit [IsIntegralClosure B A L] [FiniteDimensional K L] in
 lemma FiniteAdeleRing.mapSemialgHom_continuous : Continuous (mapSemialgHom A K L B) :=
-  sorry
+  have : FaithfulSMul A B := FaithfulSMul.of_field_isFractionRing A B K L
+  RestrictedProduct.mapAlong_continuous _ _ _ (tendsTo_comap_confinite A B) _
+    (confinite_mapsTo_adicCompletionComapSemialgHom A K L B)
+    fun w ↦ adicCompletionComapSemialgHom_continuous A K L B _ w rfl
 
 attribute [instance 100] RestrictedProduct.instSMulCoeOfSMulMemClass
 -- otherwise
 -- #synth SMul (FiniteAdeleRing A K) (FiniteAdeleRing B L)
 -- spends 2 seconds failing to find `SMul (FiniteAdeleRing A K) (adicCompletion L w)
-
-lemma BaseChange.isModuleTopology : IsModuleTopology (FiniteAdeleRing A K) (FiniteAdeleRing B L) :=
-  sorry -- this should follow from the fact that L_w has the K_v-module topology? Hopefully
-  -- **TODO** this needs an issue number.
-
-noncomputable instance : TopologicalSpace (L ⊗[K] FiniteAdeleRing A K) :=
-  moduleTopology (FiniteAdeleRing A K) (L ⊗[K] FiniteAdeleRing A K)
 
 /-- The `L`-algebra isomorphism `L ⊗_K 𝔸_K^∞ ≅ 𝔸_L^∞`. -/
 noncomputable def FiniteAdeleRing.baseChangeAlgEquiv :
@@ -115,23 +149,106 @@ noncomputable def FiniteAdeleRing.baseChangeAlgEquiv :
     -- ⊢ Function.Bijective ⇑(mapSemialgHom A K L B).baseChange_of_algebraWMap
     sorry -- #243
 
+/-- The `𝔸_K^∞`-algebra isomorphism `L ⊗_K 𝔸_K^∞ ≅ 𝔸_L^∞`. -/
+noncomputable def FiniteAdeleRing.baseChangeAdeleAlgEquiv :
+    L ⊗[K] FiniteAdeleRing A K ≃ₐ[FiniteAdeleRing A K] FiniteAdeleRing B L where
+  __ := SemialgHom.baseChangeRightOfAlgebraMap <| FiniteAdeleRing.mapSemialgHom A K L B
+  __ := FiniteAdeleRing.baseChangeAlgEquiv A K L B
+
+instance : Module.Finite (FiniteAdeleRing A K) (FiniteAdeleRing B L) :=
+  Module.Finite.equiv (FiniteAdeleRing.baseChangeAdeleAlgEquiv A K L B).toLinearEquiv
+
+section moduleTopology
+
+open scoped RestrictedProduct
+
+attribute [local instance 9999] comap_pi_algebra Algebra.toSMul
+
+/-- `𝓞_v`-module structure on `∏ L_w` from restricting the scalars of the `K_v`-module structure. -/
+noncomputable local instance (v : HeightOneSpectrum A) : Module (adicCompletionIntegers K v)
+    ((w : Extension B v) → adicCompletion L w.val) :=
+  Module.compHom _ (algebraMap (adicCompletionIntegers K v) (adicCompletion K v))
+
+/-- SMul instance from the module structure. -/
+noncomputable local instance (v : HeightOneSpectrum A) : SMul (adicCompletionIntegers K v)
+    ((w : Extension B v) → adicCompletion L w.val) :=
+  Module.toDistribMulAction.toDistribSMul.toSMul
+
+/-- `∏_{w∣v} 𝓞_w` as an `𝓞_v`-submodule of `∏_{w∣v} L_w` -/
+noncomputable def piAdicIntegerSubmodule (v : HeightOneSpectrum A) :
+    Submodule (adicCompletionIntegers K v) ((w : Extension B v) → adicCompletion L w.val) :=
+  letI (w : Extension B v) := comap_algebra A K L B w.prop
+  letI (w : Extension B v) := comap_integer_algebra A K L B w.prop
+  let module (w : Extension B v) := Module.compHom (adicCompletion L w.val)
+    (algebraMap (adicCompletionIntegers K v) (adicCompletion K v))
+  Submodule.pi Set.univ fun (w : Extension B v) ↦
+    letI := (module w).toDistribMulAction.toDistribSMul.toSMul
+    have : IsScalarTower (adicCompletionIntegers K v) (adicCompletionIntegers L w.val)
+        (adicCompletion L w.val) :=
+      IsScalarTower.of_algebraMap_smul fun _ _ ↦ rfl
+    let s := (adicCompletionIntegers L w.val).toSubmodule
+    s.restrictScalars (adicCompletionIntegers K v)
+
+-- Help instance synthesis
+private noncomputable local instance (priority := 9999) (v : HeightOneSpectrum A) :
+    Module (adicCompletion K v) ((w : Extension B v) → adicCompletion L w.val) :=
+  Algebra.toModule
+
+open scoped RestrictedProduct in
+/-- The continuous `𝔸 K`-Linear equivalence between `∏'_v ∏_{w∣v} L_w` and `𝔸 L` given by
+reaindexing the elements. -/
+noncomputable def FiniteAdeleRing.restrictedProduct_pi_equiv :
+    Πʳ (v : HeightOneSpectrum A), [(w : Extension B v) → adicCompletion L w.val,
+      piAdicIntegerSubmodule A K L B v] ≃L[FiniteAdeleRing A K] FiniteAdeleRing B L :=
+  have := FaithfulSMul.of_field_isFractionRing A B K L
+  let f : _ ≃ₜ (FiniteAdeleRing B L) := RestrictedProduct.flatten_homeomorph'
+    (G := adicCompletion L) (fun w ↦ adicCompletionIntegers L w) (tendsTo_comap_confinite A B)
+  {
+    __ := f
+    map_add' x y := rfl
+    map_smul' r x := by
+      ext w
+      letI (w : HeightOneSpectrum B) := comap_algebra A K L B (w := w) rfl
+      rw [RingHom.id_apply, Algebra.smul_def, RestrictedProduct.mul_apply,
+        BaseChange.algebraMap_apply, ← Algebra.smul_def]
+      rfl
+  }
+
+omit [IsIntegralClosure B A L] in
+lemma FiniteAdeleRing.restrictedProduct_pi_isModuleTopology : IsModuleTopology (FiniteAdeleRing A K)
+    (Πʳ (v : HeightOneSpectrum A), [(w : Extension B v) → adicCompletion L w.val,
+      piAdicIntegerSubmodule A K L B v]) := by
+  have :=
+    Module.Finite.equiv (FiniteAdeleRing.restrictedProduct_pi_equiv A K L B).symm.toLinearEquiv
+  have := prodAdicCompletionComap_isModuleTopology A K L B
+  apply RestrictedProduct.isModuleTopology
+  · exact fun v ↦ Valued.isOpen_integer (adicCompletion K v)
+  · intro v
+    simp only [piAdicIntegerSubmodule, Submodule.coe_pi, Submodule.coe_restrictScalars]
+    apply isOpen_set_pi _ (fun _ _ ↦ Valued.isOpen_integer _)
+    rw [Set.finite_univ_iff]
+    exact Extension.finite A K L B v
+
+omit [IsIntegralClosure B A L] in
+lemma BaseChange.isModuleTopology : IsModuleTopology (FiniteAdeleRing A K) (FiniteAdeleRing B L) :=
+  have := FiniteAdeleRing.restrictedProduct_pi_isModuleTopology A K L B
+  IsModuleTopology.iso (FiniteAdeleRing.restrictedProduct_pi_equiv A K L B)
+
+end moduleTopology
+
+noncomputable instance : TopologicalSpace (L ⊗[K] FiniteAdeleRing A K) :=
+  moduleTopology (FiniteAdeleRing A K) (L ⊗[K] FiniteAdeleRing A K)
+
+/-- The continuous `𝔸_K^∞`-algebra isomorphism `L ⊗_K 𝔸_K^∞ ≅ 𝔸_L^∞` -/
+noncomputable def FiniteAdeleRing.baseChangeAdeleContinuousAlgEquiv :
+    L ⊗[K] FiniteAdeleRing A K ≃A[FiniteAdeleRing A K] FiniteAdeleRing B L :=
+  have := BaseChange.isModuleTopology A K L B
+  IsModuleTopology.continuousAlgEquivOfAlgEquiv <| baseChangeAdeleAlgEquiv A K L B
+
 /-- The continuous `L`-algebra isomorphism `L ⊗_K 𝔸_K^∞ ≅ 𝔸_L^∞` -/
 noncomputable def FiniteAdeleRing.baseChangeContinuousAlgEquiv :
     L ⊗[K] FiniteAdeleRing A K ≃A[L] FiniteAdeleRing B L where
   __ := FiniteAdeleRing.baseChangeAlgEquiv A K L B
-  continuous_toFun := sorry
-  continuous_invFun := sorry
-  -- TODO needs issue number
-
-noncomputable instance baseChangeAlgebra : Algebra K (FiniteAdeleRing B L) :=
-  RingHom.toAlgebra <| (algebraMap L _).comp (algebraMap K L)
-
-noncomputable instance baseChangeScalarTower :
-    IsScalarTower K (FiniteAdeleRing A K) (FiniteAdeleRing B L) := by
-  apply IsScalarTower.of_algebraMap_eq
-  intro x
-  nth_rw 2 [RingHom.algebraMap_toAlgebra]
-  symm
-  exact SemialgHom.commutes (FiniteAdeleRing.mapSemialgHom A K L B) x
+  __ := FiniteAdeleRing.baseChangeAdeleContinuousAlgEquiv A K L B
 
 end IsDedekindDomain
---
+++ FLT/Mathlib/Topology/Algebra/RestrictedProduct/Module.lean
@@ -0,0 +1,244 @@
+/-
+Copyright (c) 2025 Matthew Jasper. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Matthew Jasper
+-/
+
+import FLT.Mathlib.Topology.Algebra.RestrictedProduct.TopologicalSpace
+import FLT.Mathlib.Topology.Algebra.Module.ModuleTopology
+import FLT.Mathlib.Topology.Algebra.MulAction
+import FLT.Mathlib.Algebra.Module.Submodule.Basic
+
+/-!
+If `R : ι → Type*` is a family of rings, `B : (i : ι) → Subring (R i)` is a family of
+subrings, `M : ι → Type*` is a family of types, with `M i` having an `R i`-module structure
+and `C : (i : ι) → Submodule (B i) (M i)`, then `Πʳ i, [M i, C i]_[𝓕]` has a
+`Πʳ i, [R i, B i]_[𝓕]`-module structure.
+
+## Main definitions
+
+- `RestrictedProduct.module'` is the module structure defined above.
+
+- `RestrictedProduct.linearMap_component` is the component `M j →ₗ[R j] M' j` given by a linear
+  map `Πʳ i, [M i, C i]_[𝓕] →ₗ[Πʳ i, [R i, B i]_[𝓕]] Πʳ i, [M' i, C' i]_[𝓕]`.
+
+- `ContinuousLinearEquiv.restrictedProductPi` is the continuous linear equivalence between
+  `Πʳ i, [(R i)^n, (B i)^n]` and `Πʳ i, [(R i), (B i)]^n`.
+
+# Main theorems
+
+- `RestrictedProduct.isOpenMap_linearMap_of_surjective` : a condition for a linear map to be
+  surjective based on properties of its components.
+
+- Instances for `ContinuousSMul (Πʳ i, [R i, B i]_[𝓟 T]) (Πʳ i, [M i, C i]_[𝓟 T])` and
+  `ContinuousSMul (Πʳ i, [R i, B i]) (Πʳ i, [M i, C i])`.
+
+- `RestrictedProduct.isModuleTopology` : `Πʳ i, [M i, C i]` has the `Πʳ i, [R i, B i]`-module
+  topology if `(M i)` have the `(R i)`-module topology and `(B i)` and `(C i)` are open.
+-/
+
+namespace RestrictedProduct
+
+variable {ι : Type*}
+variable {R : ι → Type*} [Π i, Ring (R i)]
+variable {S : ι → Type*} -- subring type
+variable [Π i, SetLike (S i) (R i)] [∀ i, SubringClass (S i) (R i)]
+variable {B : Π i, S i}
+variable {ℱ : Filter ι}
+
+variable {M : ι → Type*} [Π i, AddCommGroup (M i)] [Π i, Module (R i) (M i)]
+variable {N : ι → Type*} -- submodule type
+variable [Π i, SetLike (N i) (M i)] [∀ i, AddSubgroupClass (N i) (M i)]
+variable [∀ i, SMulMemClass (N i) (B i) (M i)]
+variable {C : Π i, N i}
+
+instance : SMul (Πʳ i, [R i, B i]_[ℱ]) (Πʳ i, [M i, C i]_[ℱ]) where
+  smul r m := ⟨fun i ↦ (r i) • (m i), by
+    filter_upwards [r.prop, m.prop] with i hr hm using SMulMemClass.smul_mem ⟨r i, hr⟩ hm⟩
+
+omit [Π i, AddSubgroupClass (N i) (M i)] in
+@[simp]
+lemma smul_apply' (r : Πʳ i, [R i, B i]_[ℱ]) (m : Πʳ i, [M i, C i]_[ℱ]) (i : ι) :
+    (r • m) i = r i • m i :=
+  rfl
+
+instance module' : Module (Πʳ i, [R i, B i]_[ℱ]) (Πʳ i, [M i, C i]_[ℱ]) where
+  zero_smul m := by ext; simp
+  smul_zero r := by ext; simp
+  one_smul m := by ext; simp
+  add_smul r s m:= by ext; simp [add_smul]
+  smul_add r m n := by ext; simp
+  mul_smul r s m := by ext; simp [mul_smul]
+
+@[simp]
+lemma single_smul [DecidableEq ι] (i : ι) (r : R i) (m : Πʳ i, [M i, C i]) :
+    single B i r • m = single C i (r • m i) := by
+  ext j
+  obtain (rfl | hi) := em (i = j)
+  · simp
+  · simp [single_eq_of_ne' _ _ hi]
+
+@[simp]
+lemma smul_single [DecidableEq ι] (i : ι) (r : Πʳ i, [R i, B i]) (m : M i) :
+    r • single C i m = single C i (r i • m) := by
+  ext j
+  obtain (rfl | hi) := em (i = j)
+  · simp
+  · simp [single_eq_of_ne' _ _ hi]
+
+lemma single_eq_smul [DecidableEq ι] (i : ι) (m : Πʳ i, [M i, C i]) :
+    single B i 1 • m = single C i (m i) := by
+  simp
+
+variable {M₂ : ι → Type*} [Π i, AddCommGroup (M₂ i)] [Π i, Module (R i) (M₂ i)]
+variable {N₂ : ι → Type*} -- submodule type
+variable [Π i, SetLike (N₂ i) (M₂ i)] [∀ i, AddSubgroupClass (N₂ i) (M₂ i)]
+variable [∀ i, SMulMemClass (N₂ i) (B i) (M₂ i)]
+variable {C₂ : Π i, N₂ i}
+
+section components
+
+variable [DecidableEq ι]
+
+/-- Components of a linear map. -/
+noncomputable def linearMap_component
+    (f : Πʳ i, [M i, C i] →ₗ[Πʳ i, [R i, B i]] Πʳ i, [M₂ i, C₂ i]) (i : ι) : M i →ₗ[R i] M₂ i where
+  toFun x :=
+    f (single C i x) i
+  map_add' x y := by
+    simp [← add_apply, ← map_add f]
+  map_smul' r m := by
+    let r' := single B i r
+    have hr : r = r' i := by simp [r']
+    rw [hr, ← smul_single, map_smul, smul_apply', RingHom.id_apply]
+
+lemma linearMap_component_apply (f : Πʳ i, [M i, C i] →ₗ[Πʳ i, [R i, B i]] Πʳ i, [M₂ i, C₂ i])
+    (i : ι) (x : M i) : linearMap_component f i x = f (single C i x) i :=
+  rfl
+
+lemma linearMap_apply_eq_component (f : Πʳ i, [M i, C i] →ₗ[Πʳ i, [R i, B i]] Πʳ i, [M₂ i, C₂ i])
+    (x : Πʳ i, [M i, C i]) (i : ι) : f x i = (linearMap_component f i) (x i):= by
+  rw [linearMap_component_apply, ← single_eq_smul, map_smul, single_smul, single_eq_same, one_smul]
+
+end components
+
+variable [Π i, TopologicalSpace (R i)] [Π i, TopologicalSpace (M i)]
+
+section continuous_smul
+
+variable [∀ i, ContinuousSMul (R i) (M i)] (T : Set ι)
+
+open scoped Filter in
+instance : ContinuousSMul (Πʳ i, [R i, B i]_[𝓟 T]) (Πʳ i, [M i, C i]_[𝓟 T]) :=
+  isEmbedding_coe_of_principal.continuousSMul isEmbedding_coe_of_principal.continuous
+    (fun {c x} ↦ by ext; rfl)
+
+variable [hBopen : Fact (∀ i, IsOpen (B i : Set (R i)))]
+variable [hCopen : Fact (∀ i, IsOpen (C i : Set (M i)))]
+
+instance [∀ i, ContinuousSMul (R i) (M i)] :
+    ContinuousSMul (Πʳ i, [R i, B i]) (Πʳ i, [M i, C i]) where
+  continuous_smul := by
+    rw [continuous_dom_prod hBopen.elim hCopen.elim]
+    exact fun S hS ↦ (continuous_inclusion hS).comp continuous_smul
+
+end continuous_smul
+
+section components
+
+variable [Π i, TopologicalSpace (M₂ i)]
+
+omit [(i : ι) → TopologicalSpace (R i)] in
+theorem isOpenMap_linearMap_of_surjective [DecidableEq ι]
+    (hCopen : ∀ i, IsOpen (C i : Set (M i)))
+    (hCopen₂ : ∀ i, IsOpen (C₂ i : Set (M₂ i)))
+    (f : Πʳ i, [M i, C i] →ₗ[Πʳ i, [R i, B i]] Πʳ i, [M₂ i, C₂ i])
+    (hf : ∀ i, IsOpenMap (linearMap_component f i))
+    (hsurj : ∀ᶠ i in Filter.cofinite, Set.SurjOn (linearMap_component f i) (C i) (C₂ i)) :
+    IsOpenMap f := by
+  apply RestrictedProduct.isOpenMap_of_open_components hCopen hCopen₂ f
+    (fun i ↦ linearMap_component f i) (linearMap_apply_eq_component f) hf hsurj
+
+end components
+
+section free_topology
+
+variable (n : Type*) [Fintype n]
+
+variable (B) in
+/-- If `B i` is subring of `R i` then `(B i)^n` is a `B i`-submodule of `(R i)^n`. -/
+def piSubringSubmodule (i : ι) : Submodule (B i) (n → R i) :=
+  Submodule.pi Set.univ fun (_ : n) ↦ Subring.toSubmodule (Subring.ofClass (B i))
+
+/-- Canonical linear equivalence between `Π' R^n` and `(Π' R)^n` -/
+def _root_.LinearEquiv.restrictedProductPi
+    : Πʳ i, [n → R i, piSubringSubmodule B n i]_[ℱ] ≃ₗ[Πʳ i, [R i, B i]_[ℱ]]
+      n → Πʳ i, [R i, B i]_[ℱ] where
+  toFun x j := congrRight (fun i y ↦ y j)
+    (by
+      filter_upwards with i r hr
+      rw [piSubringSubmodule, Submodule.coe_pi, Set.mem_univ_pi] at hr
+      exact hr j)
+    x
+  invFun x := ⟨fun i j ↦ x j i, by simpa [piSubringSubmodule] using fun j ↦ (x j).eventually⟩
+  map_add' x y := rfl
+  map_smul' x y := rfl
+
+lemma isOpen_piSubringSubmodule (hOpen : ∀ i, IsOpen (B i : Set (R i))) (i : ι) :
+    IsOpen (SetLike.coe <| piSubringSubmodule B n i) := by
+  rw [piSubringSubmodule, Submodule.coe_pi]
+  apply isOpen_set_pi Set.finite_univ
+  intro j _
+  exact hOpen i
+
+variable [∀ i, IsTopologicalRing (R i)]
+
+/-- Canonical continuous linear equivalence between `Π' R^n` and `(Π' R)^n` -/
+def _root_.ContinuousLinearEquiv.restrictedProductPi (hOpen : ∀ i, IsOpen (B i : Set (R i)))
+    : Πʳ i, [n → R i, piSubringSubmodule B n i] ≃L[Πʳ i, [R i, B i]] n → Πʳ i, [R i, B i] where
+  __ := LinearEquiv.restrictedProductPi n
+  continuous_toFun := by
+    apply continuous_pi
+    intro i
+    dsimp [LinearEquiv.restrictedProductPi]
+    exact Continuous.restrictedProduct_congrRight _ (fun _ ↦ continuous_apply i)
+  continuous_invFun := by
+    have := Fact.mk hOpen
+    have := Fact.mk (isOpen_piSubringSubmodule n hOpen)
+    exact IsModuleTopology.continuous_of_linearMap
+      (LinearEquiv.restrictedProductPi n).symm.toLinearMap
+
+lemma moduleToplogy_of_prod (hOpen : ∀ i, IsOpen (B i : Set (R i))) :
+    IsModuleTopology (Πʳ i, [R i, B i]) (Πʳ i, [n → R i, piSubringSubmodule B n i]) :=
+  have := Fact.mk hOpen
+  IsModuleTopology.iso (ContinuousLinearEquiv.restrictedProductPi n hOpen).symm
+
+end free_topology
+
+variable [∀ i, IsTopologicalRing (R i)]
+variable [∀ i, IsModuleTopology (R i) (M i)]
+variable [Module.Finite (Πʳ i, [R i, B i]) (Πʳ i, [M i, C i])]
+
+theorem isModuleTopology (hBOpen : ∀ i, IsOpen (B i : Set (R i)))
+    (hCOpen : ∀ i, IsOpen (C i : Set (M i)))
+    : IsModuleTopology (Πʳ i, [R i, B i]) (Πʳ i, [M i, C i]) := by
+  have := Fact.mk hBOpen
+  have := Fact.mk hCOpen
+  obtain ⟨n, f, hf⟩ := Module.Finite.exists_fin' (Πʳ i, [R i, B i]) (Πʳ i, [M i, C i])
+  let g' := ContinuousLinearEquiv.restrictedProductPi (Fin n) hBOpen
+  let g := f ∘ₗ g'.toLinearMap
+  have (i : ι) : ContinuousAdd (M i) := IsModuleTopology.toContinuousAdd (R i) (M i)
+  have (i : ι) : ContinuousSMul (R i) (M i) := IsModuleTopology.toContinuousSMul (R i) (M i)
+  have := moduleToplogy_of_prod (Fin n) hBOpen
+  have hsurj : Function.Surjective g := hf.comp g'.surjective
+  apply IsModuleTopology.of_isOpenMap_surjective _ g _ hsurj
+  · classical
+    apply isOpenMap_linearMap_of_surjective (isOpen_piSubringSubmodule (Fin n) hBOpen) hCOpen
+    · intro i
+      apply IsModuleTopology.isOpenMap_of_surjective
+      exact surjective_components_of_surjective _ _ (linearMap_apply_eq_component g) hsurj _
+        (Set.Finite.compl_mem_cofinite (Set.finite_singleton i))
+    · exact eventually_surjOn_of_surjective Filter.comap_id.symm g _
+        (linearMap_apply_eq_component g) hsurj
+
+end RestrictedProduct
