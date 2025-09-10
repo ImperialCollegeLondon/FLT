182f23e225b969e5838c184f87b8972ebec785da
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-09-10 19:11:20 +0100

    feat: working on definition of hardly ramified representation

--- FLT/GaloisRepresentation/HardlyRamified.lean
+++ FLT/GaloisRepresentation/HardlyRamified.lean
@@ -6,7 +6,6 @@ Authors: Kevin Buzzard
 import FLT.Deformations.LiftFunctor
 import FLT.Deformations.RepresentationTheory.Basic
 
-
 /-
 
 # Hardly ramified representations
@@ -36,6 +35,42 @@ the predicted level is 1 or 2.
 **TODO** make definition
 -/
 
+open IsDedekindDomain
+open scoped NumberField
+
+def RingEquiv.heightOneSpectrum_comap {A B : Type*} [CommRing A] [CommRing B] (e : A ≃+* B)
+    (P : HeightOneSpectrum B) : HeightOneSpectrum A :=
+  {
+    asIdeal := .comap e P.asIdeal
+    isPrime := P.asIdeal.comap_isPrime e
+    ne_bot h := P.ne_bot <| Ideal.comap_injective_of_surjective e e.surjective <| by
+      rw [h, Ideal.comap_bot_of_injective e e.injective]
+  }
+
+open IsDedekindDomain in
+def RingEquiv.heightOneSpectrum {A B : Type*} [CommRing A] [CommRing B] (e : A ≃+* B) :
+    HeightOneSpectrum A ≃ HeightOneSpectrum B where
+      toFun := e.symm.heightOneSpectrum_comap
+      invFun := e.heightOneSpectrum_comap
+      left_inv P := by
+        ext1
+        convert Ideal.comap_comap e.toRingHom e.symm.toRingHom
+        simp
+      right_inv Q := by
+        ext1
+        convert Ideal.comap_comap e.symm.toRingHom e.toRingHom
+        simp
+
+def Nat.Prime.toHeightOneSpectrumInt {p : ℕ} (hp : p.Prime) : HeightOneSpectrum ℤ where
+  asIdeal := .span {(p : ℤ)}
+  isPrime := by
+    rwa [Ideal.span_singleton_prime (Int.ofNat_ne_zero.mpr hp.ne_zero), ← prime_iff_prime_int]
+  ne_bot := mt Submodule.span_singleton_eq_bot.mp (Int.ofNat_ne_zero.mpr hp.ne_zero)
+
+noncomputable def Nat.Prime.toHeightOneSpectrumRingOfIntegersRat {p : ℕ} (hp : p.Prime) :
+    IsDedekindDomain.HeightOneSpectrum (𝓞 ℚ) :=
+  Rat.ringOfIntegersEquiv.symm.heightOneSpectrum <| hp.toHeightOneSpectrumInt
+
 namespace GaloisRepresentation
 
 local notation3 "Γ" K:max => Field.absoluteGaloisGroup K
@@ -45,17 +80,36 @@ local notation3 K:max "ᵃˡᵍ" => AlgebraicClosure K
 Galois group of `ℚ` over quite a general local base with residue characteristig `ℓ > 2`,
 which has cyclotomic determinant, is unramified outside `2ℓ`, flat at `ℓ` and upper-triangular
 at 2 with a 1-dimensional quotient which is unramified and whose square is trivial. -/
-structure IsHardlyRamified {ℓ : ℕ} [Fact ℓ.Prime] (hp : Odd ℓ)
+structure IsHardlyRamified {ℓ : ℕ} (hℓ : ℓ.Prime) (hp : Odd ℓ)
     {R : Type*} [CommRing R] [TopologicalSpace R] [IsTopologicalRing R]
     [IsLocalRing R] (hl : (ℓ : R) ∈ IsLocalRing.maximalIdeal R)
     {V : Type*} [AddCommGroup V] [Module R V]
     [Module.Finite R V] [Module.Free R V] (hdim : Module.rank R V = 2)
     (ρ : GaloisRep ℚ R V) : Prop where
-  det : (sorry : Prop) --
-    -- ∀ g v, ρ.det g v = PadicInt.toZMod (cyclotomicCharacter Qbar ℓ (g.toRingEquiv))
-  isUnramified : ∀ p, p ≠ 2 ∧ p ≠ ℓ → (sorry : Prop)
-  isFlat : (sorry : Prop) -- ρ.IsFlatAt (ℓ : IsDedekindDomain.HeightOneSpectrum _)
-  isTameAtTwo : (sorry : Prop)
+  -- needs that R is a ℤℓ-alg
+  det : ∀ g, ρ.det g = sorry--((cyclotomicCharacter (ℚ ᵃˡᵍ) ℓ (g.toRingEquiv)) : ℤ_[ℓ])
+  isUnramified : ∀ p (hp : p.Prime), p ≠ 2 ∧ p ≠ ℓ →
+    ρ.IsUnramifiedAt hp.toHeightOneSpectrumRingOfIntegersRat
+  isFlat : ρ.IsFlatAt hℓ.toHeightOneSpectrumRingOfIntegersRat
+  isTameAtTwo : (sorry : Prop) -- use ρ.toLocal
 
 end GaloisRepresentation
-#min_imports
+
+example {ℓ : ℕ} (hℓ : ℓ.Prime) (hp : Odd ℓ)
+    {R : Type*} [CommRing R] [TopologicalSpace R] [IsTopologicalRing R]
+    [IsLocalRing R] (hl : (ℓ : R) ∈ IsLocalRing.maximalIdeal R)
+    {V : Type*} [AddCommGroup V] [Module R V]
+    [Module.Finite R V] [Module.Free R V] (hdim : Module.rank R V = 2)
+    (ρ : GaloisRep ℚ R V) : False := by
+  let ρ₂ := ρ.toLocal Nat.prime_two.toHeightOneSpectrumRingOfIntegersRat
+  -- want to say "ρ₂ is upper-triangular with unram 1d quotient whose square is trivial"
+  sorry
+
+  /-
+
+  TODO
+
+  1) Change definition of IsFlatAt?
+  2) Define tame at 2
+  3) Define coefficient ring and ℤ_l-algebra
+  -/
