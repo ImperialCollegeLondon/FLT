e282577079b054f8262ff35818b08323da06e35a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-09-10 15:35:18 +0100

    fix: remove #min_imports

--- FLT/GaloisRepresentation/HardlyRamified.lean
+++ FLT/GaloisRepresentation/HardlyRamified.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Kevin Buzzard. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kevin Buzzard
 -/
-import FLT.Deformations.LiftFunctor
 import FLT.Deformations.RepresentationTheory.Basic
 
 
@@ -58,4 +57,3 @@ structure IsHardlyRamified {ℓ : ℕ} [Fact ℓ.Prime] (hp : Odd ℓ)
   isTameAtTwo : (sorry : Prop)
 
 end GaloisRepresentation
-#min_imports
