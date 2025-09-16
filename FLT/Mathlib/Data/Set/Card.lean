287066b460e762a06d5fbc22994c3b53a310e50f
Kim Morrison <kim@tqft.net>
2025-09-16 23:34:14 +1000

    chore: deprecate duplicate definitions in FLT/Mathlib/

--- FLT/Mathlib/Data/Set/Card.lean
+++ FLT/Mathlib/Data/Set/Card.lean
@@ -1,5 +1,9 @@
 import Mathlib.Data.Set.Card
 
-@[simp] lemma Nat.card_set_eq_ncard {α : Type*} (s : Set α) : Nat.card s = s.ncard := rfl
+@[deprecated Nat.card_coe_set_eq (since := "2025-09-16")]
+lemma Nat.card_set_eq_ncard {α : Type*} (s : Set α) : Nat.card s = s.ncard := rfl
 
-@[simp] lemma ENat.card_set_eq_encard {α : Type*} (s : Set α) : ENat.card s = s.encard := rfl
+-- TODO: deprecate in favour of ENat.card_coe_set_eq after
+-- https://github.com/leanprover-community/mathlib4/pull/29716
+@[simp]
+lemma ENat.card_set_eq_encard {α : Type*} (s : Set α) : ENat.card s = s.encard := rfl
