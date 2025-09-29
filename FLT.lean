28da5a0ab852b7e2c9cb75d156cb1dd2763d7410
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-09-29 10:45:58 +0100

    fix FLT.lean

--- FLT.lean
+++ FLT.lean
@@ -81,7 +81,6 @@ import FLT.Mathlib.NumberTheory.Padics.PadicIntegers
 import FLT.Mathlib.NumberTheory.RamificationInertia.Basic
 import FLT.Mathlib.RepresentationTheory.Basic
 import FLT.Mathlib.RingTheory.DedekindDomain.Ideal.Lemmas
-import FLT.Mathlib.RingTheory.Finiteness.Pi
 import FLT.Mathlib.RingTheory.Ideal.Quotient.Basic
 import FLT.Mathlib.RingTheory.LocalRing.Defs
 import FLT.Mathlib.RingTheory.LocalRing.MaximalIdeal.Basic
