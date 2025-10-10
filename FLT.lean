6966ca7be35df7d29f4094dfd2386dbfef0298c3
maddycrim <maddycrim84@gmail.com>
2025-10-10 07:42:23 -0400

    changes

--- FLT.lean
+++ FLT.lean
@@ -10,9 +10,11 @@ import FLT.Basic.Reductions
 import FLT.DedekindDomain.AdicValuation
 import FLT.DedekindDomain.Completion.BaseChange
 import FLT.DedekindDomain.FiniteAdeleRing.BaseChange
+import FLT.DedekindDomain.FiniteAdeleRing.IsDirectLimit
 import FLT.DedekindDomain.FiniteAdeleRing.LocalUnits
 import FLT.DedekindDomain.FiniteAdeleRing.TensorPi
 import FLT.DedekindDomain.FiniteAdeleRing.TensorRestrictedProduct
+import FLT.DedekindDomain.FiniteAdeleRing.test1
 import FLT.DedekindDomain.IntegralClosure
 import FLT.Deformations.Algebra.InverseLimit.Basic
 import FLT.Deformations.Algebra.InverseLimit.Topology
