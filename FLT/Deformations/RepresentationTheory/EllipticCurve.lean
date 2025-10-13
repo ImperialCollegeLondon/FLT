50098b6c4b6eff6149e8c8d2819844d6af42d0ce
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-10-13 15:43:48 +0100

    lint

--- FLT/Deformations/RepresentationTheory/EllipticCurve.lean
+++ FLT/Deformations/RepresentationTheory/EllipticCurve.lean
@@ -1,8 +1,9 @@
 import FLT.Deformations.RepresentationTheory.GaloisRep -- definition of GaloisRep as cts action
 import Mathlib.AlgebraicGeometry.EllipticCurve.Affine.Point
 import Mathlib.Topology.Instances.ZMod
+import FLT.EllipticCurve.Torsion
 
--- **TODO** fill in these sorries
+-- **TODO** fill in the sorry
 
 universe u
 
@@ -10,14 +11,5 @@ variable {K : Type u} [Field K] (E : WeierstrassCurve K) [E.IsElliptic] [Decidab
 
 open WeierstrassCurve WeierstrassCurve.Affine
 
-noncomputable instance foo : AddCommGroup E⟮AlgebraicClosure K⟯ :=
-  letI : DecidableEq (AlgebraicClosure K) := Classical.typeDecidableEq _
-  inferInstance
-
-abbrev WeierstrassCurve.n_torsion' (n : ℕ) : Type u :=
-  Submodule.torsionBy ℤ (E ⟮(AlgebraicClosure K)⟯) n
-
-noncomputable instance bar (n : ℕ) : Module (ZMod n) (E.n_torsion' n) :=
-  AddCommGroup.zmodModule sorry
-
-def WeierstrassCurve.galoisRep (n : ℕ) (hn : 0 < n) : GaloisRep K (ZMod n) (E.n_torsion' n) := sorry
+/-- The continuous Galois representation associated to an elliptic curve over a field. -/
+def WeierstrassCurve.galoisRep (n : ℕ) (hn : 0 < n) : GaloisRep K (ZMod n) (E.n_torsion n) := sorry
