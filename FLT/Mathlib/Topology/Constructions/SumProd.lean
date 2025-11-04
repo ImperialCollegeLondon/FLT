a01c2f3c65291a7a9cdfce0102bb7df39f0038f8
WilliamCoram <williamecoram@gmail.com>
2025-11-04 12:27:38 +0000

    fun_prop

--- FLT/Mathlib/Topology/Constructions/SumProd.lean
+++ FLT/Mathlib/Topology/Constructions/SumProd.lean
@@ -3,6 +3,7 @@ import Mathlib.Topology.Constructions.SumProd
 variable (R S M N : Type*) [TopologicalSpace R] [TopologicalSpace S] [TopologicalSpace M]
   [TopologicalSpace N]
 
+@[fun_prop]
 lemma continuous_prodProdProdComm : Continuous (Equiv.prodProdProdComm R S M N) := by
   unfold Equiv.prodProdProdComm
   simp only [Equiv.coe_fn_mk]
