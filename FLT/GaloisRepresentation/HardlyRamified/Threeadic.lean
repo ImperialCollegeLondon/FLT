794eb3adca6d4ecda488334ee249a4a74c1e0ebc
Stepan <stepurik@stanford.edu>
2025-11-05 08:26:34 -0800

    feat: prove FreyCurve.torsion_not_isIrreducible modulo things knowns in the 1980s  (#760) This PR reduces the proof of FreyCurve.torsion_not_isIrreducible modulo other hard theorems in other files to the fact that a Galois representation with characteristic polynomial of Frobenius at p (X-1)(X-p) is reducible, which was known in the 1980s.

--- FLT/GaloisRepresentation/HardlyRamified/Threeadic.lean
+++ FLT/GaloisRepresentation/HardlyRamified/Threeadic.lean
@@ -1,5 +1,13 @@
 import FLT.GaloisRepresentation.HardlyRamified.Defs
 import FLT.GaloisRepresentation.HardlyRamified.ModThree -- will be needed for proof
+import Mathlib.RingTheory.Ideal.Int
+
+noncomputable def toNatPrime
+  (v : IsDedekindDomain.HeightOneSpectrum (NumberField.RingOfIntegers ℚ)) :
+  {p : ℕ // Nat.Prime p} := by
+  refine ⟨Ideal.absNorm (Ideal.under ℤ v.asIdeal), ?_⟩
+  have vnezero : NeZero v.asIdeal := ⟨v.ne_bot⟩
+  apply Nat.absNorm_under_prime
 
 namespace GaloisRepresentation.IsHardlyRamified
 
@@ -11,14 +19,16 @@ local notation "Frob" => Field.AbsoluteGaloisGroup.adicArithFrob
 /--
 A 3-adic hardly ramified representation has trace(Frob_p) = 1 + p for all p ≠ 2,3
 -/
+
 theorem three_adic {R : Type*} [CommRing R] [Algebra ℤ_[3] R] [Module.Finite ℤ_[3] R]
     [Module.Free ℤ_[3] R] [TopologicalSpace R] [IsTopologicalRing R] [IsLocalRing R]
     [IsModuleTopology ℤ_[3] R]
     (V : Type*) [AddCommGroup V] [Module R V] [Module.Finite R V] [Module.Free R V]
     (hV : Module.rank R V = 2) {ρ : GaloisRep ℚ R V}
     (hρ : IsHardlyRamified (show Odd 3 by decide) hV ρ) :
-    ∀ p (hp : Nat.Prime p) (hp5 : 5 ≤ p),
-      letI v := hp.toHeightOneSpectrumRingOfIntegersRat -- p as a finite place of ℚ
-      (ρ.toLocal v (Frob v)).trace _ _ = 1 + p := sorry
+    ∀ v (hp5 : 5 ≤ (toNatPrime v).val),
+      (ρ.toLocal v (Frob v)).charpoly =
+      (Polynomial.X - (1 : Polynomial R)) * (Polynomial.X - ((toNatPrime v) : Polynomial R)) :=
+      sorry
 
 end GaloisRepresentation.IsHardlyRamified
