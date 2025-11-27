a16d75e9a2ba8d4d3035a828a159566106b815a2
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2025-11-27 14:57:51 +0000

    timeout fixes

--- FLT/AutomorphicForm/QuaternionAlgebra/HeckeOperators/Concrete.lean
+++ FLT/AutomorphicForm/QuaternionAlgebra/HeckeOperators/Concrete.lean
@@ -104,7 +104,6 @@ open IsDedekindDomain.HeightOneSpectrum
 open scoped TensorProduct
 
 attribute [local instance high] IsScalarTower.right
-attribute [local instance high] IsModuleTopology.toContinuousSMul
 
 -- Oops! This is also `IsDedekindDomain.HeightOneSpectrum.QuaternionAlgebra.TameLevel`
 -- in `FLT.QuaternionAlgebra.NumberField`, defined differently (using `Subgroup.comap` not `map`).
