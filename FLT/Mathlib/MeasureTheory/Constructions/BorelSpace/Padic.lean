633c593a4a5eab928e58fb38a0a50fb1a490a92d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-12-06 17:46:30 +0000

    final tidying up

--- FLT/Mathlib/MeasureTheory/Constructions/BorelSpace/Padic.lean
+++ FLT/Mathlib/MeasureTheory/Constructions/BorelSpace/Padic.lean
@@ -1,8 +1,8 @@
 import Mathlib.MeasureTheory.Constructions.BorelSpace.Basic
 import Mathlib.NumberTheory.Padics.PadicNumbers
 
-variable (p : ℕ) [Fact p.Prime] in
+variable (p : ℕ) [Fact p.Prime]
+
 instance : MeasurableSpace ℚ_[p] := borel _
 
-variable (p : ℕ) [Fact p.Prime] in
 instance : BorelSpace ℚ_[p] := ⟨rfl⟩
