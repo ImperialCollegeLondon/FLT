b00fe659d96fcb582220d7f6368bb365052ed48f
bwangpj <70694994+bwangpj@users.noreply.github.com>
2025-12-22 12:21:14 +0800

    Update MeasurableSpacePadics.lean

--- FLT/HaarMeasure/MeasurableSpacePadics.lean
+++ FLT/HaarMeasure/MeasurableSpacePadics.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies, David Loeffler
 -/
 import Mathlib.MeasureTheory.Measure.Haar.Basic
+import Mathlib.MeasureTheory.Measure.Haar.Unique
 import Mathlib.NumberTheory.Padics.ProperSpace
 import FLT.Mathlib.NumberTheory.Padics.PadicIntegers
 
@@ -52,6 +53,9 @@ instance instBorelSpace : BorelSpace ℤ_[p] := Subtype.borelSpace _
 lemma isMeasurableEmbedding_coe : MeasurableEmbedding ((↑) : ℤ_[p] → ℚ_[p]) := by
   convert isOpenEmbedding_coe.measurableEmbedding
 
+lemma isMeasurableEmbedding_coeRingHom : MeasurableEmbedding (Coe.ringHom (p := p)) :=
+  (coe_coeRingHom (p := p)) ▸ isMeasurableEmbedding_coe
+
 noncomputable instance instMeasureSpace : MeasureSpace ℤ_[p] := ⟨addHaarMeasure ⊤⟩
 
 instance instIsAddHaarMeasure : IsAddHaarMeasure (volume : Measure ℤ_[p]) :=
@@ -62,7 +66,33 @@ instance instIsAddHaarMeasure : IsAddHaarMeasure (volume : Measure ℤ_[p]) :=
 instance instIsFiniteMeasure : IsFiniteMeasure (volume : Measure ℤ_[p]) where
   measure_univ_lt_top := by simp
 
+instance isCompact_subring : IsCompact (subring p).carrier := by
+  simpa [Metric.closedBall] using isCompact_closedBall (0 : ℚ_[p]) 1
+
+instance compactSpace_subring : CompactSpace (subring p) :=
+  isCompact_iff_compactSpace.mp isCompact_subring
+
 -- https://github.com/ImperialCollegeLondon/FLT/issues/278
-@[simp] lemma volume_coe (s : Set ℤ_[p]) : volume ((↑) '' s : Set ℚ_[p]) = volume s := sorry
+@[simp] lemma volume_coe (s : Set ℤ_[p]) : volume ((↑) '' s : Set ℚ_[p]) = volume s := by
+  rw [← (coe_coeRingHom (p := p)), ← isMeasurableEmbedding_coeRingHom.comap_apply]
+  have := IsAddLeftInvariant.comap volume (f := Coe.ringHom.toAddMonoidHom)
+    (by apply isMeasurableEmbedding_coeRingHom (p := p))
+  have := IsFiniteMeasureOnCompacts.comap' volume
+    (by simpa using continuous_iff_le_induced.mpr fun U a ↦ a)
+    (isMeasurableEmbedding_coeRingHom (p := p))
+  rw [isAddLeftInvariant_eq_smul (comap Coe.ringHom volume) (volume : Measure ℤ_[p])]
+  suffices (comap (Coe.ringHom (p := p)) volume).addHaarScalarFactor volume = 1 by
+    simp [-coe_coeRingHom, this]
+  
+  -- #check AddMonoidHom.measurePreserving
+  /- let fcoe : ℤ_[p] →ₜ+ (subring p)  := {
+    toFun x := ⟨Coe.ringHom x, by simp⟩
+    map_zero' := by simp
+    map_add' _ _ := by norm_cast
+    continuous_toFun := by simpa using continuous_id
+  }
+  #check AddMonoidHom.measurePreserving
+  (μ := volume.restrict _) (ν := volume) fcoe.continuous_toFun -/
+  sorry
 
 end PadicInt
