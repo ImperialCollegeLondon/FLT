076ff3e02779caa2d04febed57ba65781106f697
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2025-12-23 15:11:57 +0000

    tidy up; add comments

--- FLT/HaarMeasure/HaarChar/AdeleRing.lean
+++ FLT/HaarMeasure/HaarChar/AdeleRing.lean
@@ -130,20 +130,24 @@ lemma MeasureTheory.addHaarScalarFactor_tensor_adeles_eq_one (φ : V ≃ₗ[K] V
     [MeasurableSpace (V ⊗[K] 𝔸 K)] [BorelSpace (V ⊗[K] 𝔸 K)] :
     addEquivAddHaarChar
       (ContinuousLinearEquiv.baseChange K (𝔸 K) V V φ).toContinuousAddEquiv = 1 := by
+  -- we deduce this from the corresponding statement for `K = ℚ`.
+  -- A K-module is a ℚ-module
   let : Module ℚ V := Module.compHom V (algebraMap ℚ K)
-  have : Module.Finite ℚ V := by exact FiniteDimensional.trans ℚ K V
-  have : IsScalarTower ℚ K V := inferInstance
+  have : Module.Finite ℚ V := FiniteDimensional.trans ℚ K V
   let : Module (AdeleRing (𝓞 ℚ) ℚ) (V ⊗[K] AdeleRing (𝓞 K) K) :=
     Module.compHom _ (algebraMap (AdeleRing (𝓞 ℚ) ℚ) (AdeleRing (𝓞 K) K))
   have : IsScalarTower (AdeleRing (𝓞 ℚ) ℚ) (AdeleRing (𝓞 K) K) (V ⊗[K] AdeleRing (𝓞 K) K) :=
     IsScalarTower.of_algebraMap_smul fun r ↦ congrFun rfl
+  -- and V ⊗[K] 𝔸_K ≃ V ⊗[ℚ] 𝔸_ℚ
   let f := NumberField.AdeleRing.ModuleBaseChangeContinuousAddEquiv ℚ K V
   borelize (V ⊗[ℚ] AdeleRing (𝓞 ℚ) ℚ)
   have φℚ : V ≃ₗ[ℚ] V := by exact Function.invFun (fun a ↦ φ) φ
+  -- and the obvious diagram commutes
   have := MeasureTheory.addEquivAddHaarChar_eq_addEquivAddHaarChar_of_continuousAddEquiv f
     (ContinuousLinearEquiv.baseChange ℚ (𝔸 ℚ) V V (φ.restrictScalars ℚ)).toContinuousAddEquiv
     (ContinuousLinearEquiv.baseChange K (𝔸 K) V V φ).toContinuousAddEquiv
   rw [← this]
+  -- so the result follows from the case K=ℚ
   · apply MeasureTheory.addHaarScalarFactor_tensor_adeles_rat_eq_one
   · intro x
     induction x with
