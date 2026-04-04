5573910c51ad7a16f59229cffc69ffa5eedf3577
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2026-04-04 14:44:41 +0100

    fix Mathlib.MeasureTheory.Constructions.BorelSpace

--- FLT/Mathlib/MeasureTheory/Constructions/BorelSpace/InfinitePlace.lean
+++ FLT/Mathlib/MeasureTheory/Constructions/BorelSpace/InfinitePlace.lean
@@ -6,10 +6,11 @@ open InfinitePlace.Completion
 
 variable (K : Type*) [Field K] [NumberField K] (v : InfinitePlace K)
 
-instance : MeasurableSpace (v.Completion) := borel _
+noncomputable instance : MeasurableSpace (v.Completion) := borel _
 
 instance : BorelSpace (v.Completion) := ⟨rfl⟩
 
-instance : MeasurableSpace (InfiniteAdeleRing K) := inferInstanceAs (MeasurableSpace (∀ _, _))
+noncomputable instance : MeasurableSpace (InfiniteAdeleRing K) :=
+  inferInstanceAs (MeasurableSpace (∀ _, _))
 
 instance : BorelSpace (InfiniteAdeleRing K) := inferInstanceAs (BorelSpace (∀ _, _))
