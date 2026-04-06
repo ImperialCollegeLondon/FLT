f40ce0f75c073f1757a8c6086a4c3598e0bf3f75
Salvatore Mercuri <salvatore.mercuri@imperial.ac.uk>
2026-04-06 17:06:45 +0100

    mapEquiv

--- FLT/Mathlib/Topology/Algebra/Valued/WithZeroMulInt.lean
+++ FLT/Mathlib/Topology/Algebra/Valued/WithZeroMulInt.lean
@@ -102,9 +102,9 @@ theorem integer_compactSpace [CompleteSpace K] [IsDiscreteValuationRing 𝒪[K]]
     refine isCompact_iff_isCompact_univ.1 <| isCompact_iff_totallyBounded_isComplete.2
       ⟨(hasBasis_uniformity _ _).totallyBounded_iff.2 fun γ _ ↦ ?_, (isClosed_integer K).isComplete⟩
     obtain ⟨t, htf, ht⟩ := finite_cover_of_uniformity_basis
-      (Units.map (valueGroup₀_equiv_withZeroMulInt v).toMonoidHom γ) h
+      (Units.mapEquiv (valueGroup₀_equiv_withZeroMulInt v) γ) h
     refine ⟨t, htf, ht.trans fun x hx ↦ ?_⟩
-    simp only [Units.coe_map, Set.mem_setOf_eq, Set.mem_iUnion] at hx ⊢
+    simp only [Set.mem_setOf_eq, Set.mem_iUnion] at hx ⊢
     obtain ⟨i, hit, hi⟩ := hx
     use i, hit
     rw [← (valueGroup₀_equiv_withZeroMulInt_strictMono _).lt_iff_lt,
