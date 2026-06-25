aa6562980026cd74066b205fc47378a2c5a1d35f
Andrew Yang <the.erd.one@gmail.com>
2026-06-25 17:58:39 +0200

    oops

--- FLT/Mathlib/MeasureTheory/Group/ModularCharacter.lean
+++ FLT/Mathlib/MeasureTheory/Group/ModularCharacter.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2026 Andrew Yang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Andrew Yang
+-/
 module
 
 public import FLT.AutomorphicForm.Stuff
