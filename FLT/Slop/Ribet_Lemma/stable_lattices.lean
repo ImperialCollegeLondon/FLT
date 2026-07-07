a36c2151ffb5e2ef3af9c19d569c569e5b4d5887
bryanhu623 <bryan.hu1998@gmail.com>
2026-07-07 12:02:02 +0100

    minor changes

--- FLT/Slop/Ribet_Lemma/stable_lattices.lean
+++ FLT/Slop/Ribet_Lemma/stable_lattices.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Bryan Hu. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Bryan Hu
+Authors: Zachary Feng, Bryan Hu, Y. Samanda Zhang
 -/
 module
 
