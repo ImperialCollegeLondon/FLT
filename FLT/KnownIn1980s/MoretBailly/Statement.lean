f9da6bbb863bafbd0cf5a7faf1db7dc34a145824
Justus Springer <justusspringer@gmx.de>
2026-07-10 11:39:41 +0100

    update Copyright

--- FLT/KnownIn1980s/MoretBailly/Statement.lean
+++ FLT/KnownIn1980s/MoretBailly/Statement.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2026 Kevin Buzzard. All rights reserved.
+Copyright (c) 2026 Justus Springer. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Justus Springer
 -/
